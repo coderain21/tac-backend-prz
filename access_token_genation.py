16935b7f22c7b75d47b3c64abe9e7acd6e5ad483
Namratha Shettigar <namratha.shettigar@7edge.com>
2023-08-07 18:02:36 +0530

    FF-888 updated api-documentation

--- access_token_genation.py
+++ access_token_genation.py
@@ -1,25 +1,37 @@
-import boto3, os
+import boto3
+import os
 from botocore.exceptions import ClientError
+
 from dotenv import load_dotenv
 client = boto3.client('cognito-idp', region_name='eu-west-2')
 load_dotenv()
 
 
 def generate_token():
-     try:
+    try:
+        user_pool_id = os.environ.get('COGNITO_USER_POOL_ID')
+        client_id = os.environ.get('COGNITO_CLIENT_ID')
+        username = os.environ.get('USERNAME')
+        password = os.environ.get('PASSWORD')
+
+        if user_pool_id is None or client_id is None or username is None or password is None:
+            print("Required environment variables are not set.")
+            return
+
         response = client.admin_initiate_auth(
-            UserPoolId='eu-west-2_kqcLIvA4D',
-            ClientId='3duudq593a3j7jpp7afv1vbmuc',
+            UserPoolId=user_pool_id,
+            ClientId=client_id,
             AuthFlow='ADMIN_NO_SRP_AUTH',
             AuthParameters={
-                'USERNAME': 'aishwarya+indy@7edge.com',
-                'PASSWORD': 'Aishu@76980'
+                'USERNAME': username,
+                'PASSWORD': password
             }
         )
         token = response['AuthenticationResult']['IdToken']
         os.environ['TOKEN'] = token
         print(os.environ.get('TOKEN'))
-     except ClientError as e:
+    except ClientError as e:
         print(e)
+
 
 generate_token()
