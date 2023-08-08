883d750bd2c57b1eb39bc4fea2565946ed4ab4ae
Sandhya-7EDGE <sandhyashri@7edge.com>
2023-08-08 12:02:40 +0530

    Implemented update password

--- services/users/handlers/update_password.js
+++ services/users/handlers/update_password.js
@@ -4,75 +4,48 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
-const crypto = require('crypto')
-const Joi = require('joi')
-// eslint-disable-next-line import/no-extraneous-dependencies
-const request = require('request')
-const passwordValidator = require('password-validator')
+const CryptoJS = require('crypto-js')
+const { validationCheck } = require('../data/password_validation_check')
 const Users = require('../entities/Users')
 const mongoConnection = require('../lib/mongodb_helper')
 const helpers = require('../lib/helper')
+const cognitoHelper = require('../lib/cognito_helper')
 
-function encryptWithTimeValidation(data, secretKey) {
-    const timestamp = Date.now().toString()
-    const cipher = crypto.createCipher('aes-256-cbc', secretKey)
-    let encryptedData = cipher.update(timestamp + JSON.stringify(data), 'utf8', 'hex')
-    encryptedData += cipher.final('hex')
-    return encryptedData
-}
-
-module.exports.verifyReCaptcha = async (event) => {
+module.exports.updatePassword = async (event) => {
     try {
         const userData = JSON.parse(event.body)
+        const email = decodeURIComponent(event.pathParameters.email)
         const connection = await mongoConnection.connect()
-        const user_details = await Users.findOne({ email_address: userData.email_address})
-        const decryptedPassword = await helpers.encryptDecryptPassword(user_details.password, false)
-        if (decryptedPassword !== userData.current_password) {
+        const get_user = await mongoConnection.view(Users, { email_address: email })
+        console.log('get', get_user)
+        const user_old_password = get_user[0].password
+        const bytes = CryptoJS.AES.decrypt(user_old_password, process.env.PASSWORD_SECRET_KEY)
+        const old_password = bytes.toString(CryptoJS.enc.Utf8)
+        const conditionCheck = await validationCheck(old_password, userData)
+        if (conditionCheck.statusCode !== 200) {
+            return conditionCheck
+        }
+        const request_body = {
+            password: CryptoJS.AES.encrypt(userData.new_password, process.env.PASSWORD_SECRET_KEY).toString(),
+        }
+        const update_user_information = await mongoConnection.update(Users, get_user[0]._id, request_body)
+        console.log('update_user_information', update_user_information)
+        await connection.disconnect()
+        if (update_user_information.modifiedCount > 0) {
+            userData.email = email
+            const cognito_update = await cognitoHelper.cognitoResetPassword(userData)
+            console.log('cognito', cognito_update)
             return {
-                success: false,
-                message: 'Please enter the correct password',
+                statusCode: 201,
+                headers: await helpers.getHeaders(),
+                body: JSON.stringify({ message: 'Password updated successfully' }),
             }
         }
-        // await connection.disconnect()
-        if (userData.password !== userData.confirm_password) {
-            return {
-                statusCode: 400,
-                headers: await helpers.getHeaders(),
-                body: JSON.stringify({ message: 'Password Doesnt match' }),
-            }
+        return {
+            statusCode: 400,
+            headers: await helpers.getHeaders(),
+            body: JSON.stringify({ message: 'Password not updated.Please try again.' }),
         }
-        // eslint-disable-next-line new-cap
-        const passwordSchema = new passwordValidator()
-        // Add password validation rules
-        passwordSchema
-            .is().min(8) // Minimum length 8 characters
-            .has().uppercase()
-            .is()
-            .max(100) // Maximum length 100 characters
-            .has()
-            .lowercase() // Must have at least one lowercase letter
-            .has()
-            .digits() // Must have at least one digit
-            .has()
-            .symbols() // Must have at least one symbol
-        // Validate the password
-        const isPasswordValid = passwordSchema.validate(user_details.new_password)
-        if (!isPasswordValid) {
-            return {
-                statusCode: 400,
-                headers: await helpers.getHeaders(),
-                body: JSON.stringify({ message: 'Invalid Password' }),
-            }
-        }
-        if (user_details.new_password !== user_details.confirm_password)
-        {
-            return {
-                statusCode: 400,
-                headers: await helpers.getHeaders(),
-                body: JSON.stringify({ message: 'Password Doesnt match' }),
-            }
-        
-
     } catch (error) {
         console.log(error)
         return {
