1fe4a8684a2134cab063e93b6ff0e093f86e5dc5
Sandhya-7EDGE <sandhyashri@7edge.com>
2023-08-09 10:21:59 +0530

    FF-873 #time 6h #commit Implemented goggle signup and signin

--- services/users/handlers/cognito_post_confirmation.js
+++ services/users/handlers/cognito_post_confirmation.js
@@ -2,6 +2,12 @@
 /* eslint-disable no-console */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
+const AWS = require('aws-sdk')
+const Users = require('../entities/Users')
+const mongoConnection = require('../lib/mongodb_helper')
+
+AWS.config.update({ region: process.env.REGION })
+
 /**
  * Function to handle  cognito post challenge event
  * @param {Object} event
@@ -11,31 +17,8 @@
  */
 exports.handler = async (event, _context, callback) => {
     try {
-        console.log('event', event)
-        // const eventData = event
-        // console.log(eventData)
-        // const connection = await mongoConnection.connect()
-        // const user = {
-        //     child_first_name: eventData.child_first_name,
-        //     child_last_name: eventData.child_last_name,
-        //     child_age: eventData.child_age,
-        //     parent_first_name: eventData.parent_first_name,
-        //     parent_last_name: eventData.parent_last_name,
-        //     email: eventData.email,
-        //     country: eventData.country,
-        //     password: eventData.password,
-
-        // }
-        // const get_user = await connection.db(process.env.MONGODB_NAME).collection(process.env.MONGODB_COLLECTION_NAME).insertOne(user)
-        // console.log('USER', get_user)
-        // const create_users = await cognitoHelper.cognitoCreate(user)
-        // console.log('users', create_users)
-        // await connection.disconnect()
-        // if (!user) {
-        //     return { success_status: false, message: 'There was an error while creating the usser' }
-        // }
-        // await connection.disconnect()
-        callback(null, event)
+        console.log('event', event, event.identities[0].email)
+        // callback(null, event)
     } catch (error) {
         console.log(error)
         callback(error, event)
--- services/users/handlers/cognito_presignup.js
+++ services/users/handlers/cognito_presignup.js
@@ -1,112 +1,134 @@
-/* eslint-disable no-inner-declarations */
-/* eslint-disable no-console */
+/* eslint-disable no-plusplus */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable no-shadow */
+/* eslint-disable consistent-return */
+/* eslint-disable no-useless-catch */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unused-vars */
 /* eslint-disable prefer-destructuring */
 /* eslint-disable no-promise-executor-return */
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-shadow */
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+const CryptoJS = require('crypto-js')
+const uuid = require('uuid')
 const AWS = require('aws-sdk')
 
 const cognito = new AWS.CognitoIdentityServiceProvider()
+const Users = require('../entities/Users')
+const mongoConnection = require('../lib/mongodb_helper')
+const cognitoHelper = require('../lib/cognito_helper')
 
-exports.handler = (event, context, callback) => {
-    console.log('event', event)
-    try {
-        function checkForExistingUsers(event, linkToExistingUser) {
+function generateRandomPassword(length) {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    let password = ''
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length)
+        password += charset.charAt(randomIndex)
+    }
+
+    return password
+}
+
+exports.handler = async (event, context, callback) => {
+    async function checkForExistingUsers(event, linkToExistingUser) {
+        console.log('Executing checkForExistingUsers')
+
+        try {
             const params = {
                 UserPoolId: event.userPoolId,
                 AttributesToGet: ['sub', 'email'],
                 Filter: `email = "${event.request.userAttributes.email}"`,
             }
-            return new Promise((resolve, reject) => cognito.listUsers(params, (err, result) => {
+
+            const result = await new Promise((resolve, reject) => cognito.listUsers(params, (err, data) => {
                 if (err) {
                     reject(err)
                     return
                 }
-                if (result && result.Users && result.Users[0] && result.Users[0].Username && linkToExistingUser) {
-                    console.log('Found existing users: ', result.Users)
-                    if (result.Users.length > 1) {
-                        result.Users.sort((a, b) => ((a.UserCreateDate > b.UserCreateDate) ? 1 : -1))
-                        console.log('Found more than one existing users. Ordered by createdDate: ', result.Users)
-                    }
-                    linkUser(result.Users[0].Username, event).then((result) => {
-                        resolve(result)
-                    })
-                        .catch((error) => {
-                            reject(err)
-                        })
-                } else {
-                    resolve(result)
-                }
+                resolve(data)
             }))
-        }
 
-        function linkUser(sub, event) {
-            // By default, assume the existing account is a Cognito username/password
-            let destinationProvider = 'Cognito'
-            let destinationSub = sub
-            // If the existing user is in fact an external user (Xero etc), override the the provider
-            if (sub.includes('_')) {
-                destinationProvider = sub.split('_')[0]
-                destinationSub = sub.split('_')[1]
-            }
-            const params = {
-                DestinationUser: {
-                    ProviderAttributeValue: destinationSub,
-                    ProviderName: destinationProvider,
-                },
-                SourceUser: {
-                    ProviderAttributeName: 'Cognito_Subject',
-                    ProviderAttributeValue: event.userName.split('_')[1],
-                    ProviderName: event.userName.split('_')[0],
-                },
-                UserPoolId: event.userPoolId,
-            }
-            console.log('Parameters for adminLinkProviderForUser: ', params)
-            return new Promise((resolve, reject) => cognito.adminLinkProviderForUser(params, (err, result) => {
-                if (err) {
-                    console.log('Error encountered whilst linking users: ', err)
-                    reject(err)
-                    return
+            if (result.Users && result.Users.length > 0 && result.Users[0].Username && linkToExistingUser) {
+                console.log('Found existing users: ', result.Users)
+                if (result.Users.length > 1) {
+                    result.Users.sort((a, b) => ((a.UserCreateDate > b.UserCreateDate) ? 1 : -1))
+                    console.log('Found more than one existing users. Ordered by createdDate: ', result.Users)
                 }
-                console.log('Successfully linked users.')
-                resolve(result)
-            }))
-        }
+                await linkUser(result.Users[0].Username, event)
+                return result
+            }
+            let newPassword = generateRandomPassword(10) // Change the length as needed
+            newPassword = await CryptoJS.AES.encrypt(newPassword, process.env.PASSWORD_SECRET_KEY).toString()
 
-        console.log(JSON.stringify(event))
-
-        if (event.triggerSource === 'PreSignUp_SignUp' || event.triggerSource === 'PreSignUp_AdminCreateUser') {
-            checkForExistingUsers(event, false).then((result) => {
-                if (result != null && result.Users != null && result.Users[0] != null) {
-                    console.log('Found at least one existing account with that email address: ', result)
-                    console.log('Rejecting sign-up')
-                    // prevent sign-up
-                    callback('An external provider account alreadys exists for that email address', null)
-                } else {
-                    // proceed with sign-up
-                    callback(null, event)
-                }
-            })
-                .catch((error) => {
-                    console.log('Error checking for existing users: ', error)
-                    // proceed with sign-up
-                    callback(null, event)
-                })
+            const userData = {
+                user_name: event.request.userAttributes.email,
+                email_address: event.request.userAttributes.email,
+                password: newPassword,
+                is_first_time_login: true,
+                user_type: 'seller',
+            }
+            const connection = await mongoConnection.connect()
+            const user = await mongoConnection.save(userData, Users)
+            console.log(user)
+            const cognitoResponse = await cognitoHelper.cognitoCreate(userData)
+            console.log(cognitoResponse)
+            await connection.disconnect()
+            await linkUser(event.request.userAttributes.email, event)
+        } catch (error) {
+            throw error
         }
+    }
 
-        if (event.triggerSource === 'PreSignUp_ExternalProvider') {
-            checkForExistingUsers(event, true).then((result) => {
-                console.log('Completed looking up users and linking them: ', result)
-                callback(null, event)
-            })
-                .catch((error) => {
-                    console.log('Error checking for existing users: ', error)
-                    // proceed with sign-up
-                    callback(null, event)
-                })
+    function linkUser(sub, event) {
+        console.log(`Linking user accounts with target sub: ${sub}and event: `, event)
+
+        // By default, assume the existing account is a Cognito username/password
+        let destinationProvider = 'Cognito'
+        let destinationSub = sub
+        // If the existing user is in fact an external user (Xero etc), override the the provider
+        if (sub.includes('_')) {
+            destinationProvider = sub.split('_')[0]
+            destinationSub = sub.split('_')[1]
         }
-    } catch (err) {
-        console.log(err)
+        const params = {
+            DestinationUser: {
+                ProviderAttributeValue: destinationSub,
+                ProviderName: destinationProvider,
+            },
+            SourceUser: {
+                ProviderAttributeName: 'Cognito_Subject',
+                ProviderAttributeValue: event.userName.split('_')[1],
+                ProviderName: event.userName.split('_')[0],
+            },
+            UserPoolId: event.userPoolId,
+        }
+        console.log('Parameters for adminLinkProviderForUser: ', params)
+        return new Promise((resolve, reject) => cognito.adminLinkProviderForUser(params, (err, result) => {
+            if (err) {
+                console.log('Error encountered whilst linking users: ', err)
+                reject(err)
+                return
+            }
+            console.log('Successfully linked users.')
+            resolve(result)
+        }))
+    }
+
+    console.log(JSON.stringify(event))
+
+    if (event.triggerSource === 'PreSignUp_ExternalProvider') {
+        try {
+            const result = await checkForExistingUsers(event, true)
+            console.log('Completed looking up users and linking them: ', result)
+            callback(null, event)
+        } catch (error) {
+            console.log('Error checking for existing users: ', error)
+            // proceed with sign-up
+            callback(null, event)
+        }
+    } else {
+        callback(null, event)
     }
 }
