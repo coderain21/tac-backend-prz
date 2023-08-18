722026d9a9cba8563e981f9d52601dbf3675cb76
shrinit-poojary-7edge <shrinit.poojary@7edge.com>
2023-08-18 17:53:21 +0530

    FF-876 #time 5 min

--- services/users/handlers/kyc_webhook.py
+++ services/users/handlers/kyc_webhook.py
@@ -32,8 +32,7 @@ def kyc_webhook(event, context):
 
         data = json.loads(event['body'])
 
-        if data['levelName']=='basic_kyc_level':
-
+        if data['levelName']=='basic-kyc-level':
             applicant_id = data["applicantId"]
             client = MongoClient(os.environ['MONGO_CLIENT'])
             db = client[os.environ['DATABASE']]
