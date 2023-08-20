2913c2cb35bc9669e625999caeabf2a7ebce0423
Namratha Shettigar <namratha.shettigar@7edge.com>
2023-08-20 20:38:08 +0530

    #time added domain name in the url

--- doc.sh
+++ doc.sh
@@ -28,7 +28,7 @@ do
     base_name=$(basename "$file" -swagger.json)
 
     # Construct the URL
-    url="https://indyaution.net/swagger_api_documentation/$base_name-swagger.json"
+    url="https://docs.indyaution.net/swagger_api_documentation/$base_name-swagger.json"
 
     # Check if the JSON object already exists in the array
     duplicate=false
