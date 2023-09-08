48a745e425ffc5a2ff68e2fb07a3b4d780a6a5a5
sarvesh <sarvesh.hugar@7edge.com>
2023-09-08 17:46:18 +0530

    FF-1180 #time 5m bugfix: desctiption field in CSV The description is CSV was coming with html content .Fix - used re to fix it

--- services/auctions/handlers/list.py
+++ services/auctions/handlers/list.py
@@ -1,6 +1,7 @@
 """This module is used to list the auctions """
 import json
 import os
+import re
 import csv
 import boto3
 from pymongo import MongoClient
@@ -202,7 +203,7 @@ def export_as_csv(auctions):
                 modified_auction = {}
                 modified_auction["Auction ID"] = auction["auction_id"]
                 modified_auction["Auction Name"] = auction["title"]
-                modified_auction["Auction Description"] = auction["description"]
+                modified_auction["Auction Description"] = re.sub(re.compile(r'<.*?>'), '', auction["description"])
                 modified_auction["Timezone"] = auction["time_zone"]
                 modified_auction["Auction Start Date"] = "" if auction["start_date"] is None else datetime.fromisoformat(str(auction["start_date"])).strftime("%d %B %Y")
                 modified_auction["Auction Start Time"] = "" if auction['start_date'] is None else datetime.fromisoformat(str(auction["start_date"])).strftime("%H:%M")
