af48be9f13ee607659030b7e386105a35df8c01a
Sandhya-7EDGE <sandhyashri@7edge.com>
2023-09-08 19:16:47 +0530

    bugfix: Invalid date format decription: Fixed the date issue in the auction create

--- services/auctions/handlers/create.js
+++ services/auctions/handlers/create.js
@@ -25,6 +25,8 @@ module.exports.create_auction = async (event) => {
         const counter = await Counter.findOneAndUpdate({ auction_id: email, record_type: 'Auctions', status: 'Active' }, { $inc: { starting_sequence: 1 } }, { new: true, upsert: true }).exec()
         const sequenceNumber = `A${helpers.leftPad(counter.starting_sequence, 4)}`
         request_body.auction_id = sequenceNumber
+        request_body.start_date = new Date(request_body.start_date)
+        request_body.end_date = new Date(request_body.end_date)
         const auction = await mongoConnection.save(request_body, Auction)
         if (auction) {
             update_value = {
