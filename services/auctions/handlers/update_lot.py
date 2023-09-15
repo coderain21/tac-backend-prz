ae2a3fa7ba2afb548e21e9c707e799c5e01e904a
shrinit-poojary-7edge <shrinit.poojary@7edge.com>
2023-09-15 10:36:27 +0530

    #FF-1268 #time 45m title: implementing update note. description:    update note function done all the fields to be updated are included need to check in aws offline

---
+++ services/auctions/handlers/update_lot.py
@@ -0,0 +1,99 @@
+"""this library is used to import env's in the update_lot function """
+import os
+import json
+import pymongo
+
+# CORS headers
+headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': True,
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Methods': '*'
+}
+
+def update_lot(event):
+    """
+    The `update_lot` function updates the details of a lot in a collection based
+    on the provided request body.
+    :param request_body: The `request_body` parameter is a dictionary that contains
+    the data sent in the request body. It is expected to have the following keys:
+    :return: a tuple containing the status code and a dictionary message. The status code indicates the
+    success or failure of the lot update operation, and the message provides
+    additional information about the result.
+    """
+    try:
+            seller_email = event['requestContext']['authorizer']['claims']['email']
+    except:
+            return {
+                "statusCode": 403,
+                "headers": headers,
+                "body": json.dumps({"message": "You do not have access to perform this API action"})
+            }    
+    request_body = json.loads(event['body'])
+    # Initialize the MongoDB client
+    client = pymongo.MongoClient(os.environ['MONGO_CLIENT'])
+    db = client[os.environ['DATABASE']]
+    collection = db[os.environ["LOT_COLLECTION_NAME"]]
+    lot_number = request_body.get('lot_number')
+    auction_id = request_body.get('auction_id')
+
+    if not lot_number or not seller_email:
+        return (400, {"message": "Both lot_number and seller_email are required for lot update."})
+
+    update_data = {
+        "lot_title": request_body.get('lot_title', ''),
+        "description": request_body.get('description', ''),
+        "starting_price": request_body.get('starting_price', ''),
+        "low_estimate": request_body.get('low_estimate', ''),
+        "high_estimate": request_body.get('high_estimate', ''),
+        "shipping_address": request_body.get('shipping_address', ''),
+        "tags": request_body.get('tags', []),
+        "images": request_body.get('images', []),
+    }
+
+    update_result = collection.update_one(
+        {"lot_number": lot_number, "seller_email": seller_email,"auction_id": auction_id},
+        {"$set": update_data}
+    )
+    client.close()
+    if update_result.modified_count == 1:
+        return (200, {"message": "updated successfully."})
+    else:
+        return (404, {"message": "Lot not found for seller."})
+
+def lambda_handler(event, context):
+    """
+    The lambda_handler function is a Python function that handles incoming JSON requests, 
+    updates a lot based on the request body, and returns a response with the appropriate status
+    code and response body.
+    
+    :param event: 
+    The `event` parameter is a dictionary that contains information about the event that
+    triggered the Lambda function. It typically includes details such as the HTTP request headers,
+    request body, and other metadata
+    :param context: T
+    he `context` parameter in the `lambda_handler` function is an object that provides
+    information about the runtime environment of the Lambda function.
+    It includes properties such as the
+    AWS request ID, function name, function version, and more. The `context` object is automatically
+    passed to the Lambda function by the
+    :return: The lambda_handler function is returning a dictionary with three keys: "statusCode",
+    "headers", and "body". The value of "statusCode" is the status code of the response, 
+    the value of "headers" is a dictionary of headers for the response, 
+    and the value of "body" is a JSON string representing the response body.
+    """
+    try:
+        # Parse the incoming JSON request
+        status_code, response_body = update_lot(event)
+        return {
+            "statusCode": status_code,
+            "headers": headers,
+            "body": json.dumps(response_body)
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "headers": headers,
+            "body": json.dumps({"error": str(e)})
+        }
