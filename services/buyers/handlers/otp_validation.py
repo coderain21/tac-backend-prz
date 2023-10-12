b816c1992ade9367813f31bd3373f2bca5c314bf
sarvesh <sarvesh.hugar@7edge.com>
2023-10-12 15:52:14 +0530

    FF-1018 #time 3h feature : Changed the code to python. Description: Changed the code back to python and created otp validation api in python

--- services/buyers/handlers/otp_validation.py
+++ services/buyers/handlers/otp_validation.py
@@ -2,6 +2,7 @@ import json
 import os
 import time
 import boto3
+import bcrypt
 from pymongo import MongoClient
 
 from lib.helper_python import decrypt_with_time_validation
@@ -14,10 +15,15 @@ headers = {
     'Access-Control-Allow-Methods': '*'
 }
 
+def hash_password(password):
+    # Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password.decode('utf-8')
 
 cognito_client = boto3.client('cognito-idp', region_name=os.environ['REGION'])
 
-def admin_create_user(userData,userpool_id):
+def admin_create_user(userData, userpool_id):
     try:
         attribute_list = [
             {'Name': 'email', 'Value': userData['email_address']}
@@ -30,15 +36,8 @@ def admin_create_user(userData,userpool_id):
             'MessageAction': 'SUPPRESS'
         }
 
+        # Attempt to create the user
         user = cognito_client.admin_create_user(**admin_create_user_params)
-
-        password_params = {
-            'UserPoolId': userpool_id,
-            'Username': userData['email_address'],
-            'Password': userData['password'],
-            'Permanent': True
-        }
-        cognito_client.admin_set_user_password(**password_params)
 
         if user:
             cognito_client.admin_add_user_to_group(
@@ -56,6 +55,7 @@ def admin_create_user(userData,userpool_id):
                 'success_status': False,
                 'message': 'There was an error while creating the admin account'
             }
+
     except Exception as e:
         print(e)
         return {
@@ -63,15 +63,16 @@ def admin_create_user(userData,userpool_id):
             'message': str(e)
         }
 
+
 def validate(event, context):
     try:
         data = json.loads(event['body'])
 
         encrypted_token = data.get('session_token')
-        otp = data.get('otp')
+        otp = int(data.get('otp'))
         domain = data.get('domain')
 
-        if not encrypted_token or not otp:
+        if not encrypted_token or not otp or not domain:
             return {
                 'statusCode': 400,
                 'headers': headers,
@@ -80,6 +81,7 @@ def validate(event, context):
 
         secret_key = os.environ["ENCRYPTION_SECRET_KEY"]
         decrypted_data = decrypt_with_time_validation(encrypted_token, secret_key)
+        print(decrypted_data)
         timestamp = decrypted_data["time_stamp"]
         if timestamp is None or decrypted_data is None:
             return {
@@ -99,12 +101,11 @@ def validate(event, context):
             }
 
         if int(decrypted_data.get('otp')) != otp:
-        
+            
             return {
                 'statusCode': 400,
                 'headers': headers,
-                'body': json.dumps({'message': 'Invalid OTP',
-                                    'decrypted_dtaa':decrypted_data})
+                'body': json.dumps({'message': 'Invalid OTP'})
             }
         client = MongoClient(os.environ['MONGO_CLIENT'])
         db = client[os.environ['DATABASE']]
@@ -113,12 +114,20 @@ def validate(event, context):
         # Your code to create the user in Cognito
         response = admin_create_user(decrypted_data,userpool_id["user_pool_id"])
 
-        if response:
+        if response and response["success_status"] == True:
             # Your code to store the decrypted token data in MongoDB
             
             collection = db[os.environ["BUYER_COLLECTION"]]
-            
-            collection.insert_one(decrypted_data)
+
+            insert_data = {}
+            insert_data["email_address"] = decrypted_data["email_address"]
+            insert_data["first_name"] = decrypted_data["first_name"]
+            insert_data["last_name"] = decrypted_data["last_name"]
+            insert_data["password"] = hash_password(decrypted_data["password"])
+            insert_data["terms_and_condition"] = decrypted_data["terms_and_condition"]
+            insert_data["user_type"] = decrypted_data["user_type"]
+            insert_data["newsletter_notification"] = decrypted_data["newsletter_notification"]
+            collection.insert_one(insert_data)
             client.close()
 
             return {
@@ -128,9 +137,9 @@ def validate(event, context):
             }
         else:
             return {
-                'statusCode': 500,
+                'statusCode': 409,
                 'headers': headers,
-                'body': json.dumps({'message': "Error creating the user in Cognito"})
+                'body': json.dumps({'message': "User already exists"})
             }
 
     except Exception as e:
