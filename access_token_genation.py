221948241f87354bbd54fa494f28874f82e90d6a
shrinit-poojary-7edge <shrinit.poojary@7edge.com>
2023-10-30 18:23:38 +0530

    FF-1178 #time 10m #Feature: accept buyers Dscription:API Testing

--- access_token_genation.py
+++ access_token_genation.py
@@ -63,7 +63,7 @@ def generate_token(user_type):
         token = response['AuthenticationResult']['IdToken']
         os.environ['TOKEN'] = token
         print(f'export {user_type}="{token}"')
-    except ClientError as e:
+    except ClientError as e:z
         print('error sadagrfyhh', e)
 
 generate_token("USER")
