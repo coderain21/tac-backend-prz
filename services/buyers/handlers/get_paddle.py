b781d1423a79eaabb91b377859572e37166c68cf
shrinit <shrinit.poojary@7edge.com>
2023-11-03 09:50:26 +0530

    FF-1359 #time 1h Bugfix: Search using special characters Description: Bug is Fixed

--- services/buyers/handlers/get_paddle.py
+++ services/buyers/handlers/get_paddle.py
@@ -74,7 +74,7 @@ def paddle_number(event, context):
             return {
                 "statusCode": 204,
                 "headers": headers,
-                "body": json.dumps({"message": "paddle not found"})
+                "body": json.dumps({"message": "paddle not found "})
             }
         client.close()
         if result is None:
