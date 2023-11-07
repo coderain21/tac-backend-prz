1f3b648139ad7fa38527a2eba71a979b791f1d1d
shrinit <shrinit.poojary@7edge.com>
2023-11-07 11:17:29 +0530

    FF-1130 #time 2h Feature: Credit Card validation Descriptio: this api will call stripe functions to validate the credit card

--- services/buyers/handlers/credit_card.py
+++ services/buyers/handlers/credit_card.py
@@ -1,56 +1,59 @@
-# '''this function validates the '''
+# '''this function validates the credit card '''
 import json
 import stripe
 from pymongo import MongoClient
 import os
+from bson import ObjectId
 
 
-# # Initialize MongoDB connection
-client = MongoClient(os.environ['MONGO_CLIENT'])
-db = client[os.environ['DATABASE']]
-collection = db['dev-credit_card']
+headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': True,
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Methods': '*'
+}
 def credit_card(event, context):
     # # Parse the request body to get the token
     request_body = json.loads(event['body'])
     token = request_body['token']
-    # stripe.api_key = os.environ['STRIPE_API_KEY']
     stripe.api_key = 'sk_test_51Nb00kSFIyzeA4NAfngqSbNEuIJh7fjMnkvL7nxtAgbxc3Vncw7ZJde6BlMTAGVxDwJNL6j2h0b9MlUCsSxNybpx00HZ4PqRNu'
+    # stripe.api_key = 'sk_test_tR3PYbcVNZZ796tH88S4VQ2u'
+    data = event['queryStringParameters']
+    buyer_id= data.get('buyer_id')
+    buyer_id= ObjectId(buyer_id)
     try:
         # Create a customer
         print(22)
         customer = stripe.Customer.create(source=token)
         print(33,customer)
-
-        # Create a $1 charge for verification
-        charge = stripe.Charge.create(
-            amount=100,  # $1 in cents
+        payment_intent = stripe.PaymentIntent.create(
+            amount=100,  # Replace with the actual amount
             currency='usd',
-            customer=customer.id,
-            description='Card verification charge'
+            customer=customer.id,  # Replace with the customer ID
         )
-        print(1001, charge)
-        # Refund the $1 charge
-        # stripe.Refund.create(charge=charge.id)
-        # print(8888)
-
-        # If the card is valid, store the customer data in MongoDB
+        print(11111, payment_intent)
         customer_data = {
-            'customer_id': customer.id,
-            'email': customer.email,
-            'card_last4': token.card.last4,
-            'created_at': customer.created
+            'customer':customer,
+            'buyer_id': buyer_id
         }
+        client = MongoClient(os.environ['MONGO_CLIENT'])
+        db = client[os.environ['DATABASE']]
+        collection = db['dev-credit_card']
         print(22022,customer_data)
         collection.insert_one(customer_data)
-
+        client.close()
         response = {
             'statusCode': 200,
-            'body': json.dumps({'message': 'Card verified successfully and data stored'})
+            "headers": headers,
+            'body': json.dumps({'message': 'Card verified successfully and data stored',
+                                'payment_intent_id':payment_intent})
         }
     except Exception as e:
         print(e)
         response = {
             'statusCode': 500,
+            "headers": headers,
             'body': json.dumps({'message': 'Error verifying card'})
         }
 
