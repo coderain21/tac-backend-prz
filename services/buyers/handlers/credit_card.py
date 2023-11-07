d9425a1ec50708558ac1871328d0ff2964648f2d
shrinit <shrinit.poojary@7edge.com>
2023-11-07 19:09:30 +0530

    FF-1130 #time 2h 30m Feature: Credit Card validation Description: this api will call stripe functions to validate the credit card

--- services/buyers/handlers/credit_card.py
+++ services/buyers/handlers/credit_card.py
@@ -1,10 +1,8 @@
-# '''this function validates the credit card '''
 import json
 import stripe
 from pymongo import MongoClient
 import os
 from bson import ObjectId
-
 
 headers = {
     'Content-Type': 'application/json',
@@ -13,51 +11,70 @@ headers = {
     'Access-Control-Allow-Headers': '*',
     'Access-Control-Allow-Methods': '*'
 }
+
 def credit_card(event, context):
-    # # Parse the request body to get the token
+    # Parse the request body to get the token and buyer_id
     request_body = json.loads(event['body'])
     token = request_body['token']
-    stripe.api_key = 'sk_test_51Nb00kSFIyzeA4NAfngqSbNEuIJh7fjMnkvL7nxtAgbxc3Vncw7ZJde6BlMTAGVxDwJNL6j2h0b9MlUCsSxNybpx00HZ4PqRNu'
-    # stripe.api_key = 'sk_test_tR3PYbcVNZZ796tH88S4VQ2u'
     data = event['queryStringParameters']
-    buyer_id= data.get('buyer_id')
-    buyer_id= ObjectId(buyer_id)
+    buyer_id= data['buyer_id']
+    # Set your Stripe API key
+    stripe.api_key = 'sk_test_51Nb00kSFIyzeA4NAfngqSbNEuIJh7fjMnkvL7nxtAgbxc3Vncw7ZJde6BlMTAGVxDwJNL6j2h0b9MlUCsSxNybpx00HZ4PqRNu'
+
     try:
-        # Create a customer
-        print(22)
-        customer = stripe.Customer.create(source=token)
-        
-        customer['name']='Shrinit'
-        customer['address']='teyscberfa'
-        print(33,customer)
-        payment_intent = stripe.PaymentIntent.create(
-            amount=100,  # Replace with the actual amount
-            currency='usd',
-            customer=customer.id,# Replace with the customer ID
-            description='Card verification charge'
-        )
-        print(11111, payment_intent)
-        customer_data = {
-            'customer':customer,
-            'buyer_id': buyer_id
-        }
+        # Create a SetupIntent to confirm the PaymentMethod
         client = MongoClient(os.environ['MONGO_CLIENT'])
         db = client[os.environ['DATABASE']]
         collection = db['dev-credit_card']
-        print(22022,customer_data)
+        if 'status' in data:
+            if data['status'] == 'True':
+                result = collection.find_one({'buyer_id': buyer_id})
+                if result is not None:
+                    response = {
+                    'statusCode': 200,
+                    'headers': headers,
+                    'body': json.dumps({'result':result})
+                    }
+        payment_method = stripe.PaymentMethod.create(
+            type='card',
+            card={'token': token}
+        )
+        print(111111111111,payment_method)
+        setup_intent = stripe.SetupIntent.create(
+            payment_method=payment_method.id,
+            usage='off_session'  # or 'on_session' as needed
+        )
+        print(2222222, setup_intent)
+        customer = stripe.Customer.create(
+            payment_method=payment_method.id
+        )
+        
+        # Store the SetupIntent ID along with the buyer's ID
+        customer_data = {
+            'setup_intent_id': setup_intent.id,
+            'customer_id': customer.id,
+            'buyer_id': ObjectId(buyer_id)
+        }
         collection.insert_one(customer_data)
         client.close()
+
         response = {
             'statusCode': 200,
-            "headers": headers,
-            'body': json.dumps({'message': 'Card verified successfully and data stored',
-                                'payment_intent_id':payment_intent})
+            'headers': headers,
+            'body': json.dumps({'message': 'Card verification initiated successfully',
+                                'setup_intent_id': setup_intent.id})
+        }
+    except stripe.error.StripeError as e:
+        # Handle specific Stripe errors
+        response = {
+            'statusCode': 400,
+            'headers': headers,
+            'body': json.dumps({'message': 'Card verification failed: ' + str(e)})
         }
     except Exception as e:
-        print(e)
         response = {
             'statusCode': 500,
-            "headers": headers,
+            'headers': headers,
             'body': json.dumps({'message': 'Error verifying card'})
         }
 
