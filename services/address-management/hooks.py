8f08fc6476d0ca4352b12d29b88e8d3f9ff09e2a
shrinit <shrinit.poojary@7edge.com>
2023-11-16 11:27:56 +0530

    FF-1432 #time 1h #Feature: add shipping address and billing address Description: Implementation is completed api documentation left

--- services/address-management/hooks.py
+++ services/address-management/hooks.py
@@ -17,15 +17,14 @@ def skip_404_test_results(transaction):
         transaction['expected']['statusCode'] == '500' or
         transaction['expected']['statusCode'] == '404' or
         transaction['expected']['statusCode'] == '403' or
-        transaction['expected']['statusCode'] == '400' or
-        '/subdomain' in transaction['request']['uri']
+        transaction['expected']['statusCode'] == '400'
     ):
         transaction['skip'] = True
 
 
 @before_each
 def set_authorization(transaction):
-    token = str(os.environ.get('USER'))
+    token = str(os.environ.get('BUYERS'))
     print('s', transaction['expected']['statusCode'] == '400')
     transaction['request']['uri'] = urllib.parse.unquote(
         transaction['request']['uri'])
