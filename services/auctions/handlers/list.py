cb93918a21656088709e8055e8b15e594a323520
sarvesh <sarvesh.hugar@7edge.com>
2023-11-20 11:49:14 +0530

    FF-1377 #time 30m bug : Added the exportable keys in export function in list auctions API. Description: Added the keys time zone and registration type in export function for the list auctions API and tested it.

--- services/auctions/handlers/list.py
+++ services/auctions/handlers/list.py
@@ -256,11 +256,12 @@ def export_as_csv(auctions):
                 modified_auction["Auction ID"] = auction["auction_id"]
                 modified_auction["Auction Name"] = auction["title"]
                 modified_auction["Auction Description"] = re.sub(re.compile(r'<.*?>'), '', auction["description"])
+                modified_auction["Timezone"] = auction["time_zone"]
                 modified_auction["Auction Start Date"] = "" if auction["start_date"] is None or datetime.fromisoformat(str(auction["start_date"])).year == 1970 else datetime.fromisoformat(str(auction["start_date"])).strftime("%d %B %Y")
                 modified_auction["Auction Start Time"] = "" if auction['start_date'] is None or datetime.fromisoformat(str(auction["start_date"])).year == 1970 else datetime.fromisoformat(str(auction["start_date"])).strftime("%H:%M")
                 modified_auction["Auction End Date"] = "" if auction['end_date'] is None or datetime.fromisoformat(str(auction["end_date"])).year == 1970 else datetime.fromisoformat(str(auction["end_date"])).strftime("%d %B %Y")
                 modified_auction["Auction End Time"] = "" if auction['end_date'] is None or datetime.fromisoformat(str(auction["end_date"])).year == 1970 else datetime.fromisoformat(str(auction["end_date"])).strftime("%H:%M")
-
+                modified_auction["Registration Type"] = auction["registration_type"]
                 modified_auction["Currency"] = auction["currency"]
                 modified_auction["Extension Type"] = auction["extension_type"]
                 modified_auction["Extension mins"] = "" if len(auction["extension_time"]) == 0 else auction["extension_time"]+" minutes"
