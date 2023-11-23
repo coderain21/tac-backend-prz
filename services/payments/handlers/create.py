6e0158276fde5aad7c0027e99d336ae692cea593
sarvesh <sarvesh.hugar@7edge.com>
2023-11-23 14:03:24 +0530

    FF-1500 #time 1h 15m feature : created the function to take amount accountid and other parameters and generates secret. Description: Updated the code to fetcher seller data from id and added the code for generating stripe client secret before making the payment

--- services/payments/handlers/create.py
+++ services/payments/handlers/create.py
@@ -1,9 +1,11 @@
 '''this api will retrieve the detail of the buyers'''
 import json
 import os
+import stripe
 from pymongo import MongoClient
 from bson import ObjectId
 from lib.common_helper import Encoder
+from lib.get import get_by_email,fetch_seller_data_from_auction
 
 headers = {
     'Content-Type': 'application/json',
@@ -12,6 +14,19 @@ headers = {
     'Access-Control-Allow-Headers': '*',
     'Access-Control-Allow-Methods': '*'
 }
+stripe.api_key = "sk_test_51NSrthFdWS7wL4EMgIaIlyzCIPY2387pcfibXJdCWsVJWg1dHrjAHZIoeKTrOCNcUNqkAmEuGNQti3q0mcE3hThb00CCZpfg5S"
+def generate_client_secret(account_id,amount,currency,application_fee):
+    session  = stripe.PaymentIntent.create(
+        amount=amount,
+        currency=currency,
+        automatic_payment_methods={"enabled": True},
+        application_fee_amount=application_fee,
+        stripe_account=account_id,
+        )
+    print(session)
+    print(session['client_secret'])
+    return session['client_secret']
+
 
 def create_intent(event, context):
     try:
@@ -30,11 +45,25 @@ def create_intent(event, context):
                 "headers": headers,
                 "body": json.dumps({"message": "You do not have access to perform this API action"})
             }
+        
+        data = event['queryStringParameters']
+        expected_fields = ["id", "domain","amount"]
+        fields_not_found = list(set(expected_fields).difference(data.keys()))
+        if fields_not_found:
+            return {"headers": headers,
+                    'statusCode': 400,
+                    "body": json.dumps(
+                        {"message": f"Please provide {','.join(fields_not_found)}"})
+                    }
+        
         client = MongoClient(os.environ['MONGO_CLIENT'])
         db = client[os.environ['DATABASE']]
         collection = db[os.environ["BUYER_COLLECTION"]]
-        data = event['queryStringParameters']
-        
+        sub_domain = data.get("domain")
+        auction_id = data.get("id")
+        seller_data_of_auction = fetch_seller_data_from_auction(auction_id)
+        if seller_email
+        seller_data = get_by_email(seller_email,os.environ['SELLER_TABLE'])
         if result is None:
             return {
                 "statusCode": 404,
