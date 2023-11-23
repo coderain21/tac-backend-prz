10131c2beb98dae8578592624595e35c91375b4a
khaleel <ibrahim.khaleel@7edge.com>
2023-11-23 12:39:12 +0530

    CC-1849 #time 2h 30m feature: buyer add a lot to wishlist Implemented aggregation pipeline to query to different tables

--- services/buyer-wishlist/handlers/add-to-wishlist.py
+++ services/buyer-wishlist/handlers/add-to-wishlist.py
@@ -19,63 +19,68 @@ def create(event, context):
             email_address = cognito_data['email']
             if "cognito:groups" in cognito_data and not 'buyer' in cognito_data["cognito:groups"]:
                 return {
-                "statusCode": 403,
-                "headers": headers,
-                "body": json.dumps({"message": "You do not have access to perform this API action"})
-            }
+                    "statusCode": 403,
+                    "headers": headers,
+                    "body": json.dumps({"message": "You do not have access to perform this API action"})
+                }
         except:
             return {
                 "statusCode": 403,
                 "headers": headers,
                 "body": json.dumps({"message": "You do not have access to perform this API action"})
             }
+
         body = json.loads(event['body'])
         data = event['queryStringParameters']
         lot = data['lot_id']
-        lot_id= ObjectId(lot)
+        lot_id = ObjectId(lot)
         client = MongoClient(os.environ['MONGO_CLIENT'])
         db = client[os.environ['DATABASE']]
         lot_collection = db[os.environ["LOT_COLLECTION_NAME"]]
-        buyer_collection = db[os.environ['BUYER_COLLECTION']]
-        wish_list= db[os.environ['BUYER_WISHLIST_TABLE_NAME']]
-        lot_detail = lot_collection.find_one({'_id':lot_id})
-        buyer= buyer_collection.find_one({'email_address':email_address})
-        if 'fav_lots' in buyer:
-            fav = buyer['fav_lot']
-            if lot not in fav:
-                fav.append(lot)
-                buyer_collection.update_one({'email_address':email_address},{'$set':{'fav_lots':fav}})
-            else:
-                return {
-                    "statusCode": 400,
-                    "headers": headers,
-                    "body": json.dumps({"message": "Lot has already exists"})
-                }
-            
-        else:
-            fav = []
-            fav.append(lot)
-            buyer_collection.update_one({'email_address':email_address},{'$set':{'fav_lots':fav}})
-        seller_email= lot_detail['seller_email']
+        # buyer_collection = db[os.environ['BUYER_COLLECTION']]
+        wish_list = db[os.environ['BUYER_WISHLIST_TABLE_NAME']]
+        lot_detail = lot_collection.find_one({'_id': lot_id})
+        seller_email = lot_detail['seller_email']
         auction_name = body['auction_name']
-        insert_data= {
+        insert_data = {
             'seller_email': seller_email,
-            'lot_id':lot,
+            'lot_id': str(lot_id),  # Convert ObjectId to string for consistency
             'email_address': email_address,
             'auction_name': auction_name,
             'auction_id': lot_detail['auction_id']
         }
+
         wish_list.insert_one(insert_data)
+
+        # Find lots with the same seller email and check if they are present in the wishlist
+        similar_lots = lot_collection.aggregate([
+            {"$match": {"seller_email": seller_email}},
+            {"$lookup": {
+                "from": wish_list,
+                "localField": "_id",
+                "foreignField": "lot_id",
+                "as": "wishlist"
+            }},
+            {"$addFields": {
+                "is_wishlisted": {"$gt": [{"$size": "$wishlist"}, 0]}
+            }},
+            {"$project": {"wishlist": 0}}
+        ])
+        
+        # Convert the aggregation result to a list for JSON serialization
+        similar_lots = list(similar_lots)
+
         client.close()
         return {
-                "statusCode": 200,
-                "headers": headers,
-                "body": json.dumps({"message": "Lot added to wishlist successfully"})
-            }
+            "statusCode": 200,
+            "headers": headers,
+            "body": json.dumps({"message": "Lot added to wishlist successfully", "similar_lots": similar_lots})
+        }
+
     except Exception as e:
         print(str(e))
         return {
             "statusCode": 500,
             'headers': headers,
-            "body": json.dumps({"error": str(e)})
+            "body": json.dumps({"error": "Internal server error"})
         }
