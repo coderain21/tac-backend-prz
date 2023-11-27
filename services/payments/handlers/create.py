91ff59a480406c0945742a2d41e16a80a39d4292
sarvesh <sarvesh.hugar@7edge.com>
2023-11-27 11:41:17 +0530

    FF-1500 #time 2h feature : Added the functionality to calculate application fee,generate payment intent and save payment data. Description: Added the functions to calculate application fee based on seller plan , generate payment intent and return client secret, and save payment data in the database to use it for payment history and later used for webhook.

--- services/payments/handlers/create.py
+++ services/payments/handlers/create.py
@@ -16,21 +16,56 @@ headers = {
 }
 stripe.api_key = os.environ["STRIPE_API_KEY"]
 
-def calculate_application_fee():
-    pass
+def calculate_application_fee(amount,plan_type):
+    if plan_type == "Starter":
+        application_fee = amount * 0.06
+    elif plan_type == "Free":
+        application_fee = amount * 0
+    elif plan_type == "Pro":
+        application_fee = amount * 0.09
+    else:
+        application_fee = amount * 0
+    return application_fee
 
 def generate_client_secret(account_id,amount,currency,application_fee):
-    session  = stripe.PaymentIntent.create(
-        amount=amount,
-        currency=currency,
-        automatic_payment_methods={"enabled": True},
-        application_fee_amount=application_fee,
-        stripe_account=account_id,
-        )
-    print(session)
-    print(session['client_secret'])
-    return session['client_secret']
+    try:
+        session  = stripe.PaymentIntent.create(
+            amount=amount,
+            currency=currency,
+            automatic_payment_methods={"enabled": True},
+            application_fee_amount=application_fee,
+            stripe_account=account_id,
+            )
+        print(session)
+        return session
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        raise
 
+def add_payment_data_to_collection(client_secret,id,status,email_address,application_amount,currency):
+    try:
+        # MongoDB configuration
+        client = MongoClient(os.environ['MONGO_CLIENT'])
+        db = client[os.environ['DATABASE']]
+        collection_sellers = db['PAYMENT_COLLECTION']
+        insert_data = {
+            "email_address" : email_address,
+            "id": id,
+            "client_secret": client_secret,
+            "status": status,
+            "application_amount": application_amount,
+            "currency": currency
+        }
+        insert_result = collection_sellers.insert_one(insert_data)
+        
+        client.close()
+        if insert_result:
+            return insert_result
+        return None
+    except BaseException as err:
+        client.close()
+        print(f"Unexpected {err=}, {type(err)=}")
+        raise
 
 def create_intent(event, context):
     try:
@@ -78,6 +113,7 @@ def create_intent(event, context):
                 "headers": headers,
                 "body": json.dumps({"message": "Seller not found"})
             }
+        plan_type = seller_data.get("plan_type","")
         account_id = seller_data["stripe_connected_id"]
         account_status = seller_data.get("stripe_status","")
         if account_status!="connected":
@@ -86,16 +122,18 @@ def create_intent(event, context):
             "headers": headers,
             "body": json.dumps({'message':'Seller has disconnected their stripe account,please connect'},cls=Encoder)
             }
-        application_fee = calculate_application_fee()
-        client_secret = generate_client_secret(account_id,amount,seller_data_of_auction["currency"],application_fee)
+        application_fee = calculate_application_fee(amount,plan_type)
+        stripe_data = generate_client_secret(account_id,amount,seller_data_of_auction["currency"],application_fee)
+        add_payment_data_to_collection(stripe_data["client_secret"],stripe_data["id"],stripe_data["status"],email_address,stripe_data["application_amount"],stripe_data["currency"])
         return {
             "statusCode": 200,
             "headers": headers,
-            "body": json.dumps({'data':client_secret},cls=Encoder)
+            "body": json.dumps({'data':stripe_data["client_secret"],'account_id':account_id},cls=Encoder)
             }
-    except Exception as e:
+    except Exception as err:
+        print(err)
         return {
             "statusCode": 500,
             "headers": headers,
-            "body": json.dumps({"message": e}, cls=Encoder)
+            "body": json.dumps({"message": "There was an error while generating payment data"})
             }
