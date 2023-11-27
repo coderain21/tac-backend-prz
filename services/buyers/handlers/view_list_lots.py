626590c087e39b70db26fa8d3f22179d1b582ce8
khaleel <ibrahim.khaleel@7edge.com>
2023-11-27 19:06:07 +0530

    CC-1849 #time 2h 30m feature: buyer add a lot to wishlist Implemented the api and started doing api documentation

--- services/buyers/handlers/view_list_lots.py
+++ services/buyers/handlers/view_list_lots.py
@@ -1,252 +1,3 @@
-# """This module is used to view the lots with auction id"""
-# import json
-# import os
-# from pymongo import MongoClient
-# from bson import ObjectId
-# from lib.common_helper import Encoder
-# import re
-# headers = {
-#     'Content-Type': 'application/json',
-#     'Access-Control-Allow-Origin': '*',
-#     'Access-Control-Allow-Credentials': True,
-#     'Access-Control-Allow-Headers': '*',
-#     'Access-Control-Allow-Methods': '*'
-# }
-# def prepend_backslash(text):
-#     # Define a regular expression pattern to match special characters
-#     special_chars_pattern = re.compile(r'([\\.*+?()|[\]{}^$])')
-
-#     # Use re.sub to replace each match with a backslash followed by the matched character
-#     modified_text = re.sub(special_chars_pattern, r'\\\1', text)
-
-#     return modified_text
-
-# def view_list_lots(event, context):
-#     """
-#     The function `view_list_lots` retrieves a list of lots from a MongoDB database based on the provided
-#     auction ID and seller email, and allows sorting the lots by different parameters.
-
-#     :param event: The `event` parameter is a dictionary that contains information about the event that
-#     triggered the function. In this case, it is expected to have a key called 'queryStringParameters'
-#     which contains the query parameters passed to the function
-#     :param context: The `context` parameter is an object that provides information about the runtime
-#     environment of the function. It includes details such as the AWS request ID, function name, and
-#     other metadata. In this code, the `context` parameter is not used
-#     :return: a JSON response with a status code, headers, and a body. The body contains either a list of
-#     lots or an error message.
-#     """
-#     try:
-#         data = event['queryStringParameters']
-#         if data is None or "auction_id" not in data:
-#             return {
-#                 "statusCode": 400,
-#                 "headers": headers,
-#                 "body": json.dumps({"message": "Please provide auction_id"})
-#             }
-#         client = MongoClient(os.environ['MONGO_CLIENT'])
-#         db = client[os.environ['DATABASE']]
-#         collection = db[os.environ["AUCTION_MONGODB_COLLECTION_NAME"]]
-#         auction_id = data.get("auction_id")
-#         if auction_id is not None:
-#             _id = ObjectId(auction_id)
-#         projection = {
-#             "_id": 1,
-#             "auction_id": 1,
-#             "seller_email": 1
-#         }
-#         result = collection.find_one({"_id": _id}, projection)
-#         client = MongoClient(os.environ['MONGO_CLIENT'])
-#         db = client[os.environ['DATABASE']]
-#         lot_collection = db[os.environ["LOT_COLLECTION_NAME"]]
-#         seller_email = result['seller_email']
-#         auction_id = result['auction_id']
-#         escaped_search_keyword = ""
-#         search_keyword = data.get('search')
-#         if search_keyword:
-#             escaped_search_keyword = prepend_backslash(search_keyword)
-#             print(f"Escaped search keyword: {escaped_search_keyword}")
-#             search_criteria = {
-#                 "$or": [
-#                     {"title1": {"$regex": f".*{escaped_search_keyword}.*", "$options": "i"}},
-#                     {"tags": {"$elemMatch": {"$regex": f".*{escaped_search_keyword}.*", "$options": "i"}}}
-#                 ]
-#             }
-#         print(f"Search criteria: {search_criteria}")
-
-#         # Print the find_one result for debugging
-#         find_result = lot_collection.find({"auction_id": auction_id, 'seller_email': seller_email, **search_criteria})
-#         print("find result:", list(find_result))
-#         search_result = lot_collection.aggregate([
-#             {"$match": {
-#                 "auction_id": auction_id,
-#                 'seller_email': seller_email,
-#                 "$or": [
-#                     {"title1": {"$regex": f'^{escaped_search_keyword}$', "$options": "i"}},
-#                     {"tags": {"$regex": f'.*{escaped_search_keyword}.*', "$options": "i"}}
-#                 ],
-#                 "tags": {"$ne": []}  # Exclude documents where tags is empty
-#             }},
-#             {"$lookup": {
-#                 "from": os.environ['BUYER_WISHLIST_TABLE_NAME'],
-#                 "localField": "_id",
-#                 "foreignField": "lot_id",
-#                 "as": "wishlist"
-#             }},
-#             {"$addFields": {
-#                 "is_wishlisted": {
-#                     "$cond": {
-#                         "if": {
-#                             "$in": ["$_id", "$wishlist.lot_id"]
-#                         },
-#                         "then": True,
-#                         "else": False
-#                     }
-#                 }
-#             }},
-#             {"$project": {"wishlist": 0}},  # Remove the wishlist field from the result
-#             {"$sort": {"lot_number": 1}}  # 1 for ascending order, -1 for descending order
-#         ])
-#         print('-------------------------------------------------')
-#         print("First aggregation result:", list(search_result))
-#         sort_param = data.get("sort_by", "")
-#         if sort_param == "highest_price":
-#             search_result = lot_collection.aggregate([
-#                 {"$match": {"auction_id": auction_id, 'seller_email': seller_email}},
-#                 {"$lookup": {
-#                     "from": os.environ['BUYER_WISHLIST_TABLE_NAME'],
-#                     "localField": "_id",
-#                     "foreignField": "lot_id",
-#                     "as": "wishlist"
-#                 }},
-#                 {"$addFields": {
-#                     "is_wishlisted": {
-#                         "$cond": {
-#                             "if": {
-#                                 "$in": ["$_id", "$wishlist.lot_id"]
-#                             },
-#                             "then": True,
-#                             "else": False
-#                         }
-#                     }
-#                 }},
-#                 {"$project": {"wishlist": 0}},  # Remove the wishlist field from the result
-#                 {"$sort": {"starting_price": -1}}  # Sort by starting_price in descending order
-#             ])
-#         elif sort_param == "lowest_price":
-#             search_result = lot_collection.aggregate([
-#                 {"$match": {"auction_id": auction_id, 'seller_email': seller_email}},
-#                 {"$lookup": {
-#                     "from": os.environ['BUYER_WISHLIST_TABLE_NAME'],
-#                     "localField": "_id",
-#                     "foreignField": "lot_id",
-#                     "as": "wishlist"
-#                 }},
-#                 {"$addFields": {
-#                     "is_wishlisted": {
-#                         "$cond": {
-#                             "if": {
-#                                 "$in": ["$_id", "$wishlist.lot_id"]
-#                             },
-#                             "then": True,
-#                             "else": False
-#                         }
-#                     }
-#                 }},
-#                 {"$project": {"wishlist": 0}},  # Remove the wishlist field from the result
-#                 {"$sort": {"starting_price": 1}}  # Sort by starting_price in ascending order
-#             ])
-#         elif sort_param == "highest_bid":
-#             search_result = lot_collection.aggregate([
-#                 {"$match": {"auction_id": auction_id, 'seller_email': seller_email}},
-#                 {"$lookup": {
-#                     "from": os.environ['BUYER_WISHLIST_TABLE_NAME'],
-#                     "localField": "_id",
-#                     "foreignField": "lot_id",
-#                     "as": "wishlist"
-#                 }},
-#                 {"$addFields": {
-#                     "is_wishlisted": {
-#                         "$cond": {
-#                             "if": {
-#                                 "$in": ["$_id", "$wishlist.lot_id"]
-#                             },
-#                             "then": True,
-#                             "else": False
-#                         }
-#                     }
-#                 }},
-#                 {"$project": {"wishlist": 0}},  # Remove the wishlist field from the result
-#                 {"$sort": {"current_bid": -1}}  # Sort by current_bid in descending order
-#             ])
-#         elif sort_param == "lowest_bid":
-#             search_result = lot_collection.aggregate([
-#                 {"$match": {"auction_id": auction_id, 'seller_email': seller_email}},
-#                 {"$lookup": {
-#                     "from": os.environ['BUYER_WISHLIST_TABLE_NAME'],
-#                     "localField": "_id",
-#                     "foreignField": "lot_id",
-#                     "as": "wishlist"
-#                 }},
-#                 {"$addFields": {
-#                     "is_wishlisted": {
-#                         "$cond": {
-#                             "if": {
-#                                 "$in": ["$_id", "$wishlist.lot_id"]
-#                             },
-#                             "then": True,
-#                             "else": False
-#                         }
-#                     }
-#                 }},
-#                 {"$project": {"wishlist": 0}},  # Remove the wishlist field from the result
-#                 {"$sort": {"current_bid": 1}}  # Sort by current_bid in ascending order
-#             ])
-#         else:
-#             search_result = lot_collection.aggregate([
-#                 {"$match": {"auction_id": auction_id, 'seller_email': seller_email}},
-#                 {"$lookup": {
-#                     "from": os.environ['BUYER_WISHLIST_TABLE_NAME'],
-#                     "localField": "_id",
-#                     "foreignField": "lot_id",
-#                     "as": "wishlist"
-#                 }},
-#                 {"$addFields": {
-#                     "is_wishlisted": {
-#                         "$cond": {
-#                             "if": {
-#                                 "$in": ["$_id", "$wishlist.lot_id"]
-#                             },
-#                             "then": True,
-#                             "else": False
-#                         }
-#                     }
-#                 }},
-#                 {"$project": {"wishlist": 0}},  # Remove the wishlist field from the result
-#                 {"$sort": {"lot_number": 1}}  # Sort by lot_number in ascending order
-#             ])
-#         lots_list = list(search_result)
-#         print('lot list---------------------\n', lots_list)
-#         return {
-#             "statusCode": 200,
-#             "headers": headers,
-#             "body": json.dumps({'data': lots_list}, cls=Encoder)
-#         }
-
-#     except Exception as e:
-#         print(str(e))
-#         return {
-#             "statusCode": 500,
-#             "headers": headers,
-#             "body": json.dumps({"message": str(e)})
-#         }
-
-
-
-
-
-
-
-
 import json
 import os
 from pymongo import MongoClient
@@ -282,6 +33,17 @@ def get_lots(auction_id, seller_email, search_keyword, sort_param):
         ]
     }
 
+    if sort_param == "highest_price":
+        sort_field, sort_order = "starting_price", -1
+    elif sort_param == "lowest_price":
+        sort_field, sort_order = "starting_price", 1
+    elif sort_param == "highest_bid":
+        sort_field, sort_order = "current_bid", -1
+    elif sort_param == "lowest_bid":
+        sort_field, sort_order = "current_bid", 1
+    else:
+        sort_field, sort_order = "lot_number", 1
+
     aggregation_pipeline = [
         {"$match": {"auction_id": auction_id, 'seller_email': seller_email, **search_criteria}},
         {"$lookup": {
@@ -299,15 +61,11 @@ def get_lots(auction_id, seller_email, search_keyword, sort_param):
                 }
             }
         }},
-        {"$project": {"wishlist": 0}},
-        {"$sort": {"lot_number": 1}}  # Default sorting by lot_number
+        {"$project": {"wishlist": 0}}# Default sorting by lot_number
     ]
 
-    if sort_param:
-        sort_field = sort_param.rstrip('_asc').rstrip('_desc')
-        sort_order = -1 if sort_param.endswith("_desc") else 1
-        sort_stage = {"$sort": {sort_field: sort_order}}
-        aggregation_pipeline.append(sort_stage)
+    sort_stage = {"$sort": {sort_field: sort_order}}
+    aggregation_pipeline.append(sort_stage)
 
     search_result = lot_collection.aggregate(aggregation_pipeline)
 
--- services/buyers/handlers/view_lots.py
+++ services/buyers/handlers/view_lots.py
@@ -1,4 +1,3 @@
-'''this api will list the detail of the lot id passed in the parameter'''
 import json
 import os
 from pymongo import MongoClient
@@ -14,20 +13,6 @@ headers = {
 }
 
 def lot_details(event, context):
-    """
-    The function `list_lots` retrieves details of a lot from a MongoDB database based on the provided
-    lot_id.
-
-    :param event: The `event` parameter is a dictionary that contains information about the event that
-    triggered the function. In this case, it is expected to have a key called `'queryStringParameters'`
-    which contains the query parameters passed to the function
-    :param context: The `context` parameter is an object that provides information about the runtime
-    environment of the function. It includes details such as the AWS request ID, function name, and
-    other contextual information. In this code, the `context` parameter is not used, but it is included
-    in the function signature for completeness
-    :return: The code is returning a JSON response with a status code, headers, and a body. The specific
-    response depends on the execution path of the code.
-    """
     try:
         # Parse query parameters from the event
         data = event['queryStringParameters']
@@ -37,32 +22,47 @@ def lot_details(event, context):
                 "headers": headers,
                 "body": json.dumps({"message": "Please provide lot_id"})
             }
-        lot_id= data['lot_id']
-        lot_id= ObjectId(lot_id)
+
+        lot_id = ObjectId(data['lot_id'])
+        buyer_email = data.get('buyer_email')  # Check if buyer_email is provided
+
         client = MongoClient(os.environ['MONGO_CLIENT'])
         db = client[os.environ['DATABASE']]
         collection = db[os.environ["LOT_COLLECTION_NAME"]]
-        auction=db[os.environ["AUCTION_MONGODB_COLLECTION_NAME"]]
-        result= collection.find_one({'_id': lot_id})
-        auction_details= auction.find_one({'auction_id': result['auction_id'],
-                                            'seller_email': result['seller_email']},
-                                          {'faq':1,'terms_and_condition':1,'menu_links':1,'font':1})
-        result['faq']=auction_details['faq']
-        result['font']=auction_details['font']
-        result['terms_and_condition']= auction_details['terms_and_condition']
-        result['menu_links']= auction_details['menu_links']
+        wishlist_collection = db[os.environ["BUYER_WISHLIST_TABLE_NAME"]]
+        auction = db[os.environ["AUCTION_MONGODB_COLLECTION_NAME"]]
+
+        result = collection.find_one({'_id': lot_id})
+        
+        # Check if buyer_email is provided in the query parameters
+        if buyer_email:
+            wishlist_result = wishlist_collection.find_one({'lot_id': lot_id, 'seller_email': result['seller_email'], 'email_address': buyer_email})
+            result['is_wishlisted'] = wishlist_result is not None
+        else:
+            result['is_wishlisted'] = False
+
+        auction_details = auction.find_one({'auction_id': result['auction_id'], 'seller_email': result['seller_email']},
+                                          {'faq': 1, 'terms_and_condition': 1, 'menu_links': 1, 'font': 1})
+        result['faq'] = auction_details['faq']
+        result['font'] = auction_details['font']
+        result['terms_and_condition'] = auction_details['terms_and_condition']
+        result['menu_links'] = auction_details['menu_links']
+
         client.close()
+
         if result is None:
             return {
                 "statusCode": 404,
                 "headers": headers,
                 "body": json.dumps({"message": "Lot not found"})
             }
+
         return {
             'headers': headers,
             "statusCode": 200,
-            "body": json.dumps({'data':result},cls=Encoder)
+            "body": json.dumps({'data': result}, cls=Encoder)
         }
+
     except Exception as e:
         return {
             "statusCode": 500,
