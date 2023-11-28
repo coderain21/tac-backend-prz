541599d1d3cdd75ac39f032f29584d3fc56c9c1a
Namratha Shettiagar <namratha.shettigar@7edge.com>
2023-11-28 17:49:17 +0530

    TST-01 #time 5m checking danger for all the commits

--- dangerfile.js
+++ dangerfile.js
@@ -49,6 +49,7 @@ if (shouldSkipLoop) {
 const branchNameRegex = /^(feature|bugfix|hotfix|chore|refactor|documentation|style|test|performance|ci|build|revert)\/\d+(\.\d+)?\/[a-zA-Z0-9-]+$/
 
 
+
 // Get the branch name from the BITBUCKET_BRANCH environment variable
 const branchName = process.env.BITBUCKET_BRANCH;
 console.log(branchName,"branch")
