27728389f93a2eb71be33dabb5f72107d6f290d9
khaleel <ibrahim.khaleel@7edge.com>
2023-12-04 18:01:47 +0530

    CC-1905 #time 2h 30m feature: seller export the lot list Implemented the export functionality in the view lot api

--- services/auctions/handlers/view_lots.py
+++ services/auctions/handlers/view_lots.py
@@ -1,8 +1,12 @@
 '''The `import os` statement is importing the `os` module in Python'''
 import os
 import json
+from bson import ObjectId
 import pymongo
 import re
+import csv
+import tempfile
+import boto3
 from lib.common_helper import Encoder
 
 headers = {
@@ -52,6 +56,9 @@ def list_lots(event, context):
         limit = int(event['queryStringParameters'].get(
             'per_page', '200'))  # Number of records per page
 
+        export = event['queryStringParameters'].get('export', False)
+        download_link = None
+
         client = pymongo.MongoClient(os.environ['MONGO_CLIENT'])
         db = client[os.environ['DATABASE']]
         collection = db[os.environ["LOT_COLLECTION_NAME"]]
@@ -79,7 +86,7 @@ def list_lots(event, context):
                     sort(sort_criteria).skip((page-1)*limit).limit(limit))
         total_documents = collection.count_documents(query)
         total_lots = collection.count_documents({"seller_email": seller_email, "auction_id": auction_id})
-        client.close()
+        
 
         body = {
             "data": lots,
@@ -88,6 +95,11 @@ def list_lots(event, context):
             "current_page": page,
             "total_pages": (total_documents + limit - 1) // limit
         }
+        if export:
+            download_link = export_lots_as_csv(lots, db)
+        if download_link is not None:
+            body["csv_url"] = download_link
+        client.close()
         return {
             'headers': headers,
             "statusCode": 200,
@@ -99,3 +111,70 @@ def list_lots(event, context):
             'headers': headers,
             "body": json.dumps({"error": str(e)})
         }
+
+
+def export_lots_as_csv(lots, db):
+    try:
+        auction_id = str(lots[0].get('auction_id', ''))
+        filename = auction_id
+        auction_collection = db[os.environ['AUCTION_MONGODB_COLLECTION_NAME']]
+        seller_email = lots[0]["seller_email"]
+        auction_status = auction_collection.find_one({
+            "seller_email": seller_email,
+            "auction_id": auction_id
+        }, {"status": 1})
+        # Use a temporary directory
+        temp_dir = tempfile.mkdtemp()
+        csv_file_path = os.path.join(temp_dir, f'{filename}_lots.csv')
+
+        s3_key = f"exports/lots/{auction_id}/{filename}_lots.csv"
+        s3_bucket = os.environ['S3_BUCKET']
+        print('Lots details------------', lots)
+        
+        with open(csv_file_path, "w") as file:
+            writer = csv.DictWriter(file, [
+                "Total Current Bid", "Total Bids", "Active Bidders", "Lot Number",
+                "Thumbnail URL", "Title", "Paddle Number", "Top Bidder", "Status(Selling, No Bids)", "Top Bid"
+            ])
+            writer.writeheader()
+            
+            for lot in lots:
+                lot_images = lot.get("images", [])
+                featured_image = next((img["url"] for img in lot_images if img.get("featured")), None)
+                thumbnail_url = featured_image or ""
+                writer.writerow({
+                    "Total Current Bid": lot.get("current_bid", ""),
+                    "Total Bids": lot.get("total_bids", ""),
+                    "Active Bidders": lot.get("active_bidders", ""),
+                    "Lot Number": lot.get("lot_number", ""),
+                    "Thumbnail URL": thumbnail_url,
+                    "Title": lot.get("title1", ""),
+                    "Paddle Number": lot.get("paddle_number", ""),
+                    "Top Bidder": lot.get("top_bidder", ""),
+                    "Status": auction_status['status'],
+                    "Top Bid": lot.get("top_bid", "")
+                })
+
+        # Upload the file to S3
+        s3_client = boto3.client("s3", region_name='eu-west-2')
+        s3_client.upload_file(csv_file_path, s3_bucket, s3_key)
+
+        # Ensure that the file is made public
+        s3_resource = boto3.resource("s3", region_name='eu-west-2')
+        object_acl = s3_resource.ObjectAcl(s3_bucket, s3_key)
+        object_acl.put(ACL="public-read")
+
+        # Generate a presigned URL
+        s3_signed_url = s3_client.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": s3_bucket, "Key": s3_key},
+            ExpiresIn=3600,
+        )
+
+        # print("CSV file uploaded successfully.")
+        # print("Presigned URL:", s3_signed_url)
+
+        return s3_signed_url
+    except Exception as err:
+        print("Error:", err)
+        return None
