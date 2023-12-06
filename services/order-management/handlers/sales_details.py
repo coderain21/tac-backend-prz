64458efcac95eb53e310cbb59913e8cc5a29a70f
shrinit <shrinit.poojary@7edge.com>
2023-12-06 15:44:13 +0530

    FF-1475 #time 3h feature: view sales details implemented sales deails api

---
+++ services/order-management/handlers/sales_details.py
@@ -0,0 +1,84 @@
+'''this api will list all the lots'''
+import json
+import os
+from pymongo import MongoClient
+from lib.common_helper import Encoder
+from bson import ObjectId
+
+headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': True,
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Methods': '*'
+}
+def sales_details(event, context):
+    """
+    The above function is a Python code that retrieves cart details for a specific auction from a
+    MongoDB database, based on the user's email address.
+    :param event: The `event` parameter is a dictionary that contains information about the event that
+    triggered the function. It typically includes details such as the HTTP request, headers, and query
+    parameters
+    :param context: The `context` parameter is an object that provides information about the runtime
+    environment of the function. It includes details such as the AWS request ID, function name, and
+    other contextual information. In this code snippet, the `context` parameter is not used
+    :return: The code is returning a response object with a status code, headers, and a body. The body
+    contains a JSON object with a "data" key, which holds the cart details.
+    """
+    try:
+        # try:
+        #     seller_email = event['requestContext']['authorizer']['claims']['email']
+        #     print('email ', seller_email)
+        #     if ("cognito:groups" in event['requestContext']['authorizer']['claims'] and not
+        #             'seller' in event['requestContext']['authorizer']['claims']["cognito:groups"]):
+        #         return {
+        #             "statusCode": 403,
+        #             "headers": headers,
+        #             "body": json.dumps({"message": "do not have access to perform this API action"})
+        #         }
+        # except:
+        #     return {
+        #         "statusCode": 403,
+        #         "headers": headers,
+        #         "body": json.dumps({"message": "You do not have access to perform this API action"})
+        #     }
+        seller_email= 'anusha.k+subdomain@7edge.com'
+        projection={
+            'order_number':1,
+            'created_at':1,
+            'auction_title':1,
+            'purchases':1,
+            'payment_method_types':1,
+            'payment_status':1,
+            'amount':1,
+            'payment_intent':1,
+            'email_address':1,
+            'status':1,
+            "billing_address":1,
+            "shipping_address":1,
+            'seller_email':1
+        }
+        client = MongoClient(os.environ['MONGO_CLIENT'])
+        db = client[os.environ['DATABASE']]
+        collection = db[os.environ['ORDERS_COLLECTION']]
+        data = event['queryStringParameters']
+        order_id = data['order_id']
+        order_data = collection.find_one({'_id':ObjectId(order_id),'seller_email':seller_email},projection)
+        if order_data is None:
+            return {
+                "statusCode": 404,
+                "headers": headers,
+                "body": json.dumps({"message": "No orders found"})
+            }
+        return {
+                "statusCode": 200,
+                "headers": headers,
+                "body": json.dumps({"data":order_data},cls = Encoder)
+            }
+    except Exception as err:
+        print(err)
+        return {
+            "headers": headers,
+            "statusCode": 500,
+            "body": json.dumps({"message": "There was an error "})
+        }
