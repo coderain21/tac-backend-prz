42ef85def4e24f5211f1db17cf6d82a401908dfe
sandhyashri <sandhyashri@7edge.com>
2023-12-07 14:56:05 +0530

    FF-1193 #time 5h #comment feature: bid for auction Implemented step fucntions to handle the extension usecase

--- services/web-socket/src/utilities/stop_step_function.js
+++ services/web-socket/src/utilities/stop_step_function.js
@@ -1,29 +1,11 @@
 const { StepFunctions } = require('aws-sdk')
+const mongodbHelper = require('./mongodb_helper')
 
-module.exports.getExecutionArn = async (currentLotDetails) => {
-    try {
-        console.log('currentLotDetails', currentLotDetails)
-        const connectionData = await this.connect()
-        const database = connectionData.connection.db// Access the database
-        const collection = database.collection('dev-buyers') // Replace with your collection name
-        const query = {
-            lot_id: currentLotDetails.lot_id, // Replace 'excluded_buyer_id' with the buyer_id you want to exclude
-            seller_email: currentLotDetails.seller_email,
-        } // Corrected 'document.buyer_id'
-        console.log('seller', query)
-        const documents = await collection.find(query).toArray() // Await the query result
-        connectionData.disconnect()
-        return documents
-    } catch (err) {
-        console.log(err)
-        return false
-    }
-}
 
 module.exports.stopExecution = async (currentLotDetails) => {
     try {
         const stepFunctions = new StepFunctions()
-        const getArn = await this.getExecutionArn(currentLotDetails)
+        const getArn = await mongodbHelper.getExecutionArn(currentLotDetails)
         console.log('getarn', getArn)
         const executionArn = getArn[0].arn
         const response = await stepFunctions.stopExecution({
