ac041bf26d89fd73ea6c20865eb19f01ccbe3151
sandhyashri <sandhyashri@7edge.com>
2023-12-07 15:37:44 +0530

    FF-1193 #time 5h #comment feature: bid for auction Implemented step fucntions to handle the extension usecase

--- services/web-socket/src/utilities/mongodb_helper.js
+++ services/web-socket/src/utilities/mongodb_helper.js
@@ -365,9 +365,9 @@ module.exports.getExecutionArn = async (currentLotDetails) => {
         console.log('getExecutionArn mongo', currentLotDetails)
         const connectionData = await this.connect()
         const database = connectionData.connection.db// Access the database
-        const collection = database.collection('dev-buyers') // Replace with your collection name
+        const collection = database.collection('dev-step-function-arns') // Replace with your collection name
         const query = {
-            lot_id: currentLotDetails.lot_id, // Replace 'excluded_buyer_id' with the buyer_id you want to exclude
+            lot_id: currentLotDetails._id, // Replace 'excluded_buyer_id' with the buyer_id you want to exclude
             seller_email: currentLotDetails.seller_email,
         } // Corrected 'document.buyer_id'
         console.log('seller', query)
