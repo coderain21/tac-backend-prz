f72360ed433efcee5d50ed19a91efa1e9e51214b
sandhyashri <sandhyashri@7edge.com>
2023-12-14 12:06:34 +0530

    FF-1193 #time 30m  #comment feature: bid for auction Fixed eslint issue

--- services/web-socket/src/config/beta.js
+++ services/web-socket/src/config/beta.js
@@ -1,4 +1,4 @@
 module.exports = {
-    FCMKEY: 'AAAAQl0eWgA:APA91bGPlN0ph4ghLm6n16FM23ONZYsbTATSN9lqZkcDlHVv0YAsMMBOHyiRYBFwNwq3YQDHOsn72zRFevi6po9Mepdxdwusc7x_aYMtUxe5yDhVuyhx01q7MunsHM1kAKe1ORbjYjUK',    
+    FCMKEY: 'AAAAQl0eWgA:APA91bGPlN0ph4ghLm6n16FM23ONZYsbTATSN9lqZkcDlHVv0YAsMMBOHyiRYBFwNwq3YQDHOsn72zRFevi6po9Mepdxdwusc7x_aYMtUxe5yDhVuyhx01q7MunsHM1kAKe1ORbjYjUK',
     mongoDBConnectionString: 'mongodb://devAdmin:petsrwise@3.217.180.16:3000:27017/petsrwise-beta?authSource=admin',
 }
