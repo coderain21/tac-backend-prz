1aa953ad74823dbcb55649ccc7c8282101868019
Namratha Shettiagar <namratha.shettigar@7edge.com>
2023-12-14 15:24:01 +0530

    FF-1193 #time 2m feature: removed redundant console from danger file

--- dangerfile.js
+++ dangerfile.js
@@ -4,7 +4,6 @@ import { danger, warn, fail } from "danger"
 const { execSync } = require("child_process");
 // const prDescription = danger.bitbucket_cloud.pr.description;
 const commits = danger.bitbucket_cloud.commits;
-console.log(commits, "commits")
 const cardNumberRegex = /\b[A-Za-z]+-\d+\b/;
 const timeTagRegex = /#time \d+[hmdw]*/;
 const Branchtype = /(feature:|bugfix:|hotfix:|chore:|refactor:|documentation:|style:|test:|performance:|ci:|build:|revert:)/;
@@ -54,7 +53,7 @@ const branchNameRegex = /\/(\d+\.\d+)\//;
 
 // Get the branch name from the BITBUCKET_BRANCH environment variable
 const branchName = process.env.BITBUCKET_BRANCH;
-console.log(branchName,"branch")
+
 
 if (!branchNameRegex.test(branchName)) {
     fail(`Jira fix version is missing in branch name: ${branchName}`);
