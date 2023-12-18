77867ddc5c5b06390bcf18eb417453a221821975
sandhyashri <sandhyashri@7edge.com>
2023-12-19 00:23:40 +0530

    FF-1494 #time 2h feature: auo-completion-of-bid completed email templates  for buyer

--- services/auctions/handlers/auto_completion_bid.js
+++ services/auctions/handlers/auto_completion_bid.js
@@ -1,21 +1,26 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-param-reassign */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable camelcase */
 /* eslint-disable array-callback-return */
 /* eslint-disable no-await-in-loop */
 const redis = require('redis')
-
 const {
     PinpointEmail,
 } = require('aws-sdk')
+const mongodbHelper = require('../lib/mongodb_helper')
 
 const pinpoint = new PinpointEmail()
 
 async function getLot(rediskey, client, auctionData) {
+    console.log('auction_data', auctionData)
     const allBidders = await client.hGetAll('lot', rediskey)
     return Object.values(allBidders || {}).filter((bidder) => {
         const parsedBidder = JSON.parse(bidder)
-        return parsedBidder.seller_email === auctionData.seller_email && parsedBidder.auction_id === auctionData.seller_email
+        return parsedBidder.seller_email === auctionData.seller_email && parsedBidder.auction_id === auctionData.auction_id
     })
 }
 
@@ -57,45 +62,19 @@ module.exports.handler = async (event) => {
         for (let i = 0; i < getAllLots.length; i++) {
             get_lot.push(JSON.parse(getAllLots[i]))
         }
-        // const get_lot = [{
-        //     winning_user: 'sandhyashri@7edge.com',
-        //     title1: 'LOt1',
-        //     bid_amount: 150,
-        //     lot_image: 'https://indy-auction-dev-assets.s3.eu-west-2.amazonaws.com/public/DomainName/Auctions/images/0072528b-ec36-6fa0-d959-a8ffdbf20dec/auction_logo.jpg',
-        //     lot_number: 1,
-        //     name: 'sandy',
-        //     auction_title: 'Auction 1',
-        // },
-        // {
-        //     winning_user: 'sandhyashri@7edge.com',
-        //     title1: 'LOt4',
-        //     bid_amount: 1000,
-        //     lot_image: 'https://indy-auction-dev-assets.s3.eu-west-2.amazonaws.com/public/DomainName/Auctions/images/0072528b-ec36-6fa0-d959-a8ffdbf20dec/auction_logo.jpg',
-
-        //     lot_number: 2,
-        //     name: 'Abhi',
-        //     auction_title: 'Auction 2',
-
-        // },
-        // {
-        //     winning_user: 'sandhyashri+test@7edge.com',
-        //     title1: 'LOt1',
-        //     name: 'sandy',
-        //     bid_amount: 150,
-        //     auction_title: 'Auction 3',
-        //     lot_number: 4,
-        //     lot_image: 'https://indy-auction-dev-assets.s3.eu-west-2.amazonaws.com/public/DomainName/Auctions/images/0072528b-ec36-6fa0-d959-a8ffdbf20dec/auction_logo.jpg',
-
-        // }]
         console.log('lot from redis', get_lot)
         const uniqueWinningUsers = [...new Set(get_lot.map((item) => item.winning_user))]
+        const auctionData = await mongodbHelper.getAuction(event)
         for (const user of uniqueWinningUsers) {
             const winningLot = []
+            const notWinning = []
             get_lot.map((item) => {
                 if (item.winning_user === user) {
+                    console.log('itemm', item)
+                    item.lot_image = `https://indy-auction-dev-assets.s3.eu-west-2.amazonaws.com/public/${item.images[0].url}`
                     winningLot.push(item)
                 } else {
-                    
+                    notWinning.push(item)
                 }
             })
             console.log('winninglir', winningLot, user)
@@ -103,9 +82,11 @@ module.exports.handler = async (event) => {
                 url: 'sdhfjk',
                 winning_lot: winningLot,
                 buyer: user,
+                title: auctionData[0].title,
+                not_winning_lot: notWinning,
             }
             await sendMail(user, process.env.SENDER_EMAIL_ADDRESS, JSON.stringify(template_data), 'arn:aws:mobiletargeting:eu-west-2:929441721738:templates/send-auction-completion-email/EMAIL')
-        } 
+        }
     } catch (err) {
         console.log('err', err)
     }
