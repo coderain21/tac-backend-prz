d99ddb867374e70a1482730750217721c64e84d7
sandhyashri <sandhyashri@7edge.com>
2023-12-19 19:20:15 +0530

    FF-1494 #time 6h  feature: auo-completion-of-bid Working on the AWS SQS to send bulk email

--- services/auctions/handlers/auto_completion_bid.js
+++ services/auctions/handlers/auto_completion_bid.js
@@ -8,12 +8,18 @@
 /* eslint-disable array-callback-return */
 /* eslint-disable no-await-in-loop */
 const redis = require('redis')
-const {
-    PinpointEmail,
-} = require('aws-sdk')
+const splitArray = require('split-array')
+const uuid = require('uuid')
+
+const AWS = require('aws-sdk')
 const mongodbHelper = require('../lib/mongodb_helper')
 
-const pinpoint = new PinpointEmail()
+const queueUrl = 'https://sqs.eu-west-2.amazonaws.com/929441721738/auto-completion-email-dev.fifo' // `${process.env.QUEUE_URL}`
+
+AWS.config.update({ region: 'eu-west-2' })
+const sqs = new AWS.SQS()
+
+// const pinpoint = new PinpointEmail()
 
 async function getLot(rediskey, client, auctionData) {
     console.log('auction_data', auctionData)
@@ -24,36 +30,41 @@ async function getLot(rediskey, client, auctionData) {
     })
 }
 
-async function sendMail(destinationId, sourceId, templateData, templateArn) {
-    console.log(templateArn)
-    const params = {
-        Content: {
-            Template: {
-                TemplateArn: templateArn,
-                TemplateData: templateData,
-            },
-        },
-        FromEmailAddress: 'shrinit.poojary@7edge.com',
-        Destination: {
-            ToAddresses: ['sandhyashri@7edge.com'],
-        },
-    }
-    console.log(params.Content.Template)
-    console.log(JSON.stringify(params), 'params')
-    try {
-        const response = await pinpoint.sendEmail(params).promise()
-        console.log('Email sent successfully:', response)
-    } catch (error) {
-        console.error('Failed to send email:', error)
-    }
-}
+// async function sendMail(destinationId, sourceId, templateData, templateArn) {
+//     console.log(templateArn)
+//     const params = {
+//         Content: {
+//             Template: {
+//                 TemplateArn: templateArn,
+//                 TemplateData: templateData,
+//             },
+//         },
+//         FromEmailAddress: 'shrinit.poojary@7edge.com',
+//         Destination: {
+//             ToAddresses: [destinationId],
+//         },
+//     }
+//     console.log(params.Content.Template)
+//     console.log(JSON.stringify(params), 'params')
+//     try {
+//         const response = await pinpoint.sendEmail(params).promise()
+//         console.log('Email sent successfully:', response)
+//     } catch (error) {
+//         console.error('Failed to send email:', error)
+//     }
+// }
 
 module.exports.handler = async (event) => {
     try {
         console.log('event', event)
+        const getBidders = await mongodbHelper.getBidders(event)
+        console.log('get', getBidders)
+        // const getLots = await mongodbHelper.getAuctionLots(event)
+        // console.log('getting lots', getLots)
         const client = await redis.createClient({
             url: process.env.REDIS_URL,
         }).on('error', (err) => console.log('Redis Client Error', err)).connect()
+        // const client = await redis.createClient()
         if (!client.isOpen) {
             await client.connect()
         }
@@ -63,30 +74,78 @@ module.exports.handler = async (event) => {
             get_lot.push(JSON.parse(getAllLots[i]))
         }
         console.log('lot from redis', get_lot)
-        const uniqueWinningUsers = [...new Set(get_lot.map((item) => item.winning_user))]
+        // const uniqueWinningUsers = [...new Set(get_lot.map((item) => item.winning_user))]
         const auctionData = await mongodbHelper.getAuction(event)
-        for (const user of uniqueWinningUsers) {
-            const winningLot = []
-            const notWinning = []
-            get_lot.map((item) => {
-                if (item.winning_user === user) {
-                    console.log('itemm', item)
-                    item.lot_image = `https://indy-auction-dev-assets.s3.eu-west-2.amazonaws.com/public/${item.images[0].url}`
-                    winningLot.push(item)
-                } else {
-                    notWinning.push(item)
-                }
-            })
-            console.log('winninglir', winningLot, user)
-            const template_data = {
-                url: 'sdhfjk',
-                winning_lot: winningLot,
-                buyer: user,
-                title: auctionData[0].title,
-                not_winning_lot: notWinning,
-            }
-            await sendMail(user, process.env.SENDER_EMAIL_ADDRESS, JSON.stringify(template_data), 'arn:aws:mobiletargeting:eu-west-2:929441721738:templates/send-auction-completion-email/EMAIL')
+        const promiseList = []
+        const splittedUsers = splitArray(getBidders, 25)
+        const params = {
+            QueueUrl: queueUrl,
+            Entries: [],
         }
+        console.log('para', params)
+        const messageList = []
+        for (const user of splittedUsers) {
+            console.log('splittedUsers', user, user.buyer_id)
+            const id = uuid.v4()
+            params.Entries.push({
+                Id: id,
+                MessageBody: JSON.stringify(user),
+                MessageDeduplicationId: uuid.v4(),
+                MessageGroupId: uuid.v4(), // auctionData[0].seller_email,
+            })
+            // messageList.push(sqs.sendMessageBatch(params))
+            // console.log('sqsResponse', send)
+        }
+        const send = await new Promise((resolve, reject) => {
+            sqs.sendMessageBatch(params, (error, data) => {
+                if (error) reject(error)
+                resolve(data)
+            })
+        })
+        console.log('send', send)
+        // const  sqsResponse = await Promise.all(messageList)
+        return event
+
+    // }
+    //     for (const user of getBidders) {
+    //         const winningLot = []
+    //         const notWinning = []
+    //         const query = {
+    //             _id: new ObjectId(user.buyer_id), // Replace 'excluded_buyer_id' with the buyer_id you want to exclude
+    //         }
+    //         const sellerQuery = {
+    //             email_address: event.seller_email,
+    //         }
+    //         const buyerInformation = await mongodbHelper.getUser(query, process.env.BUYERS_TABLE)
+    //         const sellerInformation = await mongodbHelper.getUser(sellerQuery, process.env.SELLERS_TABLE)
+    //         get_lot.map((item) => {
+    //             if (item.winning_user === user.buyer_id) {
+    //                 console.log('itemm', item)
+    //                 item.lot_image = `https://indy-auction-dev-assets.s3.eu-west-2.amazonaws.com/public/${item.images[0].url}`
+    //                 winningLot.push(item)
+    //             } else if (item.winning_user !== user.buyer_id) {
+    //                 console.log('elseeeeeee')
+    //                 item.lot_image = `https://indy-auction-dev-assets.s3.eu-west-2.amazonaws.com/public/${item.images[0].url}`
+    //                 notWinning.push(item)
+    //             }
+    //         })
+    //         console.log('buyerInformation', buyerInformation)
+    //         const template_data = {
+    //             url: 'sdhfjk',
+    //             winning_lot: winningLot,
+    //             winning_lot_count: winningLot.length,
+    //             buyer: buyerInformation[0].first_name === '' ? 'Customer' : `${buyerInformation[0].first_name}${buyerInformation[0].last_name}`,
+    //             title: auctionData[0].title,
+    //             not_winning_lot: notWinning,
+    //             not_winning_lot_count: notWinning.length,
+    //             seller_name: sellerInformation[0].first_name === '' ? 'Customer' : `${sellerInformation[0].first_name}${sellerInformation[0].last_name}`,
+    //             seller_email: auctionData[0].seller_email,
+    //         }
+    //         promiseList.push(sendMail(user.email_address, process.env.SENDER_EMAIL_ADDRESS, JSON.stringify(template_data), 'arn:aws:mobiletargeting:eu-west-2:929441721738:templates/send-auction-completion-email/EMAIL'))
+    //         // await sendMail(user, process.env.SENDER_EMAIL_ADDRESS, JSON.stringify(template_data), 'arn:aws:mobiletargeting:eu-west-2:929441721738:templates/send-auction-completion-email/EMAIL')
+    //     }
+    //     const response = await Promise.all(promiseList)
+    //     console.log('response', response)
     } catch (err) {
         console.log('err', err)
     }
