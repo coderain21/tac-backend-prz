5f64e2f29376c80916335903404dd8d8dc80a136
shrinit <shrinit.poojary@7edge.com>
2024-01-04 09:48:19 +0530

    FF-1475 #time 15m feature: seller easily access auction

--- services/auctions/handlers/view.py
+++ services/auctions/handlers/view.py
@@ -50,7 +50,6 @@ def view(event, context):
                 "headers": headers,
                 "body": json.dumps({"message": "Please provide auction_id"})
             }
-
         client = MongoClient(os.environ['MONGO_CLIENT'])
         db = client[os.environ['DATABASE']]
         collection = db[os.environ["AUCTION_MONGODB_COLLECTION_NAME"]]
@@ -120,7 +119,7 @@ def view(event, context):
         end_time= result['end_date']
         current_time = datetime.timestamp(datetime.now())
         current_time=current_time*1000
-        if end_time != '':
+        if end_time != '' and end_time is not None:
             if current_time >= end_time:
                 # Auction has ended
                 updated_status = "Completed"
@@ -128,7 +127,6 @@ def view(event, context):
                 updated_status = result["status"]
         else:
             updated_status = result["status"]  # No change in status
-
         # Update the status in the database
         collection.update_one({"_id": auction_id}, {
                               "$set": {"status": updated_status}})
@@ -140,7 +138,6 @@ def view(event, context):
                 "statusCode": 404,
                 "body": json.dumps({"message": "Auction with associated auction_id doesn't exists"})
             }
-
         if "paddle" in result and "_id" in result["paddle"]:
             del result["paddle"]["_id"]
         client.close()
