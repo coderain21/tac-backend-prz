a5f3dcbcda44d4ac29a696746a51364be80baf3e
sandhyashri <sandhyashri@7edge.com>
2024-01-08 18:08:19 +0530

    FF-1586  #time 10m  feature: transition from socketio to websocket changed the push notification sending implementation

--- services/web-socket/src/utilities/mongodb_helper.js
+++ services/web-socket/src/utilities/mongodb_helper.js
@@ -315,30 +315,7 @@ module.exports.lotToCart = async (document) => {
     }
 }
 
-module.exports.getBuyer = async (buyerIds, seller_email) => {
-    try {
-        const connectionData = await this.connect()
-        const database = connectionData.connection.db// Access the database
-        const collection = database.collection('dev-buyers') // Replace with your collection name
-        // const query = {
-        //     _id: ObjectId(buyer_id), // Replace 'excluded_buyer_id' with the buyer_id you want to exclude
-        //     seller_email,
-        // } // Corrected 'document.buyer_id'
-        const query = {
-            _id: { $nin: buyerIds.map((buyerId) => ObjectId(buyerId)) },
-            seller_email,
-        }
-        console.log(query, 'queryy')
-        const documents = await collection.find(query).toArray() // Await the query result
-        connectionData.disconnect()
-        return documents
-    } catch (err) {
-        console.log(err)
-        return false
-    }
-}
-
-module.exports.getBuyer1 = async (buyer_id, seller_email) => {
+module.exports.getBuyer = async (buyer_id, seller_email) => {
     try {
         const connectionData = await this.connect()
         const database = connectionData.connection.db// Access the database
