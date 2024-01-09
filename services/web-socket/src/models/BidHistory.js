7952d1676c54454f9cd593eef3d23e0c43602fe4
sandhyashri <sandhyashri@7edge.com>
2024-01-09 20:03:17 +0530

    FF-1505  #time 10m  feature: update-the-extension-implementationWorked on the extension issue WOrking on the optmization of the code

--- services/web-socket/src/models/BidHistory.js
+++ services/web-socket/src/models/BidHistory.js
@@ -63,7 +63,9 @@ const BidSchema = new Schema({
     max_bid: {
         type: Number,
     },
-    email_address: { type: String},
+    email_address: { type: String },
+    lot_image: { type: String },
+    name: { type: String },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
 
