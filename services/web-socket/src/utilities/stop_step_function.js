951d3b9084e066a1983cf4b2c3c63a95dd88a84c
sandhyashri <sandhyashri@7edge.com>
2024-01-10 16:26:24 +0530

    FF-1505  #time 10m  feature: update-the-extension-implementationWorked on the extension issue

--- services/web-socket/src/utilities/stop_step_function.js
+++ services/web-socket/src/utilities/stop_step_function.js
@@ -20,11 +20,12 @@ async function startExecution(executionARN, lots) {
     console.log('Starting', typeof lots.start_date, lots.start_date)
     const newStartDate = new Date(lots.start_date).toISOString()
     console.log('new start date', newStartDate)
-    lots.start_date = newStartDate
-    console.log('before stepfunc', lots)
+    const allLOts = lots
+    allLOts.start_date = newStartDate
+    console.log('before stepfunc', allLOts)
     const params = {
         stateMachineArn: executionARN,
-        input: JSON.stringify(lots),
+        input: JSON.stringify(allLOts),
     }
     console.log('params', params)
     const stepfunctions = new StepFunctions()
