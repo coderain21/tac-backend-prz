1cb1208e9b6e12a62e54d9ac377ec55fe4bf99b7
sandhyashri <sandhyashri@7edge.com>
2024-01-11 00:01:54 +0530

    FF-1505  #time 10m  feature: update-the-extension-implementationWorked on the extension issue

--- services/web-socket/src/routes/place_bid.js
+++ services/web-socket/src/routes/place_bid.js
@@ -323,9 +323,6 @@ module.exports.placeBid = async (socket, data, io, userData) => {
                     } else {
                         console.log('6666')
                         currentLotDetails.bid_amount = await calculateNextAmont(data.bid_amount) 
-                        const updatingHighestBdder = highestBidder
-                        updatingHighestBdder.bid_amount = currentLotDetails.bid_amount
-                        await client.hSet(`auction:${data.auction_id}#${data.lot_id}`, highestBidder.buyer_id, JSON.stringify(updatingHighestBdder))
                     }
                 }
             } 
