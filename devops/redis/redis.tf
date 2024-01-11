054c445aaede6776bd89bf1b3d151cc0ca1f512a
Namratha Shettiagar <namratha.shettigar@7edge.com>
2024-01-11 21:44:38 +0530

    FF-1564 #time 5m feature: implemented redis and ecs

--- devops/redis/redis.tf
+++ devops/redis/redis.tf
@@ -18,32 +18,57 @@ provider "aws" {
   alias = "deployment-us"   # Specify a default AWS region here
   profile = "indyauction-${data.external.env.result["STAGE"]}"
 }
-# Create a subnet within the VPC
-resource "aws_subnet" "mongodb_subnet" {
-  vpc_id     = aws_default_vpc.def_vpc.id
-  cidr_block = "172.31.96.0/20"
+data "aws_vpc" "default" {
+  default = true
+  provider = aws.deployment-us
+}
+data "aws_subnets" "default" {
+  filter {
+    
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
   provider = aws.deployment-us
 }
 
-# Create security group 
-resource "aws_security_group" "ssh_sg" {
-  name        = "ssh-security-group"
-  description = "SSH Security Group"
-  vpc_id = aws_default_vpc.def_vpc.id
-  # Allow SSH traffic
+resource "aws_elasticache_subnet_group" "subnet_groups" {
+  name       = "redis-subnet-group"
+  subnet_ids = data.aws_subnets.default.ids
+  provider = aws.deployment-us
+}
+
+resource "aws_security_group" "security_groups" {
+  name        = "redis-security-group"
+  description = "Allow inbound traffic on ports 22, 80, 443, and 6379"
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Be cautious with this rule in a production environment
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Be cautious with this rule in a production environment
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
@@ -52,19 +77,14 @@ resource "aws_security_group" "ssh_sg" {
   }
   provider = aws.deployment-us
 }
-
-resource "aws_elasticache_cluster" "redis" {
-  cluster_id              = "redis-cluster"
-  engine                  = "redis"
-  node_type               = "cache.t4g.micro"
-  num_cache_nodes         = 2
-  port                    = 6379
-  subnet_group_name       = "redis-cluster-subnet-group"
-  security_group_ids      = ["sg-123456"]
-  maintenance_window      = "sun:05:00-sun:09:00"
-  snapshot_retention_limit = 0
-  snapshot_window         = "05:00-09:00"
-  tags = {
-    Name = "redis-cluster"
-  }
+resource "aws_elasticache_replication_group" "websocket" {
+  automatic_failover_enabled = true
+  subnet_group_name         = aws_elasticache_subnet_group.subnet_groups.name # Use the default subnet group
+  replication_group_id      = "websocket-redis-cluster"
+  description               = "websocket description"
+  node_type                 = "cache.t4g.micro"  # Fix the syntax error here
+  parameter_group_name      = "default.redis7.cluster.on"   # Adjust this based on your Redis version
+  port                      = 6379
+  num_cache_clusters        = 1
+  provider                  = aws.deployment-us
 }
