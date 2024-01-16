1b81715c72eee5c65c86fb2c920d06898dc8a9c1
shrinit <shrinit.poojary@7edge.com>
2024-01-16 10:46:56 +0530

    FF-1538 #time 5h feature: view all buyers in the admin management Api documentation completed and necessary changes made to the other files

--- services/admin-management/handlers/list_buyers.py
+++ services/admin-management/handlers/list_buyers.py
@@ -21,81 +21,86 @@ def prepend_backslash(text):
 
     return modified_text
 
-import json
-import os
-from pymongo import MongoClient
-
-headers = {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': True,
-    'Access-Control-Allow-Headers': '*',
-    'Access-Control-Allow-Methods': '*'
-}
 
 def list_buyers(event, context):
     try:
-        try:
-            email_address = event['requestContext']['authorizer']['claims']['cognito:username']
-        except:
-            return {
-                "statusCode": 403,
-                "headers": headers,
-                "body": json.dumps({"message": "You do not have access to perform this API action"})
-            }
+        # try:
+        #     email_address = event['requestContext']['authorizer']['claims']['cognito:username']
+        # except:
+        #     return {
+        #         "statusCode": 403,
+        #         "headers": headers,
+        #         "body": json.dumps({"message": "You do not have access to perform this API action"})
+        #     }
 
         client = MongoClient(os.environ['MONGO_CLIENT'])
         db = client[os.environ['DATABASE']]
         buyer_collection = db[os.environ["BUYER_COLLECTION"]]
         user_collection = db[os.environ["SELLERS_TABLE"]]
-        result
-
+        print(1)
+        # result= user_collection.find_one({"user_type":"admin","email_address":email_address})
+        # if result is None:
+        #     return {
+        #         "statusCode": 403,
+        #         "headers": headers,
+        #         "body": json.dumps({"message": "You do not have access to perform this API action"})
+        #     }
         # Default sorting by name
-        sort_key = 'first_name'
+        sort_key = 'full_name'
         # Check if sorting key is provided
+        projection={
+            "email_address": 1,
+            "_id":1,
+            "created_at":1,
+            "full_name": 1
+        }
+        print(2,event)
         if 'queryStringParameters' in event and 'sort_by' in event['queryStringParameters']:
             sort_key = event['queryStringParameters']['sort_by']
 
         # Sorting order (default: ascending)
         sort_order = 1
+        print(3)
         if 'queryStringParameters' in event and 'sort_order' in event['queryStringParameters']:
             sort_order = 1 if event['queryStringParameters']['sort_order'].lower() == 'ascending' else -1
-
+        print(4)
         # Search options
         search_query = {}
         if 'queryStringParameters' in event and 'search' in event['queryStringParameters']:
             search_text = event['queryStringParameters']['search']
+            search_text = prepend_backslash(search_text)
             search_query['$or'] = [
                 {"buyer_id": {"$regex": search_text, "$options": "i"}},
                 {"first_name": {"$regex": search_text, "$options": "i"}},
                 {"last_name": {"$regex": search_text, "$options": "i"}},
                 {"email_address": {"$regex": search_text, "$options": "i"}}
             ]
-
+        print(5)
         # Pagination options
         page_size = 10
         page_number = 1
         if 'queryStringParameters' in event:
             if 'page_number' in event['queryStringParameters']:
                 page_number = int(event['queryStringParameters']['page_number'])
-
+        print(6)
         # Total buyers count
         total_buyers = buyer_collection.count_documents(search_query)
 
         # Fetching buyers with sorting, pagination, and search options
-        buyers = buyer_collection.find(search_query).sort(sort_key, sort_order).skip((page_number - 1) * page_size).limit(page_size)
-
+        buyers = buyer_collection.find(search_query,projection).sort(sort_key, sort_order).skip((page_number - 1) * page_size).limit(page_size)
+        print(7)
         response_body = {
             "buyers": list(buyers),
             "total_buyers": total_buyers,
             "page_size": page_size,
             "page_number": page_number
         }
+        print(8)
 
         return {
             "statusCode": 200,
             "headers": headers,
-            "body": json.dumps(response_body)
+            "body": json.dumps(response_body, cls=Encoder)
         }
 
     except Exception as e:
