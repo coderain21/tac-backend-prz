b4a91324a8fd7fc456271c436b5a26b26ba75c08
Namratha Shettiagar <namratha.shettigar@7edge.com>
2024-01-18 10:53:51 +0530

    FF-1564 #time 5m feature: added key in buyer list

--- services/admin-management/handlers/list_buyers.py
+++ services/admin-management/handlers/list_buyers.py
@@ -64,7 +64,8 @@ def list_buyers(event, context):
             "email_address": 1,
             "_id":1,
             "created_at":1,
-            "full_name": 1
+            "full_name": 1,
+            "buyer_id": 1
         }
         if 'queryStringParameters' in event and 'sort_by' in event['queryStringParameters']:
             sort_key = event['queryStringParameters']['sort_by']
