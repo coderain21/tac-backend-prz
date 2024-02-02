83006200b6e5faced5583c87d0b4b1b97f227906
sandhyashri <sandhyashri@7edge.com>
2024-02-02 18:31:08 +0530

    FF-1636 #time 30m #comment feature:rectifying-theNAN

--- entities/Counter.js
+++ entities/Counter.js
@@ -11,7 +11,11 @@ const CounterSchema = new Schema({
     auction_id: {
         type: Schema.Types.ObjectId, trim: true,
     },
+<<<<<<< Updated upstream
     seller_email: { type: String, default: '' },
+=======
+    seller_email: { type: String, default: '', uppercase: true },
+>>>>>>> Stashed changes
     record_type: { type: String, default: '' },
     prefix: { type: String, default: '', uppercase: true },
     suffix: { type: String, default: '', uppercase: true },
