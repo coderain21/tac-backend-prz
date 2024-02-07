a1371a67926e4a0e52d6d48705b5ba16ed1a164c
khaleel <ibrahim.khaleel@7edge.com>
2024-02-07 11:19:44 +0530

    FF-1567 #time 45m feature: admin export the bidder registration trying to fix api documentation issues

--- access_token_genation.py
+++ access_token_genation.py
@@ -10,9 +10,9 @@ from dotenv import load_dotenv  # Import the library
 # Load environment variables from .env file
 load_dotenv()
 
-aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-aws_region = os.environ.get('REGION')
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+aws_region = os.environ['AWS_REGION']
 
 # Configure AWS SDK
 session = boto3.Session(
@@ -26,19 +26,19 @@ client = session.client('cognito-idp',region_name='eu-west-2')
 def generate_token(user_type):
     try:
         if user_type == 'USER':
-            user_pool_id = os.environ['COGNITO_USER_POOL_ID']
-            client_id = os.environ['COGNITO_SELLER_CLIENT_ID']
-            username = os.environ['API_USERNAME']
+            user_pool_id = os.environ['SELLER_COGNITO_USERPOOL_ID']
+            client_id = os.environ['SELLER_COGNITO_CLIENT_ID']
+            username = 'anusha.k+indyauction@7edge.com'#os.environ['API_USERNAME']
             password = os.environ['PASSWORD']
         if user_type == 'BUYERS':
-            user_pool_id = os.environ['BUYER_COGNITO_USER_POOL_ID']
-            client_id = os.environ['BUYER_COGNITO_SELLER_CLIENT_ID']
+            user_pool_id = os.environ['BUYER_COGNITO_USERPOOL_ID']
+            client_id = os.environ['BUYER_COGNITO_CLIENT_ID']
             username = os.environ['BUYER_API_USERNAME']
             password = os.environ['BUYER_PASSWORD']
             print(user_pool_id,client_id, username, password)
         if user_type == 'ADMIN':
-            user_pool_id = os.environ['COGNITO_ADMIN_USER_POOL_ID']
-            client_id = os.environ['COGNITO_ADMIN_CLIENT_ID']
+            user_pool_id = os.environ['SELLER_COGNITO_USERPOOL_ID']
+            client_id = os.environ['ADMIN_COGNITO_CLIENT_ID']
             username = os.environ['ADMIN_USERNAME']
             password = os.environ['ADMIN_PASSWORD']
             print(user_pool_id,client_id, username, password)
