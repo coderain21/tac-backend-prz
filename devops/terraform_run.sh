38142f42bd25e42ae93e830b06dc01c8caf8ba3a
Namratha Shettiagar <namratha.shettigar@7edge.com>
2024-02-08 19:49:05 +0530

    FF-1613 #time 5m feature: updated dependency layer ondition

--- devops/terraform_run.sh
+++ devops/terraform_run.sh
@@ -30,30 +30,12 @@ terraform -chdir=devops/seller_web_application init
 terraform -chdir=devops/seller_web_application apply -auto-approve
 terraform -chdir=devops/api_gateway init
 terraform -chdir=devops/api_gateway apply -auto-approve
-git diff --quiet --exit-code HEAD -- devops/dependency/node || {
-  # If changes are found, execute the desired command
-  echo "Changes detected in the devops folder!"
-  # Replace "your_command_here" with the actual command you want to run
-  terraform -chdir=devops/dependency/node init
-  terraform -chdir=devops/dependency/node apply -auto-approve
-}
-
-
-git diff --quiet --exit-code HEAD -- devops/dependency/nodejs-auth-layer || {
-  # If changes are found, execute the desired command
-  echo "Changes detected in the devops folder!"
-  # Replace "your_command_here" with the actual command you want to run
-  terraform -chdir=devops/dependency/nodejs-auth-layer init
-  terraform -chdir=devops/dependency/nodejs-auth-layer apply -auto-approve
-}
-
-git diff --quiet --exit-code HEAD -- devops/dependency/python || {
-  # If changes are found, execute the desired command
-  echo "Changes detected in the devops folder!"
-  # Replace "your_command_here" with the actual command you want to run
-  terraform -chdir=devops/dependency/python init
-  terraform -chdir=devops/dependency/python apply -auto-approve
-}
+terraform -chdir=devops/dependency/node init
+terraform -chdir=devops/dependency/node apply -auto-approve
+terraform -chdir=devops/dependency/nodejs-auth-layer init
+terraform -chdir=devops/dependency/nodejs-auth-layer apply -auto-approve
+terraform -chdir=devops/dependency/python init
+terraform -chdir=devops/dependency/python apply -auto-approve
 terraform -chdir=devops/kms init
 terraform -chdir=devops/kms apply -auto-approve
 terraform -chdir=devops/mongodb init
