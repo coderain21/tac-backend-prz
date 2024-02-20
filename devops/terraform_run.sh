0094f7aa6933223399852b3c76418ed38180f859
Namratha Shettiagar <namratha.shettigar@7edge.com>
2024-02-20 10:44:01 +0530

    FF-1661 #time 5m feature: running cognito buyer service

--- devops/terraform_run.sh
+++ devops/terraform_run.sh
@@ -82,7 +82,7 @@ export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 cd resources
 sls deploy --region $REGION --config cognito-serverless.yml --stage $STAGE
 cd ..
-cd cognito-buyer
+cd services/cognito-buyer
 sls deploy --region $REGION --stage $STAGE
 cd ..
 cd services/lambda-authorizer
