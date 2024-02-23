0edb54fcf5ff038b995aa6a78527ee78997572a5
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2024-02-23 13:51:46 +0530

    FF-1577 #time 2h feature: Accounting view all orders Finished the api documentation and trying to fix the bugs related to paginations

--- services/seller-bidder-management/handlers/list_of_bidders.py
+++ services/seller-bidder-management/handlers/list_of_bidders.py
@@ -38,7 +38,6 @@ def list_bidders(event, context):
                 "headers": headers,
                 "body": json.dumps({"message": "You do not have access to perform this API action"})
             }
-        # email_address = 'anusha.k+subdomain@7edge.com'
         buyer_collection = db[os.environ["REGISTER_AUCTION_COLLECTION"]]
         # Default sorting by name
         sort_key = 'created_at'
@@ -76,6 +75,8 @@ def list_bidders(event, context):
         if 'queryStringParameters' in event:
             if 'page_number' in event['queryStringParameters']:
                 page_number = int(event['queryStringParameters']['page_number'])
+            if 'per_page' in event['queryStringParameters']:
+                page_size = int(event['queryStringParameters']['per_page'], 10)
 
         # Total buyers count
         total_buyers = buyer_collection.count_documents(search_query)
@@ -138,13 +139,27 @@ def list_bidders(event, context):
         buyers = buyer_collection.aggregate(pipeline)
         total_buyers_pipeline = [
             {"$match": search_query},
-            {"$group": {"_id": None, "unique_emails": {"$addToSet": "$email_address"}}},
-            {"$addFields": {"total_buyers": {"$size": "$unique_emails"}}},
-            {"$project": {"_id": 0, "total_buyers": 1}},
+            {"$lookup": {
+                "from": os.environ["AUCTION_MONGODB_COLLECTION_NAME"],
+                "localField": "auction_id",
+                "foreignField": "_id",
+                "as": "auction_info"
+            }},
+            {"$unwind": "$auction_info"},
+            {"$lookup": {
+                "from": os.environ["AUCTION_MONGODB_COLLECTION_NAME"],
+                "localField": "auction_info.auction_id",
+                "foreignField": "auction_id",
+                "as": "auction_details"
+            }},
+            {"$unwind": "$auction_details"},
+            {"$group": {"_id": "$email_address"}},
+            {"$count": "total_buyers"}
         ]
 
         print('total_buyers_pipeline', total_buyers_pipeline)
         total_buyers_result = list(buyer_collection.aggregate(total_buyers_pipeline))
+        print('total_buyers_result', total_buyers_result)
         total_buyers = total_buyers_result[0]["total_buyers"] if total_buyers_result else 0
 
         collection = db[os.environ["AUCTION_MONGODB_COLLECTION_NAME"]]
