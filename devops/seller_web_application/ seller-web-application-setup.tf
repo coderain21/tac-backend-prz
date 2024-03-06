4e8ecb86847b3672fb0a2ad124b67c813355256e
Namratha Shettiagar <namratha.shettigar@7edge.com>
2024-03-06 16:45:23 +0530

    FF-1661 #time 5m feature: updated domain for production

--- devops/seller_web_application/ seller-web-application-setup.tf
+++ devops/seller_web_application/ seller-web-application-setup.tf
@@ -70,7 +70,9 @@ locals {
 locals {
   computed_variable = "${data.external.env.result["STAGE"]}" == "prod" ? "seller.${data.external.env.result["DOMAIN"]}" : "${data.external.env.result["STAGE"]}-seller.${data.external.env.result["DOMAIN"]}"
 }
-
+locals {
+  computed_domain_variable = "${data.external.env.result["STAGE"]}" == "prod" ? "www" : "www-${data.external.env.result["STAGE"]}"
+}
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket.b.bucket_regional_domain_name
@@ -173,7 +175,7 @@ resource "aws_ssm_parameter" "dashboard_application_url" {
 resource "aws_ssm_parameter" "default_subdomain" {
   name  = "DEFAULT_SUB_DOMAIN"
   type  = "String"
-  value = "www-${data.external.env.result["STAGE"]}"
+  value = local.computed_domain_variable
   provider = aws.deployment-eu
   overwrite = true
 }
