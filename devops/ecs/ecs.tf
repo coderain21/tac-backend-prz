80c49cf44826287aaba2b94c991b76b606ea7891
Namratha Shettiagar <namratha.shettigar@7edge.com>
2024-03-11 23:00:53 +0530

    FF-1697 #time 15m feature: solving deployment errors

--- devops/ecs/ecs.tf
+++ devops/ecs/ecs.tf
@@ -136,7 +136,7 @@ resource "aws_iam_role_policy_attachment" "stepfunctions_full_access_task_role" 
 }
 
 resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
-  role       = [aws_iam_role.ecs_task_execution_role.name]
+  role       = "${aws_iam_role.ecs_task_execution_role.name}
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
   provider = aws.deployment-eu
 }
