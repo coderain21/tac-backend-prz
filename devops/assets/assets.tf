12e57e0ac5a8e2fd36de3de5cd4a6ff59f45fb23
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2024-03-11 16:07:03 +0530

    FF-1704 #time 5m bugfix: auction image is not uploading Trying to fix the env issues

--- devops/assets/assets.tf
+++ devops/assets/assets.tf
@@ -456,11 +456,10 @@ resource "aws_ssm_parameter" "socket_url" {
   value = data.external.env.result["SOCKET_URL"]
   provider = aws.deployment-ap
 }
-
-resource "aws_ssm_parameter" "domain_url" {
-  name = "DOMAIN_URL"
+resource "aws_ssm_parameter" "domain_url_reset" {
+  name  = "DOMAIN_URL"
   overwrite = true
-  type = "String"
+  type  = "String"
   value = data.external.env.result["DOMAIN_URL"]
-  provider = aws.development-ap
+  provider = aws.deployment-ap
 }
