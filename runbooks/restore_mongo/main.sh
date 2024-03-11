69757d51e71aea74fb149507ca5bd65ab676d9eb
Namratha Shettiagar <namratha.shettigar@7edge.com>
2024-03-11 19:14:02 +0530

    FF-1697 #time 5m feature: restoring data with dev

--- runbooks/restore_mongo/main.sh
+++ runbooks/restore_mongo/main.sh
@@ -1,14 +1,14 @@
 sudo apt-get install -y awscli
 aws s3 sync s3://indyauction-runbooks/backups/ . --include "*backup.zip"
 unzip backup.zip
-cd dump/qa/
+cd dump/dev/
 
 for folder in *-*; do
-    new_name="pre-production-${folder#*-}"
+    new_name="dev-${folder#*-}"
     mv "$folder" "$new_name"
 done
 cd ../..
-mongorestore --uri "$(aws ssm get-parameter --name "MONGODB_REPLICA_ENDPOINT" --region "eu-west-2" --with-decryption --output text --query Parameter.Value)" dump/qa/
-mongosh "$(aws ssm get-parameter --name "MONGODB_REPLICA_ENDPOINT" --region "eu-west-2" --with-decryption --output text --query Parameter.Value)" --eval 'db.getCollection("pre-production-subdomain").updateMany({"subdomain": "www-qa"}, {$set: {"subdomain": "www-pre-production"}}, {multi: true})' 
+mongorestore --uri "$(aws ssm get-parameter --name "MONGODB_REPLICA_ENDPOINT" --region "eu-west-2" --with-decryption --output text --query Parameter.Value)" dump/dev/
+# mongosh "$(aws ssm get-parameter --name "MONGODB_REPLICA_ENDPOINT" --region "eu-west-2" --with-decryption --output text --query Parameter.Value)" --eval 'db.getCollection("pre-production-subdomain").updateMany({"subdomain": "www-dev"}, {$set: {"subdomain": "www-pre-production"}}, {multi: true})' 
 # mongosh --eval 'db.createCollection("pre-production-buyer-wishlists")'
 rm -r dump backup.zip
