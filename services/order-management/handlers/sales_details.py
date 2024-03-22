c7ab948c9f513f15c0fe89bee2c44726ca27cc78
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2024-03-22 11:13:44 +0530

    FF-1718 #time 2m bugfix: application error on checkout removed the commented lines

--- services/order-management/handlers/sales_details.py
+++ services/order-management/handlers/sales_details.py
@@ -26,23 +26,23 @@ def sales_details(event, context):
     :return: The code is returning a JSON response with the following structure:
     """
     try:
-        # try:
-        #     seller_email = event['requestContext']['authorizer']['claims']['email']
-        #     print('email ', seller_email)
-        #     if ("cognito:groups" in event['requestContext']['authorizer']['claims'] and not
-        #             'seller' in event['requestContext']['authorizer']['claims']["cognito:groups"]):
-        #         return {
-        #             "statusCode": 403,
-        #             "headers": headers,
-        #             "body": json.dumps({"message": "do not have access to perform this API action"})
-        #         }
-        # except:
-        #     return {
-        #         "statusCode": 403,
-        #         "headers": headers,
-        #         "body": json.dumps({"message": "You do not have access to perform this API action"})
-        #     }
-        seller_email= 'sthuthi+stripe@7edge.com'
+        try:
+            seller_email = event['requestContext']['authorizer']['claims']['email']
+            print('email ', seller_email)
+            if ("cognito:groups" in event['requestContext']['authorizer']['claims'] and not
+                    'seller' in event['requestContext']['authorizer']['claims']["cognito:groups"]):
+                return {
+                    "statusCode": 403,
+                    "headers": headers,
+                    "body": json.dumps({"message": "do not have access to perform this API action"})
+                }
+        except:
+            return {
+                "statusCode": 403,
+                "headers": headers,
+                "body": json.dumps({"message": "You do not have access to perform this API action"})
+            }
+        
         projection={
             'order_number':1,
             'created_at':1,
