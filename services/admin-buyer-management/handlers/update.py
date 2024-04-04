d1cf5c82edebec8758776ad02c8bd5cf0629e3b5
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2024-04-04 10:45:59 +0530

    FF-1738 #time 2m bugfix: seller application issues Removed the connection string which was being used directly in code

--- services/admin-buyer-management/handlers/update.py
+++ services/admin-buyer-management/handlers/update.py
@@ -1,10 +1,10 @@
 '''This module is used to update the auction details'''
+import os
 import pymongo
 
-
-client = pymongo.MongoClient('mongodb://indyauctionmaster:IndyAuction7EDGE@localhost:27017/dev?authMechanism=SCRAM-SHA-1&authSource=dev&retryWrites=false&directConnection=true')
-db = client['dev']
-collection = db['dev-bid-informations']
+client = pymongo.MongoClient(os.environ['MONGO_CLIENT'])
+db = client[os.environ['DATABASE']]
+collection = db[os.environ["LOT_COLLECTION_NAME"]]
 # collection_bidders = db[os.environ["UNIQUE_BIDDERS_COLLECTIONS"]]
 # user_collection = db[os.environ['SELLERS_TABLE']]
 
