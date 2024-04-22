972b16f764d934c0fdc0eb0ea20aa63a843da2ef
sandhyashri <sandhyashri@7edge.com>
2024-04-22 18:02:00 +0530

    FF-1751 #time 1h feature: seller-export-bid-insights Fixed psckage issue: pytz

--- services/bids/handlers/update.py
+++ services/bids/handlers/update.py
@@ -1,21 +1,13 @@
-"""This module contains AWS Lambda functions for creating user pools and handling requests related to subdomains, DynamoDB, AWS KMS, and MongoDB.
+"""
+Module: user_update_module
 
-Module Functions:
-- encrypt_data(data): Encrypt data using AWS Key Management Service (KMS).
-- create_user_pool(sub_domain_name): Create a Cognito User Pool with a specified subdomain.
-- fetch_item_from_dynamodb(sub_domain_name): Fetch an item from DynamoDB based on a subdomain name.
-- create(event, context): Handle a create event for a subdomain, fetch relevant data, encrypt it, and return the encrypted data as a response.
-
-The code provides functionality to create user pools in Amazon Cognito, fetch data from DynamoDB based on subdomains, and encrypt the data using AWS KMS. It also handles errors and returns appropriate responses.
-
-Note that this code assumes specific environment variables are set for configuration, such as 'REGION', 'KMS_KEY_ID', 'SUB_DOMAIN_TABLE', 'MONGO_CLIENT', 'DATABASE', 'USERPOOLS_MONGO', and others as required.
+This module contains functions related to updating user information and adding users to Cognito groups.
 """
 from pymongo import MongoClient
 import json
+import boto3
 import os
 from bson import ObjectId
-client = MongoClient(os.environ['MONGO_CLIENT'])
-db = client[os.environ['DATABASE']]
 
 headers = {
     'Content-Type': 'application/json',
@@ -24,6 +16,9 @@ headers = {
     'Access-Control-Allow-Headers': '*',
     'Access-Control-Allow-Methods': '*'
 }
+
+cognito_client = boto3.client('cognito-idp', region_name=os.environ['REGION'])
+
 
 def fetch_seller_email_from_auction(auction_id):
     """
@@ -35,36 +30,138 @@ def fetch_seller_email_from_auction(auction_id):
     Returns:
         str: The seller's email associated with the given auction_id or None if not found.
     """
+    client = MongoClient(os.environ['MONGO_CLIENT'])
+    db = client[os.environ['DATABASE']]
     auction_collection = db[os.environ["AUCTION_MONGODB_COLLECTION_NAME"]]
-    email = auction_collection.find_one(
-            {"_id":ObjectId(auction_id)},{'seller_email' : 1}).get('seller_email')
+    email = auction_collection.find_one({"_id": ObjectId(auction_id)}, {
+                                        'seller_email': 1}).get('seller_email')
+    client.close()
     return email
 
-def create(event, context):
-    """Handle a create event for a subdomain, fetch relevant data, encrypt it, and return the encrypted data as a response.
+
+def update_user(event, context):
+    """
+    Update user information and add the user to a Cognito group.
 
     Args:
-        event (dict): The event data containing information about the subdomain in the 'pathParameters'.
-        context: The AWS Lambda context object (not used in this function).
+        event (dict): The AWS Lambda event object containing details about the API Gateway request.
+        context (object): The AWS Lambda context object.
 
     Returns:
-        dict: A response containing the encrypted data as a base64-encoded string or an error response in case of issues.
+        dict: A dictionary containing the API Gateway response.
     """
     try:
-        # sub_domain_name = event['queryStringParameters'].get('domain')
-        auction_id = event['queryStringParameters'].get('auction_id')
-        # default = sub_domain_name == os.environ["DEFAULT_SUB_DOMAIN"]
-        data = fetch_seller_email_from_auction(auction_id)
+        try:
+            print(event)
+            cognito_data = json.loads(
+                event['requestContext']['authorizer']['data'])
+            print(cognito_data)
+            email_address = cognito_data['email']
+            if "cognito:groups" in cognito_data and not 'buyer' in cognito_data["cognito:groups"]:
+                return {
+                    "statusCode": 403,
+                    "headers": headers,
+                    "body": json.dumps({"message": "You do not have access to perform this API action"})
+                }
+        except:
+            return {
+                "statusCode": 403,
+                "headers": headers,
+                "body": json.dumps({"message": "You do not have access to perform this API action"})
+            }
 
-        return {
-            "statusCode": 201,
-            "headers": headers,
-            "body": json.dumps({"seller_email": data})
-        }
+        data = json.loads(event["body"])
+        print('dataa', data)
+        expected_fields = ["auction_id", "group"]
+        fields_not_found = list(set(expected_fields).difference(data.keys()))
+        if fields_not_found:
+            return {"headers": headers,
+                    'statusCode': 400,
+                    "body": json.dumps(
+                        {"message": f"Please provide {','.join(fields_not_found)}"})
+                    }
+        auction_id = data.get("auction_id")
+        group = data.get("group")
+        new = data.get("new",False)
+        seller_email = fetch_seller_email_from_auction(auction_id)
+        print('seller_email', seller_email, group)
+        # Add user to the specified Cognito group
+        x = cognito_client.admin_add_user_to_group(
+            GroupName= group.split('@')[0],
+            UserPoolId=os.environ["DEFAULT_USERPOOL_ID"],
+            Username=email_address
+        )
+        print('xxxx', x)
+
+        if seller_email is not None:
+            buyer_data_to_add = {
+                "user_type": "buyer",
+                "password": "",
+                "email_address": email_address,
+                "newsletter_notification": False,
+                "seller_email": seller_email,
+                "terms_and_condition": True,
+                "first_name": "",
+                "last_name": ""
+            }
+            client = MongoClient(os.environ['MONGO_CLIENT'])
+            db = client[os.environ['DATABASE']]
+            buyer_collection = db[os.environ["BUYER_COLLECTION"]]
+
+            # Check if the user already has a seller_email associated
+            buyer_data = buyer_collection.find_one(
+                {"email_address": email_address, "seller_email": seller_email})
+
+            if buyer_data is None:
+                if new is True:
+                    print("new")
+                    new_data = {
+                        "user_type": "buyer",
+                        "password": "",
+                        "email_address": email_address,
+                        "newsletter_notification": False,
+                        "seller_email": seller_email,
+                        "terms_and_condition": True,
+                        "first_name": "",
+                        "last_name": ""
+                    }
+                    buyer_data_new = buyer_collection.find_one(
+                        {"email_address": email_address},{"_id":0,"seller_email":0})
+                    print("-->",buyer_data_new)
+                    if buyer_data_new is not None:
+                        buyer_data_new["seller_email"]=seller_email
+                        buyer_collection.insert_one(buyer_data_new)
+                    else:
+                        buyer_collection.insert_one(new_data)
+
+                else:
+                    # Check if the user has an existing record without seller_email
+                    buyer_data_without_seller = buyer_collection.find_one(
+                        {"email_address": email_address, "registered_through": "federated"})
+                    print("-->", buyer_data_without_seller)
+                    if buyer_data_without_seller is not None and "seller_email" not in buyer_data_without_seller:
+                        buyer_collection.update_one({"_id": buyer_data_without_seller["_id"]}, {
+                                                    '$set': {"seller_email": seller_email, "registered_through": ""}})
+
+                    if buyer_data_without_seller == None:
+                        buyer_collection.insert_one(buyer_data_to_add)
+
+            client.close()
+            return {
+                "statusCode": 204,
+                'headers': headers,
+                "body": json.dumps({})
+            }
+        else:
+            return {
+                "statusCode": 404,
+                'headers': headers,
+                "body": json.dumps({"message": "User does not exist"})
+            }
     except Exception as err:
         print(err)
         return {
-            "headers": headers,
             "statusCode": 500,
-            "body": json.dumps({"message": "There was an error"})
+            'headers': headers,
+            "body": json.dumps({"message": "There was an error while adding to the group"})
         }
