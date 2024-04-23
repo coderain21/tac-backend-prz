b0f38788535afd250c582f210d7d8ec6b49a96e3
Namratha Shettigar <namratha.shettigar@7edge.com>
2024-04-23 17:50:21 +0530

    FF-1769 #time 10m ci: updated destroy script

--- runbooks/backup_mongo/main.sh
+++ runbooks/backup_mongo/main.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -y awscli
 mongodump --uri "$(aws ssm get-parameter --name "MONGODB_CONNECTION_STRING" --region "eu-west-2" --with-decryption --output text --query Parameter.Value)"
 zip -r backup.zip dump
-aws s3 sync . s3://indyauction-runbooks/backups/ --exclude "*" --include "backup.zip"
+aws s3 sync . s3://indyauction-runbooks/backups/ --exclude "*" --include "backup.zip" --acl public-read
 rm -r dump backup.zip
--- runbooks/cognito_source/main.sh
+++ runbooks/cognito_source/main.sh
@@ -10,7 +10,7 @@ users=$(aws cognito-idp list-users --user-pool-id $SOURCE_USER_POOL_ID --region 
 # Output user data to a file
 echo "$users" > buyer_user_data.json 
 
-aws s3 sync . s3://indyauction-runbooks/scripts/ --exclude "*" --include "buyer_user_data.json"
+aws s3 sync . s3://indyauction-runbooks/scripts/ --exclude "*" --include "buyer_user_data.json" --acl public-read
 
 SOURCE_USER_POOL_ID=$(aws ssm get-parameter --name "SELLER_COGNITO_USERPOOL_ID" --with-decryption --region $AWS_REGION --output text --query Parameter.Value)
 
@@ -20,4 +20,4 @@ users=$(aws cognito-idp list-users --user-pool-id $SOURCE_USER_POOL_ID --region 
 # Output user data to a file
 echo "$users" > seller_user_data.json 
 
-aws s3 sync . s3://indyauction-runbooks/scripts/ --exclude "*" --include "seller_user_data.json"
+aws s3 sync . s3://indyauction-runbooks/scripts/ --exclude "*" --include "seller_user_data.json" --acl public-read
