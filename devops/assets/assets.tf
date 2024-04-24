a1551e978fc22d3037b6bd82b56651ddf336835e
Ranjith N <ranjith.n@7edge.com>
2024-04-24 14:11:54 +0530

    FF-1769 #time 1h ci: new changes

--- devops/assets/assets.tf
+++ devops/assets/assets.tf
@@ -16,16 +16,44 @@ provider "aws" {
 
 provider "aws" {
   region = "eu-west-2"
-  alias = "deployment-ap"   
+  alias = "deployment-eu"   
   profile = "indyauction-${var.STAGE}"
 }
 
-provider "aws" {
-  region = data.external.env.result["REGION"]
+locals {
+  sub_domain = var.STAGE == "prod" ? var.DOMAIN : "${var.STAGE}.${var.DOMAIN}"
+}
+data "aws_route53_zone" "domain_zone" {
+  name = data.external.env.result["DOMAIN"] # Replace with your domain name
+  provider = aws.main
+}
+resource "aws_route53_zone" "dev" {
+  count = var.STAGE != "prod" ? 1 : 0
+  name = local.sub_domain
+  tags = {
+    Environment = var.STAGE
+  }
 }
 
+resource "aws_route53_record" "dev-ns" {
+  count = var.STAGE != "prod" ? 1 : 0
+  zone_id = data.aws_route53_zone.domain_zone.zone_id
+  name    = local.sub_domain
+  type    = "NS"
+  ttl     = "30"
+  records = aws_route53_zone.dev.name_servers
+  provider =  aws.main
+}
+
+
+
+locals {
+  zone_id = "${var.STAGE}" == "prod" ? data.aws_route53_zone.domain_zone : aws_route53_zone.dev.zone_id
+}
+
+
 resource "aws_acm_certificate" "cert_us_east_1" {
-  domain_name ="*.${data.external.env.result["DOMAIN"]}"
+  domain_name ="*.${local.sub_domain}"
   validation_method = "DNS"
   lifecycle {
     create_before_destroy = true
@@ -36,20 +64,16 @@ resource "aws_acm_certificate" "cert_us_east_1" {
 
 
 resource "aws_acm_certificate" "cert_ap_south_1" {
-  domain_name ="*.${data.external.env.result["DOMAIN"]}"
+  domain_name ="*.${local.sub_domain}"
   validation_method = "DNS"
   lifecycle {
     create_before_destroy = true
   }
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 
-data "aws_route53_zone" "domain_zone" {
-  name = data.external.env.result["DOMAIN"] # Replace with your domain name
-  provider = aws.main
-}
-
-resource "aws_route53_record" "route_53_certificate_records_ap_south_1" {
+resource "aws_route53_record" "route_53_certificate_records_ap_south_1_dev" {
+  count = var.STAGE != "prod" ? 1 : 0
   for_each = {
     for dvo in aws_acm_certificate.cert_ap_south_1.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -63,11 +87,32 @@ resource "aws_route53_record" "route_53_certificate_records_ap_south_1" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.domain_zone.zone_id
+  zone_id         = local.zone_id
+  provider = aws.deployment-us
+}
+
+
+resource "aws_route53_record" "route_53_certificate_records_ap_south_1_prod" {
+  count = var.STAGE == "prod" ? 1 : 0
+  for_each = {
+    for dvo in aws_acm_certificate.cert_ap_south_1.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = local.zone_id
   provider = aws.main
 }
 
-resource "aws_route53_record" "route_53_certificate_records_us_east_1" {
+resource "aws_route53_record" "route_53_certificate_records_us_east_1_dev" {
+  count = var.STAGE != "prod" ? 1 : 0
   for_each = {
     for dvo in aws_acm_certificate.cert_us_east_1.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -81,7 +126,26 @@ resource "aws_route53_record" "route_53_certificate_records_us_east_1" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.domain_zone.zone_id
+  zone_id         = local.zone_id
+  provider = aws.deployment-us
+}
+
+resource "aws_route53_record" "route_53_certificate_records_us_east_1_prod" {
+  count = var.STAGE == "prod" ? 1 : 0
+  for_each = {
+    for dvo in aws_acm_certificate.cert_us_east_1.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = local.zone_id
   provider = aws.main
 }
 
@@ -92,7 +156,7 @@ resource "aws_s3_bucket" "assets" {
   tags = {
     Name = "${var.STAGE}"
   }
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_enable" {
   bucket = aws_s3_bucket.assets.id
@@ -123,7 +187,7 @@ resource "aws_s3_bucket_cors_configuration" "enable_cors_assets" {
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 
 
@@ -133,7 +197,7 @@ resource "aws_cloudfront_origin_access_control" "cdn" {
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
@@ -142,7 +206,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     origin_access_control_id = aws_cloudfront_origin_access_control.cdn.id
     origin_id                = "myS3Origin"
   }
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 
 
   enabled             = true
@@ -150,7 +214,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   comment             = "CDN for application"
 
 
-  aliases = ["${var.STAGE}-cdn.${data.external.env.result["DOMAIN"]}"]
+  aliases = ["cdn.${locals.sub_domain}"]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -192,7 +256,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.assets.id
   policy = data.aws_iam_policy_document.s3_policy.json
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 
 }
 
@@ -207,14 +271,25 @@ data "aws_iam_policy_document" "s3_policy" {
       identifiers = ["cloudfront.amazonaws.com"]
     }
   }
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 
 }
 
-resource "aws_route53_record" "assets_cname" {
-  name    = "${var.STAGE}-cdn.${data.external.env.result["DOMAIN"]}" # Replace with your desired CNAME
+resource "aws_route53_record" "assets_cname_dev" {
+  count = var.STAGE != "prod" ? 1 : 0
+  name    = "cdn.${locals.sub_domain}" # Replace with your desired CNAME
   type    = "CNAME"
-  zone_id = data.aws_route53_zone.domain_zone.zone_id
+  zone_id = local.zone_id
+  records = [aws_cloudfront_distribution.s3_distribution.domain_name]
+  ttl = 300
+  provider = aws.deployment-us
+}
+
+resource "aws_route53_record" "assets_cname_prod" {
+  count = var.STAGE == "prod" ? 1 : 0
+  name    = "cdn.${locals.sub_domain}" # Replace with your desired CNAME
+  type    = "CNAME"
+  zone_id = local.zone_id
   records = [aws_cloudfront_distribution.s3_distribution.domain_name]
   ttl = 300
   provider = aws.main
@@ -225,14 +300,14 @@ resource "aws_ssm_parameter" "assets_bucket" {
   name  = "BUCKET_NAME"
   type  = "String"
   value = "indyauction-assets-${var.STAGE}"
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
   overwrite = true
 }
 resource "aws_ssm_parameter" "application_url" {
   name  = "CDN_URL"
   type  = "String"
-  value = "https://${var.STAGE}-cdn.${data.external.env.result["DOMAIN"]}/public/"
-  provider = aws.deployment-ap
+  value = "https://cdn.${locals.sub_domain}/public/"
+  provider = aws.deployment-eu
   overwrite = true
 }
 
@@ -241,21 +316,21 @@ resource "aws_ssm_parameter" "customer_session_token" {
   overwrite = true
   type  = "String"
   value = data.external.env.result["CUSTOMER_SESSION_TOKEN_SECRET"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 resource "aws_ssm_parameter" "recaptch_key" {
   name  = "RECAPTCHA_KEY"
   overwrite = true
   type  = "String"
   value = data.external.env.result["RECAPTCHA_KEY"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 resource "aws_ssm_parameter" "password_secret_key" {
   name  = "PASSWORD_SECRET_KEY"
   overwrite = true
   type  = "String"
   value = data.external.env.result["PASSWORD_SECRET_KEY"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 
 
@@ -264,28 +339,28 @@ resource "aws_ssm_parameter" "sumsub_secret_key_webhook" {
   overwrite = true
   type  = "String"
   value = data.external.env.result["SUMSUB_SECRET_KEY_WEBHOOK"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 resource "aws_ssm_parameter" "jwt_secret_key" {
   name  = "JWT_SECRET_KEY"
   overwrite = true
   type  = "String"
   value = data.external.env.result["JWT_SECRET_KEY"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 resource "aws_ssm_parameter" "sumsub_secret_key" {
   name  = "SUMSUB_SECRET_KEY"
   overwrite = true
   type  = "String"
   value = data.external.env.result["SUMSUB_SECRET_KEY"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 resource "aws_ssm_parameter" "sumsub_app_token" {
   name  = "SUMSUB_APP_TOKEN"
   overwrite = true
   type  = "String"
   value = data.external.env.result["SUMSUB_APP_TOKEN"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 
 
@@ -294,7 +369,7 @@ resource "aws_ssm_parameter" "stripe_api_key" {
   overwrite = true
   type  = "String"
   value = data.external.env.result["STRIPE_API_KEY"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 
 
@@ -305,14 +380,14 @@ resource "aws_ssm_parameter" "amplify_branch" {
   overwrite = true
   type  = "String"
   value = data.external.env.result["BITBUCKET_BRANCH"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 resource "aws_ssm_parameter" "stripe_endpoint_secret" {
   name  = "STRIPE_ENDPOINT_SECRET"
   overwrite = true
   type  = "String"
   value = data.external.env.result["STRIPE_ENDPOINT_SECRET"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 
 resource "aws_ssm_parameter" "facebook_client_id" {
@@ -320,28 +395,28 @@ resource "aws_ssm_parameter" "facebook_client_id" {
   overwrite = true
   type  = "String"
   value = data.external.env.result["FACEBOOK_CLIENT_ID"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 resource "aws_ssm_parameter" "facebook_client_secret" {
   name  = "FACEBOOK_CLIENT_SECRET"
   overwrite = true
   type  = "String"
   value = data.external.env.result["FACEBOOK_CLIENT_SECRET"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 resource "aws_ssm_parameter" "google_client_id" {
   name  = "GOOGLE_CLIENT_ID"
   overwrite = true
   type  = "String"
   value = data.external.env.result["GOOGLE_CLIENT_ID"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 resource "aws_ssm_parameter" "google_client_secret" {
   name  = "GOOGLE_CLIENT_SECRET"
   overwrite = true
   type  = "String"
   value = data.external.env.result["GOOGLE_CLIENT_SECRET"]
-  provider = aws.deployment-ap
+  provider = aws.deployment-eu
 }
 
 
