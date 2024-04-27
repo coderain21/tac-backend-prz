6169f922c4c611165b9b5d7a9b81bc48cbcfce2c
Namratha Shettigar <namratha.shettigar@7edge.com>
2024-04-27 17:18:54 +0530

    FF-1769 #time 5m ci: removing duplicate ssm

--- devops/assets/assets.tf
+++ devops/assets/assets.tf
@@ -496,11 +496,3 @@ resource "aws_ssm_parameter" "buyer_cognito_custom_domain" {
   provider = aws.deployment-eu
   overwrite = true
 }
-
-resource "aws_ssm_parameter" "buyer_cognito_custom_domain" {
-  name  = "BUYER_COGNITO_USERPOOL_DOMAIN"
-  type  = "String"
-  value = "no-reply@${local.sub_domain}"
-  provider = aws.deployment-eu
-  overwrite = true
-}
