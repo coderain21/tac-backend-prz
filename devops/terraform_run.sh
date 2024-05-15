feab3f140716d0b7078c9a148f46cec905a07739
Namratha Shettigar <namratha.shettigar@7edge.com>
2024-05-15 19:44:35 +0530

    FF-1769 #time 5m ci: added  printing bitbucket_secret

--- devops/terraform_run.sh
+++ devops/terraform_run.sh
@@ -96,6 +96,7 @@ if [ -f "$STATE_FILE" ]; then
 #   domain=$(aws amplify get-domain-association --app-id $APP_ID --domain-name $AMPLIFY_DOMAIN_NAME --profile $PROFILE_ENV --region $REGION --query 'domainAssociation.subDomains[*].subDomainSetting.prefix')
 #   echo "{\"subdomains\": $domain}" > devops/buyer_web_application/subdomains.json
   # Add your commands here that use $DOMAIN_ASSOCIATION_ID (if needed)
+  terraform -chdir=devops/buyer_web_application plan
   terraform -chdir=devops/buyer_web_application apply -auto-approve -target=aws_amplify_app.customer_web_application \
                -target=aws_amplify_branch.amplify_branch \
                -target=aws_ssm_parameter.amplify_id \
