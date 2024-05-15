1407c6ff1e06c713be1a0b972f63ac48d675a47f
Namratha Shettigar <namratha.shettigar@7edge.com>
2024-05-15 21:19:20 +0530

    FF-1769 #time 5m ci: added  printing environemnt value

--- devops/buyer_web_application/web_application.tf
+++ devops/buyer_web_application/web_application.tf
@@ -25,7 +25,7 @@ provider "aws" {
 }
 
 data "external" "token" {
-  program = ["bash", "-c", "echo \"{\\\"token\\\":\\\"$(curl -s -X POST -u '${var.BITBUCKET_SECRET}' https://bitbucket.org/site/oauth2/access_token -d grant_type=client_credentials -d code=420 | jq -r '.access_token')\\\"}\""]
+  program = ["bash", "-c", "echo \"{\\\"token\\\":\\\"$(curl -s -X POST -u 'F6BUkzxemBHrQRNmBw:bjuamEZ7jFm5ms43k49NBSCK4n6zTUy2' https://bitbucket.org/site/oauth2/access_token -d grant_type=client_credentials -d code=420 | jq -r '.access_token')\\\"}\""]
 }
 
 
