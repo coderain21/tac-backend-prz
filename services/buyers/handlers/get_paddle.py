247ba89bafa323e0f0b8e49e48f3409e3728ceaa
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2024-05-23 16:20:32 +0530

    FF-1805 #time 30m feature: cognito changing lambda to cognito Implemented the cognito auth for the paddle api in the buyer service

--- services/buyers/handlers/get_paddle.py
+++ services/buyers/handlers/get_paddle.py
@@ -34,21 +34,39 @@ def paddle_number(event, context):
     response depends on the execution path of the code.
     """
     try:
+        # try:
+        #     cognito_data = json.loads(
+        #         event['requestContext']['authorizer']['data'])
+        #     email_address = cognito_data['email']
+        #     if "cognito:groups" not in cognito_data :
+        #         return {
+        #             "statusCode": 403,
+        #             "headers": headers,
+        #             "body": json.dumps({"message": "You do not have access to perform this API action"})
+        #         }
+        # except:
+        #     return {
+        #         "statusCode": 403,
+        #         "headers": headers,
+        #         "body": json.dumps({"message": "You do not have access to perform this API action"})
+        #     }
         try:
-            cognito_data = json.loads(event['requestContext']['authorizer']['data'])
-            email_address = cognito_data['email']
-            if "cognito:groups" not in cognito_data :
+            email_address = event['requestContext']['authorizer']['claims']['email']
+            print('email', email_address)
+            if "cognito:groups" in event['requestContext']['authorizer']['claims'] and not 'buyer' in event['requestContext']['authorizer']['claims']["cognito:groups"]:
+                print('here in first')
                 return {
-                "statusCode": 403,
-                "headers": headers,
-                "body": json.dumps({"message": "You do not have access to perform this API action"})
-            }
+                    "statusCode": 403,
+                    "headers": headers,
+                    "body": json.dumps({"message": "You do not have access to perform this API action"})
+                }
         except:
+            print('here in second')
             return {
                 "statusCode": 403,
                 "headers": headers,
                 "body": json.dumps({"message": "You do not have access to perform this API action"})
-        }
+            }
         # Parse query parameters from the event
         data = event['queryStringParameters']
         if data is None or "auction_id" not in data:
