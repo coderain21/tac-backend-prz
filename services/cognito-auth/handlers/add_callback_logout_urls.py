4b0ccf15bfede20095bcdac7adb67f38c8f5c8ee
Namratha Shettigar <namratha.shettigar@7edge.com>
2024-05-23 08:24:51 +0530

    FF-1802 #time 4h ci: added the function for callback url and updated buyer client resorce

--- services/cognito-auth/handlers/add_callback_logout_urls.py
+++ services/cognito-auth/handlers/add_callback_logout_urls.py
@@ -1,44 +1,56 @@
-"""
-Module: address_management_api
-
-AWS Lambda function `add_shipping_address` adds shipping/billing addresses to user profiles.
-Handles HTTP requests, validates permissions via Amazon Cognito, and stores data in MongoDB.
-
-Dependencies:
-- json: Parsing JSON data.
-- pymongo: MongoDB driver.
-- os: Accessing environment variables.
-- lib.common_helper.Encoder: Custom JSON encoder.
-
-Response Structure:
-- JSON response with status code, headers, and message.
-"""
-
-import boto3
 import os
+import json
+import boto3
+import cfnresponse
 
 cognito_idp = boto3.client('cognito-idp')
 
-def add_callback_logout_urls(event, context):
-    user_pool_id = os.environ['BUYER_COGNITO_USERPOOL_ID']
-    client_id = os.environ['BUYER_COGNITO_CLIENT_ID']
-    callback_urls = event.get('callback_urls', [])
-    logout_urls = event.get('logout_urls', [])
-    allowed_oauth_flows_user_pool_client = event.get('allowed_oauth_flows_user_pool_client', False)
-    allowed_oauth_flows = event.get('allowed_oauth_flows', [])
-    allowed_oauth_scopes = event.get('allowed_oauth_scopes', [])
-
-    response = cognito_idp.update_user_pool_client(
+def get_user_pool_client_config(user_pool_id, client_id):
+    response = cognito_idp.describe_user_pool_client(
         UserPoolId=user_pool_id,
-        ClientId=client_id,
-        CallbackURLs=callback_urls,
-        LogoutURLs=logout_urls,
-        AllowedOAuthFlowsUserPoolClient=allowed_oauth_flows_user_pool_client,
-        AllowedOAuthFlows=allowed_oauth_flows,
-        AllowedOAuthScopes=allowed_oauth_scopes
+        ClientId=client_id
     )
+    return response['UserPoolClient']
 
-    return {
-        'statusCode': 200,
-        'body': 'User Pool Client updated successfully'
-    }
+def add_callback_logout_urls(event, context):
+    try:
+        user_pool_id = os.environ['BUYER_COGNITO_USERPOOL_ID']
+        client_id = os.environ['BUYER_COGNITO_CLIENT_ID']
+        resource_properties = event.get('ResourceProperties', {})
+        callback_urls = resource_properties.get('CallbackURLs', [])
+        logout_urls = resource_properties.get('LogoutURLs', [])
+        allowed_oauth_flows = resource_properties.get('AllowedOAuthFlows', [])
+        allowed_oauth_scopes = resource_properties.get('AllowedOAuthScopes', [])
+        supported_identity_providers = resource_properties.get('SupportedIdentityProviders', [])
+        allowed_oauth_flows_user_pool_client = resource_properties.get('AllowedOAuthFlowsUserPoolClient', 'false')
+        if isinstance(allowed_oauth_flows_user_pool_client, str):
+            allowed_oauth_flows_user_pool_client = allowed_oauth_flows_user_pool_client.lower() == 'true'
+        
+        print("supported_identity_providers",supported_identity_providers)
+
+        # Get current configuration of the user pool client
+        existing_config = get_user_pool_client_config(user_pool_id, client_id)
+
+        # Merge existing and new settings
+        callback_urls = list(set(existing_config.get('CallbackURLs', []) + callback_urls))
+        logout_urls = list(set(existing_config.get('LogoutURLs', []) + logout_urls))
+        allowed_oauth_flows = list(set(existing_config.get('AllowedOAuthFlows', []) + allowed_oauth_flows))
+        allowed_oauth_scopes = list(set(existing_config.get('AllowedOAuthScopes', []) + allowed_oauth_scopes))
+        # supported_identity_providers = list(set(existing_config.get('SupportedIdentityProviders', []) + supported_identity_providers))
+
+        # Update user pool client with merged settings
+        response = cognito_idp.update_user_pool_client(
+            UserPoolId=user_pool_id,
+            ClientId=client_id,
+            CallbackURLs=callback_urls,
+            LogoutURLs=logout_urls,
+            AllowedOAuthFlowsUserPoolClient=allowed_oauth_flows_user_pool_client,
+            AllowedOAuthFlows=allowed_oauth_flows,
+            AllowedOAuthScopes=allowed_oauth_scopes,
+            SupportedIdentityProviders=supported_identity_providers
+        )
+        # Send success response back to CloudFormation
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, {'message': 'User Pool Client updated successfully'})
+    except Exception as e:
+        # Send failure response back to CloudFormation
+        cfnresponse.send(event, context, cfnresponse.FAILED, {'error': str(e)})
