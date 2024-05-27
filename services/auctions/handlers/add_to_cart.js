610dcd07fa91819f2a66956b6148231d0cac9ff0
sandhyashri <sandhyashri@7edge.com>
2024-05-27 13:08:16 +0530

    FF-1811 #time 1h bugfix: auction-status-not-getting-chnaging-if-no-winning-user Added a consition to handle the no winning user usecase

--- services/auctions/handlers/add_to_cart.js
+++ services/auctions/handlers/add_to_cart.js
@@ -72,14 +72,26 @@ module.exports.handler = async (event) => {
             get_lot.push(JSON.parse(getLotInfo[i]))
         }
         const lotInformation = get_lot[0]
+        const getLots = await mongodbHelper.getAuctionsLots(event, currentTimestamp, Lot)
         if (lotInformation.end_date < currentTimestamp && get_lot.length > 0 && lotInformation.winning_user) {
             const getBuyerData = await mongodbHelper.getBuyer(lotInformation.winning_user, Buyers)
             lotInformation.email_address = getBuyerData.email_address === undefined ? null : getBuyerData.email_address
             lotInformation.name = getBuyerData.first_name === undefined ? null : getBuyerData.first_name
             await mongodbHelper.lotToCart(lotInformation, auctionData, Cart)
             await mongodbHelper.getLatestRecord(lotInformation, BidInformation)
-            const getLots = await mongodbHelper.getAuctionsLots(event, currentTimestamp, Lot)
             // const callSQS = await sqsTriggerFunction(event)
+            if (auctionData.extension_type === 'All Lots' && event.lot_number === 1) {
+                await sqsTriggerFunction(event)
+            }
+            if (getLots.length <= 0) {
+                if (auctionData.extension_type === 'Cascade' || auctionData.extension_type === 'Individual Lots') {
+                    await sqsTriggerFunction(event)
+                }
+            } else {
+                console.log('no match')
+            }
+        }
+        if (lotInformation.end_date < currentTimestamp && get_lot.length > 0 && !lotInformation.winning_user) {
             if (auctionData.extension_type === 'All Lots' && event.lot_number === 1) {
                 await sqsTriggerFunction(event)
             }
