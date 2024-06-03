0d93f8f6a705116161b256aa216ff8faabaa719b
sandhyashri <sandhyashri@7edge.com>
2024-06-03 14:12:57 +0530

    FF-1824 #time 30m bugfix: update-auction-not-working Fixed the flow that it will update auction type first then it will update the end date

--- services/auctions/handlers/update.py
+++ services/auctions/handlers/update.py
@@ -170,7 +170,6 @@ def update_auction(event, context):
                 "headers": headers,
                 "body": json.dumps({"message": "You do not have access to perform this API action"})
             }
-        authorizationCheck = authorization(seller_email)
         request_body = json.loads(event['body'])
         auction_end_date = request_body.get('end_date', None)
         auction_start_date = request_body.get('start_date', None)
@@ -534,7 +533,7 @@ def update_auction(event, context):
                 if  len(listLots) > 0 and auction_extension_type in ["Cascade", "Individual Lots"]:
                     additional_time_ms = end_date_update + (existing_lots_count -1 ) * extension_time * 60 * 1000
                     update_data ['end_date'] = additional_time_ms
-                else: 
+                else:
                     end_date_update =  request_body.get('end_date', auction_record.get('end_date'))
                     update_data ['end_date'] = end_date_update
         if auction_start_date != None:
