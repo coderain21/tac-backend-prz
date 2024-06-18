ec82a72c423a179b85e30a3d584efc88c264264e
sandhyashri <sandhyashri@7edge.com>
2024-06-18 12:13:44 +0530

    FF-1901 #time 2h feature: buyer-seller-and-admin Added dynamic user_id S0001 and so on for all the users for access logs

--- entities/Users.js
+++ entities/Users.js
@@ -18,6 +18,7 @@ const UserSchema = new Schema({
     user_type: {
         type: String, trim: true, required: true,
     },
+    seller_id: { type: String, trim: true },
     user_name: { type: String, trim: true },
     password: { type: String, trim: true, required: true },
     email_address: { type: String, trim: true },
