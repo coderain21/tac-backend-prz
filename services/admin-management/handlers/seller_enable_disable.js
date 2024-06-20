905343c523766f7afa451b2f9bb2b9fa92ccef04
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2024-06-20 18:26:16 +0530

    FF-1910 #time 1h feature: admin email template deactivate seller Added email template for the seller notification while deactivating the seller

--- services/admin-management/handlers/seller_enable_disable.js
+++ services/admin-management/handlers/seller_enable_disable.js
@@ -130,7 +130,7 @@ module.exports.handler = async (event) => {
         payload.updated_by.email_address = emailAddress
         const Status = payload.section.action === 'Activate' ? 'adminEnableUser' : 'adminDisableUser'
         const seller_email = payload.section.user_id
-        const templateName = payload.section.action === 'Activate' ? process.env.TEMPLATE_ARN_ADMIN_ACTIVATE_SELLER : process.env.TEMPLATE_ARN_ADMIN_ACTIVATE_SELLER
+        const templateName = payload.section.action === 'Activate' ? process.env.TEMPLATE_ARN_ADMIN_ACTIVATE_SELLER : process.env.TEMPLATE_ARN_ADMIN_DEACTIVATE_SELLER
 
         /** ----------------------------------------------chnage status in the documentDB------------------------------------------------------------------------------------------------- */
         const query = { email_address: seller_email }
