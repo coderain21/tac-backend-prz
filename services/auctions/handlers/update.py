9eebd6ca54b7ca7e47a81034607771afb9a147cb
sandhyashri <sandhyashri@7edge.com>
2024-06-21 18:38:08 +0530

    FF-1938 #time 15m hotfix: update-auction-failing Added one line code to the update auction

--- services/auctions/handlers/update.py
+++ services/auctions/handlers/update.py
@@ -317,7 +317,6 @@ def update_auction(event, context):
                     # Prepare entries for each batch in send_batches
                     entries = []
                     for item in send_batches:
-                        print('published', item)
                         message_body = 'published'
 
                         message_attributes = {
