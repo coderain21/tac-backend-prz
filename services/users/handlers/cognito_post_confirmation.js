677b39d032d052a138103e11f5393ae06263dfc5
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2024-07-01 14:10:19 +0530

    FF-1951 #time 2h 30m feature: creation of email template Trying to create the email template using post confirmation trigger

--- services/users/handlers/cognito_post_confirmation.js
+++ services/users/handlers/cognito_post_confirmation.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable camelcase */
 /* eslint-disable no-console */
 /* eslint-disable import/extensions */
@@ -5,7 +6,6 @@
 const AWS = require('aws-sdk')
 
 AWS.config.update({ region: process.env.REGION })
-
 /**
  * Function to handle  cognito post challenge event
  * @param {Object} event
@@ -13,12 +13,45 @@ AWS.config.update({ region: process.env.REGION })
  * @param {Object} callback
  * @returns returns event object
  */
-exports.handler = async (event, _context, callback) => {
+const crypto = require('crypto')
+const mailchimp = require('@mailchimp/mailchimp_transactional')(process.env.MAILCHIMP_API_KEY)
+
+// Initialize AWS services
+const cognito = new AWS.CognitoIdentityServiceProvider()
+
+// Function to create a Mailchimp template
+async function createMailchimpTemplate(userEmail) {
+    const templateName = `Welcome Template for ${userEmail}`
+    const htmlContent = `
+        <h1>Welcome to Our Platform, ${userEmail}!</h1>
+        <p>We're excited to have you on board.</p>
+        <!-- Add more HTML content as needed -->
+    `
+
     try {
-        console.log('event', event, event.identities[0].email)
-        // callback(null, event)
+        const response = await mailchimp.templates.add({
+            name: templateName,
+            html: htmlContent,
+        })
+
+        console.log('Mailchimp template created:', response)
+        return response
     } catch (error) {
-        console.log(error)
-        callback(error, event)
+        console.error('Error creating Mailchimp template:', error)
+        throw error
+    }
+}
+
+// Handler for the Post Confirmation Lambda trigger
+exports.handler = async (event, context) => {
+    const userEmail = event.request.userAttributes.email
+
+    try {
+        await createMailchimpTemplate(userEmail)
+        // Additional logic to handle post-confirmation actions...
+        console.log('Mailchimp template creation completed.')
+    } catch (error) {
+        console.error('Error creating Mailchimp template:', error)
+        // Handle error appropriately...
     }
 }
