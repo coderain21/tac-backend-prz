1afb2d9a3c385ec46e190093b603bb403d938263
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2024-07-15 15:23:13 +0530

    FF-1605 #time 3h feature: admin view list of logs Implemented the api to list all the logs in the admin side

---
+++ services/access-logs/handlers/list.py
@@ -0,0 +1,263 @@
+'''this api will list all the orders'''
+import json
+import os
+import re
+import pymongo
+from pymongo import MongoClient
+from lib.common_helper import Encoder
+import math
+import csv
+import boto3
+import tempfile
+from datetime import datetime
+
+headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': True,
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Methods': '*'
+}
+
+#database
+client = MongoClient(
+                      os.environ['MONGO_CLIENT'],
+                      maxIdleTimeMS=60000  # Set maxIdleTimeMS to 60 seconds (60000 milliseconds)
+                        )
+db = client[os.environ['DATABASE']]
+access_log_collection = db[os.environ['ACCESS_LOG_COLLECTION']]
+
+
+def prepend_backslash(text):
+    """
+    Prepend backslash to special characters in the given text.
+
+    Args:
+        text (str): The input text to modify.
+
+    Returns:
+        str: The modified text with backslashes before special characters.
+    """
+    # Define a regular expression pattern to match special characters
+    special_chars_pattern = re.compile(r'([\\.*+?()|[\]{}^$])')
+
+    # Use re.sub to replace each match with a backslash followed by the matched character
+    modified_text = re.sub(special_chars_pattern, r'\\\1', text)
+
+    return modified_text
+
+def list_all_logs(event, context):
+    try:
+        # Extract email address from the request context
+        email_address = event['requestContext']['authorizer']['claims'].get('cognito:username')
+        if not email_address:
+            return {
+                "statusCode": 403,
+                "headers": headers,
+                "body": json.dumps({"message": "You do not have access to perform this API action"})
+            }
+
+        # Initialize the query
+        query = {}
+
+        # Extract parameters from the request
+        data = event.get('queryStringParameters', {})
+        print('data', data)
+
+        # Extract individual parameters with default values
+        sort_by = data.get('sort_by', 'updated_at')
+        sort_order = data.get('sort_order', 'descending')
+        page = int(data.get('page', '1'))
+        limit = int(data.get('per_page', '10'))
+        start_date = data.get('start_date')
+        end_date = data.get('end_date')
+        export = data.get('export', 'false').lower() == 'true'
+        print('sort_by', sort_by, 'sort_order', sort_order, 'page', page, 'limit', limit, 'start_date', start_date, 'end_date', end_date, 'export', export)
+
+        # Initialize sort_criteria with a default value
+        sort_criteria = [(sort_by, pymongo.ASCENDING if sort_order == 'ascending' else pymongo.DESCENDING)]
+
+        # Initialize search query
+        
+        search_query = {}
+        if 'search' in data:
+            search_terms = data['search'].split()
+            search_conditions = []
+            for term in search_terms:
+                escaped_term = prepend_backslash(term)
+                search_conditions.append({
+                    "$or": [
+                        {"updated_by.email_address": {"$regex": escaped_term, "$options": "i"}},
+                        {"actor_id": {"$regex": escaped_term, "$options": "i"}}
+
+                             ]})
+            if search_conditions:
+                search_query["$and"] = search_conditions
+
+        # Merge search query with the existing query
+        query.update(search_query)
+
+        # Date range filter
+        if start_date and end_date:
+            print('start_date', start_date, 'end_date', end_date)
+            date_range_condition = {
+                "updated_at": {
+                    "$gte": int(start_date),
+                    "$lte": int(end_date)
+                }
+            }
+            query.update(date_range_condition)
+
+        # Query the MongoDB collection
+        access_log_list = list(access_log_collection.find(
+            query,
+            {
+                "_id": 1,
+                "actor_id": 1,
+                "updated_by": 1,
+                "section": 1,
+                "updated_at": 1,
+                "name": 1
+            }
+        ).sort(sort_criteria).skip((page - 1) * limit).limit(limit))
+
+        total_records = access_log_collection.count_documents(query)
+        total_pages = math.ceil(total_records / limit)
+
+        if not access_log_list:
+            print('No access logs found matching the criteria')
+
+        # Prepare the response body
+        body = {
+            "data": access_log_list,
+            "total_pages": total_pages,
+            "total_records": total_records,
+            "current_page": page,
+            "download_link": None
+        }
+
+        # Handle export to CSV
+        if export:
+            body["download_link"] = export_as_csv(access_log_list, email_address)
+
+        return {
+            "statusCode": 200,
+            "headers": headers,
+            "body": json.dumps(body, cls=Encoder)
+        }
+    except Exception as err:
+        print(err)
+        return {
+            "headers": headers,
+            "statusCode": 500,
+            "body": json.dumps({"message": "There was an error"})
+        }
+
+
+
+
+
+
+
+
+def format_date(date_value):
+    try:
+        if isinstance(date_value, int):
+            # Treat the value as a Unix timestamp (seconds since epoch)
+            date_obj = datetime.fromtimestamp(date_value)
+        else:
+            # Treat the value as a formatted string
+            date_obj = datetime.strptime(date_value, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+        # Format the datetime object as "03 June 2024"
+        formatted_date = date_obj.strftime('%d %B %Y')
+
+        return formatted_date
+
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+
+
+
+# currencySymbolMapping = {
+#     "GBP": '£',
+#     "USD": '$',
+#     "EUR": '€',
+#     "HKD": 'HK$',
+#     "JPY": '¥',
+#     "CHF": 'Fr',
+#     "SGD": 'S$',
+#     "AUD": 'A$',
+#     "CAD": 'C$',
+#     "INR": '₹',
+# }
+
+
+
+
+
+
+
+def export_as_csv(sales, email_address):
+    """
+    Exports a list of auctions as a CSV file and uploads it to an S3 bucket.
+
+    Args:
+        auctions (list): A list of dictionaries representing the auctions.
+
+    Returns:
+        str: The signed URL of the uploaded CSV file on S3.
+
+    Raises:
+        Exception: If an error occurs during the export and upload process.
+    """
+    try:
+        filename = "All Orders"
+        seller_email = email_address
+        temp_dir = tempfile.mkdtemp()
+        csv_file = os.path.join(temp_dir, f'{filename}.csv')
+
+        s3_key = f"exports/seller/{seller_email}/{filename}.csv"
+        s3_bucket = os.environ['S3_BUCKET']
+        print(s3_bucket, type(s3_bucket))
+        with open(csv_file, "w") as file:
+            writer = csv.DictWriter(file, ["Order ID", "Customer name", "Auction name","Order date", "Payment type", "Payment status"])
+            writer.writeheader()
+            print(333)
+            # Format the created_at field as dd-mm-year
+            for sale in sales:
+                modified_sales = {}
+                # currency = sale.get("currency", "")
+                # if currency in currencySymbolMapping:
+                #     currency = currencySymbolMapping.get(currency, "")
+                date = format_date(int(sale['created_at']))
+                # Format the date as a string with only the date
+                formatted_date = date
+                # shipping_address = sale['shipping_address']
+                # full_name = sale['name']
+                # print('full_name', full_name)
+                modified_sales["Order ID"] = sale["order_number"]
+                modified_sales["Customer name"] = sale.get("name", "")
+                modified_sales["Auction name"] = sale['auction_title']
+                modified_sales["Order date"] = formatted_date
+                # modified_sales["Result"] = currency + str(sale["amount"])
+                modified_sales["Payment status"] = sale["payment_status"]
+                modified_sales["Payment type"]= sale["payment"]
+                writer.writerow(modified_sales)
+        s3_client = boto3.client("s3", region_name='eu-west-2')
+        s3_client.upload_file(csv_file, s3_bucket, s3_key)
+        s3_resource = boto3.resource("s3", region_name='eu-west-2')
+        object_acl = s3_resource.ObjectAcl(s3_bucket, s3_key)
+        object_acl.put(ACL="public-read")
+        s3_signed_url = s3_client.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": s3_bucket, "Key": s3_key},
+            # URL expiration time in seconds (adjust as needed)
+            ExpiresIn=3600,
+        )
+        return s3_signed_url
+    except Exception as err:
+        print('Error: ', str(err))
+        return None
