461c0bce17296e934b6f31bf8f219fd58ee3d920
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2024-07-16 11:07:10 +0530

    FF-1987 #time 20m bugfix: paypal connect fix Fixed the redirection issue by adding more path to the redirection url

--- services/paypal/handlers/paypal_connect.py
+++ services/paypal/handlers/paypal_connect.py
@@ -47,7 +47,7 @@ def create_partner_referral(access_token, tracking_id, return_url):
             "partner_logo_url": "https://www.paypalobjects.com/webstatic/mktg/logo/pp_cc_mark_111x69.jpg",
             "return_url": return_url,
             "return_url_description": "the URL to return the merchant after the PayPal onboarding process.",
-            "action_renewal_url": os.environ['DASHBOARD_URL']
+            "action_renewal_url": os.environ['DASHBOARD_URL']+os.environ['PAYPAL_REDIRECTION_PATH'],
         },
         "operations": [
             {
