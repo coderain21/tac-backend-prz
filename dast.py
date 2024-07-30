54a24ef9941c053a9c4e3e616a26b95f039c3ec5
Namratha Shettigar <namratha.shettigar@7edge.com>
2024-07-30 15:04:51 +0530

    FF-2018 #time 5m ci: testing dast by changing swagger files

--- dast.py
+++ dast.py
@@ -10,18 +10,6 @@ import argparse
 # Initialize the Cognito client
 client = boto3.client('cognito-idp', region_name='eu-west-2')
 
-# Function to find Swagger files in the repository
-def find_swagger_files(root_dir):
-    swagger_files = []
-    service_dir = os.path.join(root_dir, 'services')
-    for dirpath, dirnames, filenames in os.walk(service_dir):
-        for filename in filenames:
-            if 'swagger.json' in filename:
-                path = os.path.join(dirpath, filename)
-                pats = path.split('/services/')
-                swagger_files.append('services/{0}'.format(pats[1]))
-    return swagger_files
-
 async def run_dast_for_swagger(url, api_token):
     command = f"docker run -v $(pwd):/zap/wrk/:rw -t -e ZAP_AUTH_HEADER_VALUE='Bearer {api_token}' softwaresecurityproject/zap-stable zap-api-scan.py -t '{url}' -f openapi -r test_results/report.html"
     try:
@@ -42,27 +30,22 @@ async def run_dast_for_swagger(url, api_token):
 async def generate_cognito_token(user_type):
     try:
         if user_type == 'USER':
-            print("here")
-            user_pool_id = os.environ['SELLER_COGNITO_USERPOOL_ID']
-            client_id = os.environ['SELLER_COGNITO_CLIENT_ID']
-            username = os.environ['API_USERNAME']
-            password = os.environ['PASSWORD']
+            user_pool_id = 'your_seller_cognito_userpool_id'
+            client_id = 'your_seller_cognito_client_id'
+            username = 'your_api_username'
+            password = 'your_password'
         elif user_type == 'BUYERS':
-            user_pool_id = os.environ['BUYER_COGNITO_USERPOOL_ID']
-            client_id = os.environ['BUYER_COGNITO_CLIENT_ID']
-            username = os.environ['BUYER_API_USERNAME']
-            password = os.environ['BUYER_PASSWORD']
+            user_pool_id = 'your_buyer_cognito_userpool_id'
+            client_id = 'your_buyer_cognito_client_id'
+            username = 'your_buyer_api_username'
+            password = 'your_buyer_password'
         elif user_type == 'ADMIN':
-            user_pool_id = os.environ['ADMIN_COGNITO_USERPOOL_ID']
-            client_id = os.environ['ADMIN_COGNITO_CLIENT_ID']
-            username = os.environ['ADMIN_USERNAME']
-            password = os.environ['ADMIN_PASSWORD']
+            user_pool_id = 'your_admin_cognito_userpool_id'
+            client_id = 'your_admin_cognito_client_id'
+            username = 'your_admin_username'
+            password = 'your_admin_password'
         else:
             print("Invalid user type specified.")
-            return
-
-        if user_pool_id is None or client_id is None or username is None or password is None:
-            print("Required environment variables are not set.")
             return
 
         response = client.admin_initiate_auth(
@@ -80,7 +63,6 @@ async def generate_cognito_token(user_type):
         else:
             token = response['Session']
 
-        os.environ['TOKEN'] = token
         return token
     except ClientError as e:
         print(e)
@@ -94,32 +76,32 @@ async def main(changed_files):
 
         if users_token and buyers_token and admin_token:
             tokens = {
-                'services/auctions': buyers_token,
-                'services/users': users_token,
-                'services/buyers': users_token,
-                'services/address-management': buyers_token,
-                'services/access-logs': admin_token,
-                'services/admin-buyer-management': admin_token,
-                'services/admin-management': admin_token,
-                'services/buyer-wishlist': buyers_token,
-                'services/cart-management': buyers_token,
-                'services/lot-bid-history': users_token,
-                'services/newsletter': users_token,
-                'services/order-management': users_token,
-                'services/payments': buyers_token,
-                'services/paypal': users_token,
-                'services/quicksight-dashboards': users_token,
-                'services/seller-bidder-management': users_token,
-                'services/site-banner': admin_token,
-                'services/subdomain': users_token
+                'admin-buyer-management': admin_token,
+                'users': users_token,
+                'buyers': users_token,
+                'address-management': buyers_token,
+                'access-logs': admin_token,
+                'admin-management': admin_token,
+                'buyer-wishlist': buyers_token,
+                'cart-management': buyers_token,
+                'lot-bid-history': users_token,
+                'newsletter': users_token,
+                'order-management': users_token,
+                'payments': buyers_token,
+                'paypal': users_token,
+                'quicksight-dashboards': users_token,
+                'seller-bidder-management': users_token,
+                'site-banner': admin_token,
+                'subdomain': users_token
                 # Add other service directories and their corresponding tokens here
             }
 
-            for service_dir in changed_files:
-                for service_directory, token in tokens.items():
-                    if service_directory in service_dir:
-                        await run_dast_for_swagger(service_dir, token)
-                        break  # Break the loop after finding and using the correct token
+            for changed_file in changed_files:
+                service_directory = changed_file.split('/')[0]
+                if service_directory in tokens:
+                    await run_dast_for_swagger(changed_file, tokens[service_directory])
+                else:
+                    print(f"No token found for service directory: {service_directory}")
         else:
             print("Token generation failed")
     except Exception as e:
