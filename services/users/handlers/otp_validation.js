3863ee5fa293bb1a86b4113b0c08e33364dec0ff
sandhyashri <sandhyashri@7edge.com>
2024-08-06 13:18:41 +0530

    FF-2037 #time 30m feature: sending-email-templates-from-mailchimp Query the mailchimp to get templates for the seller

--- services/users/handlers/otp_validation.js
+++ services/users/handlers/otp_validation.js
@@ -166,7 +166,7 @@ module.exports.otpValidation = async (event, _context, callback) => {
                         url: process.env.DASHBOARD_URL,
                     }
                     await helpers.sendPinpointEmail(userData.email_address, process.env.SES_SENDER_EMAIL_ID, JSON.stringify(template_data), process.env.TEMPLATE_ARN_WELCOME_EMAIL)
-
+                    // await mailchimpHelper.createTemplate(userData)
                     return {
                         statusCode: 201,
                         headers: await helpers.getHeaders(),
