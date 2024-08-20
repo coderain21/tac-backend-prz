493416f2f2be08bca478103ffc976690e55a27de
sandhyashri <sandhyashri@7edge.com>
2024-08-20 13:17:40 +0530

    FF-2054 #time 1h hotfix: seller-modify-congratulations-emaul-template While sending congratulations email, checking whether seller is having his own template from mailchimp Queried mailchimp , fetching the seller templates if template exist, sending from existing templates else, sending default template.

--- entities/Users.js
+++ entities/Users.js
@@ -49,6 +49,7 @@ const UserSchema = new Schema({
     tiktok_link: { type: String, trim: true, default: '' },
     kyc_status: { type: String, trim: true, default: false },
     kyb_status: { type: String, trim: true, default: false },
+    seller_id: { type: String, trim: true, default: '' },
 
 })
 
