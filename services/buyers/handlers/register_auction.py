49496c89ac914a0fa93661ef56c4a69086ebc0f1
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2024-08-27 16:41:37 +0530

    FF-2072 #time 20m hotfix: registration flow fix for federated login Fixed the pylint issue

--- services/buyers/handlers/register_auction.py
+++ services/buyers/handlers/register_auction.py
@@ -6,13 +6,8 @@ from pymongo import MongoClient
 from bson import ObjectId
 # from lib.helper_python import send_pinpoint_email
 from lib.email_helper import send_mailchimp_email
-# from lib.helper_python import send_pinpoint_email
-from lib.email_helper import send_mailchimp_email
 from datetime import datetime
 import pytz
-import mailchimp_transactional as MailchimpTransactional
-from mailchimp_transactional.api_client import ApiClientError
-
 import mailchimp_transactional as MailchimpTransactional
 from mailchimp_transactional.api_client import ApiClientError
 
