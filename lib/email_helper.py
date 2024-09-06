ba0bfb2dadba53416d9499d39b35301a5ebebb2b
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2024-09-06 16:58:55 +0530

    FF-2084 #time 2h hotfix: payment reciept issues Fixed the bugs related to currency, email address and seller name

--- lib/email_helper.py
+++ lib/email_helper.py
@@ -65,7 +65,7 @@ def send_mailchimp_payment_email(email, template_name, template_data, sender_ema
                 {"name": "country", "content": template_data["country"]},
                 {"name": "zip_code", "content": template_data["zip_code"]},
                 {"name": "email_address", "content": template_data["email_address"]},
-                {"name": "seller_email", "content": template_data["seller_email"]},
+                {"name": "seller_name", "content": template_data["seller_name"]},
                 {"name": "amount_paid", "content": template_data["amount_paid"]},
                 # {"name": "cdn_url", "content": template_data["cdn_url"]},
                 {"name": "lots", "content": template_data["lots"]}  # Assuming lots are correctly formatted
