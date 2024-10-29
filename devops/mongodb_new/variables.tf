54876e8c245ea44605dcad0f19ad791ac13a42f7
Nidhi <nidhi.shashi@7edge.com>
2024-10-29 10:56:56 +0530

    FF-2158 #time 5m ci: added terraform files for vpc peering

---
+++ devops/mongodb_new/variables.tf
@@ -0,0 +1,10 @@
+variable "STAGE" {
+  type        = string
+  description = "Description of the variable"
+}
+
+variable "REGION" {
+  type        = string
+  description = "Description of the variable"
+}
+
