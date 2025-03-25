4b3caabd33154316cc00262b14cc179335a0f6e5
Namratha Shettigar <namratha.shettigar@7edge.com>
2025-03-25 12:48:22 +0530

    INDY-237 #time 2h ci: create separate scripts for deployment,

--- devops/deployment.sh
+++ devops/deployment.sh
@@ -28,6 +28,7 @@ if [ "${STAGE}" = "pre-production" ] ; then
 fi
 log_bucket="indyauction-pipeline-states"
 echo "$log_bucket"
+# aws s3 sync $log_bucket . --profile $PROFILE_MAIN
 
 
 # Print AWS CLI configurations for verification
@@ -35,17 +36,20 @@ aws configure list --profile $PROFILE_MAIN
 aws configure list --profile $PROFILE_ENV
 run_command terraform -chdir=devops/assets init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/assets/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
 run_command terraform -chdir=devops/assets apply -auto-approve
+run_command terraform -chdir=devops/ses init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/ses/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
+run_command terraform -chdir=devops/ses apply -auto-approve
 run_command terraform -chdir=devops/admin_web_application init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/admin_web_application/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
 run_command terraform -chdir=devops/admin_web_application apply -auto-approve
 run_command terraform -chdir=devops/seller_web_application init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/seller_web_application/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
 run_command terraform -chdir=devops/seller_web_application apply -auto-approve
 run_command terraform -chdir=devops/api_gateway init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/api_gateway/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
 run_command terraform -chdir=devops/api_gateway apply -auto-approve
-run_command terraform -chdir=devops/ecs_new_service init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/ecs_new/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
-run_command terraform -chdir=devops/ecs_new_service apply -auto-approve
-run_command terraform -chdir=devops/stripe_webhook init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/stripe_webhook/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
-run_command terraform -chdir=devops/stripe_webhook apply -auto-approve
+run_command terraform -chdir=devops/vpc init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/vpc/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
+run_command terraform -chdir=devops/vpc apply -auto-approve
+run_command terraform -chdir=devops/ecs_new init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/ecs_new/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
+run_command terraform -chdir=devops/ecs_new apply -auto-approve
 
+# aws s3 sync . $log_bucket --exclude "*" --include "*.tfstate" --include "*tf-key-pair*" --exclude "*/dependency/*" --profile $PROFILE_MAIN
 npm i -g serverless@3.15.2
 npm i -g @serverless/compose
 npm i serverless-aws-documentation
@@ -69,18 +73,22 @@ cd services/users
 sls deploy --region $REGION --stage $STAGE
 sls deploy --region $REGION --stage $STAGE
 cd ../..
-
+cd services/lambda-authorizer
+sls deploy --region $REGION --stage $STAGE
+sls deploy --region $REGION --stage $STAGE
+cd ../..
 cd services/auctions
 sls deploy --region $REGION --stage $STAGE
 sls deploy --region $REGION --stage $STAGE
 cd ../..
+# terraform -chdir=devops/buyer_web_application init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/buyer_web_application/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
 terraform -chdir=devops/buyer_web_application init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/buyer_web_application/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
 echo "{\"subdomains\": [\"www\"]}" > devops/buyer_web_application/subdomains.json
 STATE_FILE="s3://${log_bucket}/$STAGE/devops/buyer_web_application/terraform.tfstate"
 # Check if the file exists in the S3 bucket
 if aws s3 ls "$STATE_FILE" --profile "${PROFILE_MAIN}" > /dev/null 2>&1; then
   echo "State file exists. Applying Terraform with targets..."
-  terraform -chdir=devops/buyer_web_application apply -auto-approve -target=aws_amplify_app.customer_web_application \
+  run_command terraform -chdir=devops/buyer_web_application apply -auto-approve -target=aws_amplify_app.customer_web_application \
                -target=aws_amplify_branch.amplify_branch \
                -target=aws_ssm_parameter.amplify_id \
                -target=aws_ssm_parameter.bitbucket_secret \
@@ -88,12 +96,12 @@ if aws s3 ls "$STATE_FILE" --profile "${PROFILE_MAIN}" > /dev/null 2>&1; then
                -target=data.external.env \
                -target=data.external.token
 else
-  terraform -chdir=devops/buyer_web_application apply -auto-approve
+  run_command terraform -chdir=devops/buyer_web_application apply -auto-approve
   #terraform -chdir=devops/buyer_web_application apply -auto-approveSS
 fi
-terraform -chdir=devops/cognito_custom_domain init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/cognito_custom_domain/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
-terraform -chdir=devops/cognito_custom_domain apply -auto-approve
-run_command sls deploy --stage ${STAGE} --max-concurrency 5
+run_command terraform -chdir=devops/cognito_custom_domain init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/cognito_custom_domain/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
+run_command terraform -chdir=devops/cognito_custom_domain apply -auto-approve
+run_command sls deploy --stage ${STAGE} --max-concurrency 8
 
 if [ $overall_status -ne 0 ]; then
     echo "One or more commands failed."
