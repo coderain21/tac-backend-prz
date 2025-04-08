e2a6d0c163fbbeaa91dfa5f137a27244b1a57cb9
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2025-04-08 11:35:56 +0530

    INDY-25 #time 30m feature: skipping the delete notification for now since there are multiple usecases

--- services/site-banner/hooks.py
+++ services/site-banner/hooks.py
@@ -3,6 +3,7 @@ import os
 import logging
 import urllib.parse
 
+# Configure logging with more detailed format including log level
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(message)s',
@@ -39,12 +40,20 @@ def set_authorization(transaction):
         logging.info(transaction['request'])
 
     global notification_id
-    
+
+    # Set notification_id first if it's a create operation
+    if transaction['expected']['statusCode'] == '201' and '/' in transaction['request']['uri']:
+        try:
+            logging.info(transaction['real']['body'])
+            notification_id = transaction['real']['body']['notification_id']
+        except (KeyError, TypeError):
+            logging.error("Could not set notification_id from response")
+            pass
+
+    # Then handle delete operation
     if '/67f3bf494d4a369d223917f5' in transaction['request']['uri'] and transaction['request']['method'] == 'DELETE':
         if notification_id:
             transaction['fullPath'] = transaction['fullPath'].replace('CMP00218', notification_id)
+            transaction['request']['uri'] = transaction['request']['uri'].replace('67f3bf494d4a369d223917f5', notification_id)
         else:
             transaction['skip'] = True
-            
-    if transaction['expected']['statusCode'] == '201':
-        notification_id = transaction['real']['body']['_id']
