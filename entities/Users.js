09ec8e15ffa214c5cd6abb1e13c8e5defcf6d1cf
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2025-05-02 14:33:41 +0530

    INDY-5 #time 20m feature: seller buyer update and view privacy policy successsfully updating the privacy policy in the seller side

--- entities/Users.js
+++ entities/Users.js
@@ -49,9 +49,10 @@ const UserSchema = new Schema({
     linkedin_link: { type: String, trim: true, default: '' },
     tiktok_link: { type: String, trim: true, default: '' },
     kyb_status: { type: String, trim: true, default: false },
-    seller_id: { type: String, trim: true, default: '' },
+    // seller_id: { type: String, trim: true, default: '' }, // Removed duplicate key
     full_name: { type: String, trim: true, default: '' },
-    plan_type: {type: String, trim: true, default: ''},
+    plan_type: { type: String, trim: true, default: '' }, // Added spaces
+    privacy_policy: { type: String, trim: true, default: '' },
 })
 
 UserSchema.plugin(mongoosePaginate)
