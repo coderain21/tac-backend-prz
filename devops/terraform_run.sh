4f20ac1cd9b1db6c441500de009ab33f6d4070a1
Namratha Shettigar <namratha.shettigar@7edge.com>
2025-05-03 14:09:10 +0530

    INDY-304 #time 10m ci: updated script

--- devops/terraform_run.sh
+++ devops/terraform_run.sh
@@ -15,30 +15,27 @@ run_command() {
 }
 
 
-
-KEY_PATH="$1" 
-CERT_PATH="$2"
-
+CERT_PATH="$1"
+KEY_PATH="$2" 
 
 apt-get update && apt-get install python-is-python3 -y && apt-get install python3-pip -y
 
-
-echo "Using certificate path: $CERT_PATH"
-
 aws configure set credential_process "$(pwd)/aws_signing_helper credential-process --certificate $CERT_PATH --private-key $KEY_PATH --trust-anchor-arn $TRUST_ANCHOR_ARN --profile-arn $PROFILE_ARN --role-arn $ROLE_ARN" --profile "$PROFILE_ENV"
-run_command aws configure set credential_process "$(pwd)/aws_signing_helper credential-process --certificate $CERT_PATH --private-key $KEY_PATH --trust-anchor-arn $TRUST_ANCHOR_ARN_MAIN --profile-arn $PROFILE_ARN_MAIN --role-arn $ROLE_ARN_MAIN" --profile $PROFILE_MAIN
-
-# Additional credential setup for pre-production
-if [ "${STAGE}" = "pre-production" ] ; then
-    echo "Setting up AWS credential process for QA profile"
-    run_command aws configure set credential_process "$(pwd)/aws_signing_helper credential-process --certificate $CERT_PATH --private-key $KEY_PATH --trust-anchor-arn $TRUST_ANCHOR_ARN_QA --profile-arn $PROFILE_ARN_QA --role-arn $ROLE_ARN_QA" --profile $AWS_ENV_QA
-    aws configure list --profile $AWS_ENV_QA
-fi
 export AWS_PROFILE=$PROFILE_ENV
 
 echo "Enabling AWS_SDK_LOAD_CONFIG..."
 export AWS_SDK_LOAD_CONFIG=1 
 
+# # Configure AWS CLI profiles
+run_command aws configure set credential_process "$(pwd)/aws_signing_helper credential-process --certificate $CERT_PATH --private-key $KEY_PATH --trust-anchor-arn $TRUST_ANCHOR_ARN_MAIN --profile-arn $PROFILE_ARN_MAIN --role-arn $ROLE_ARN_MAIN" --profile $PROFILE_MAIN
+
+# aws configure set profile.$PROFILE_ENV.aws_access_key_id $AWS_ACCESS_KEY_ID
+# aws configure set profile.$PROFILE_ENV.aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+if [ "${STAGE}" = "pre-production" ] ; then
+    echo "Setting up AWS credential process for QA profile"
+    run_command aws configure set credential_process "$(pwd)/aws_signing_helper credential-process --certificate $CERT_PATH --private-key $KEY_PATH --trust-anchor-arn $TRUST_ANCHOR_ARN_QA --profile-arn $PROFILE_ARN_QA --role-arn $ROLE_ARN_QA" --profile $AWS_ENV_QA
+    aws configure list --profile $AWS_ENV_QA
+fi
 log_bucket="indyauction-pipeline-states"
 echo "$log_bucket"
 # aws s3 sync $log_bucket . --profile $PROFILE_MAIN
@@ -183,28 +180,56 @@ npm i serverless-package-external
 npm i serverless-python-requirements
 npm i serverless-appsync-plugin
 export config=serverless.yml
+unset AWS_PROFILE
+eval $( $(pwd)/aws_signing_helper credential-process \
+  --certificate $CERT_PATH \
+  --private-key $KEY_PATH \
+  --trust-anchor-arn $TRUSTANCHORARN \
+  --profile-arn $PROFILEARN \
+  --role-arn $ROLEARN \
+| jq -r '. | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)"' )
+
 
 
 cd services/cognito-auth
-run_command sls deploy --region $REGION --stage $STAGE --aws-profile $PROFILE_ENV
+run_command sls deploy --region $REGION --stage $STAGE 
 cd ../..
 cd services/users
-run_command sls deploy --region $REGION --stage $STAGE --aws-profile $PROFILE_ENV
+run_command sls deploy --region $REGION --stage $STAGE 
 cd ../..
 # cd services/lambda-authorizer
 # run_command sls deploy --region $REGION --stage $STAGE
 # cd ../..
 cd services/auctions
-run_command sls deploy --region $REGION --stage $STAGE --aws-profile $PROFILE_ENV
+run_command sls deploy --region $REGION --stage $STAGE
 cd ../..
 # terraform -chdir=devops/buyer_web_application init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/buyer_web_application/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
-
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
+export AWS_PROFILE=$PROFILE_ENV
 run_command terraform -chdir=devops/buyer_web_application init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/buyer_web_application/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
 echo "{\"subdomains\": [\"www\"]}" > devops/buyer_web_application/subdomains.json
 STATE_FILE="s3://${log_bucket}/$STAGE/devops/buyer_web_application/terraform.tfstate"
 # Check if the file exists in the S3 bucket
 if aws s3 ls "$STATE_FILE" --profile "${PROFILE_MAIN}" > /dev/null 2>&1; then
   echo "State file exists. Applying Terraform with targets..."
+# STATE_FILE="$STAGE/devops/buyer_web_application/terraform.tfstate"
+# if [ -f "$STATE_FILE" ]; then
+#     echo "cheching for subdomain"
+  # Extract the DOMAIN_ASSOCIATION_ID only if the state file exists
+#   DOMAIN_ASSOCIATION_ID=$(terraform -chdir=devops/buyer_web_application state show aws_amplify_domain_association.domain_association | grep -oP '^\s*id\s*=\s*"\K[^"]+')
+#   APP_ID=$(terraform -chdir=devops/buyer_web_application state show aws_amplify_domain_association.domain_association | grep -oP '^\s*app_id\s*=\s*"\K[^"]+')
+#   AMPLIFY_DOMAIN_NAME=$(terraform -chdir=devops/buyer_web_application state show aws_amplify_domain_association.domain_association | grep -oP '^\s*domain_name\s*=\s*"\K[^"]+')
+  
+#   # Use the extracted ID (if any) in your subsequent commands
+#   echo "Extracted DOMAIN_ASSOCIATION_ID: $DOMAIN_ASSOCIATION_ID"
+#   terraform -chdir=devops/buyer_web_application state rm aws_amplify_domain_association.domain_association
+#   terraform -chdir=devops/buyer_web_application import aws_amplify_domain_association.domain_association $DOMAIN_ASSOCIATION_ID
+#   domain=$(aws amplify get-domain-association --app-id $APP_ID --domain-name $AMPLIFY_DOMAIN_NAME --profile $PROFILE_ENV --region $REGION --query 'domainAssociation.subDomains[*].subDomainSetting.prefix')
+#   echo "{\"subdomains\": $domain}" > devops/buyer_web_application/subdomains.json
+  # Add your commands here that use $DOMAIN_ASSOCIATION_ID (if needed)
+  # terraform -chdir=devops/buyer_web_application apply -auto-approve -target=aws_amplify_app.customer_web_application \
   run_command terraform -chdir=devops/buyer_web_application apply -auto-approve -target=aws_amplify_app.customer_web_application \
                -target=aws_amplify_branch.amplify_branch \
                -target=aws_ssm_parameter.amplify_id \
@@ -221,19 +246,24 @@ run_command terraform -chdir=devops/cognito_custom_domain apply -auto-approve
 # terraform -chdir=devops/cognito_custom_domain init -backend-config="bucket=${log_bucket}" -backend-config="key=$STAGE/devops/cognito_custom_domain/terraform.tfstate" -backend-config="profile=${PROFILE_MAIN}"
 # terraform -chdir=devops/cognito_custom_domain apply -auto-approve
 # aws s3 sync . $log_bucket --exclude "*" --include "*.tfstate" --include "*tf-key-pair*" --exclude "*/dependency/*" --profile $PROFILE_MAIN
+unset AWS_PROFILE
+eval $( $(pwd)/aws_signing_helper credential-process \
+  --certificate $CERT_PATH \
+  --private-key $KEY_PATH \
+  --trust-anchor-arn $TRUSTANCHORARN \
+  --profile-arn $PROFILEARN \
+  --role-arn $ROLEARN \
+| jq -r '. | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)"' )
 
 if [ "${STAGE}" = "qa" ] || [ "${STAGE}" = "pre-production" ]; then
   cd services/bdd-api
   sls deploy --region $REGION --stage $STAGE
   cd ../..
 fi
-export AWS_PROFILE=$PROFILE_ENV
-export AWS_REGION="eu-west-2"
 run_command sls deploy --stage ${STAGE} --max-concurrency 5
 cd services/quicksight-dashboards
-run_command sls deploy --region $REGION --stage $STAGE --aws-profile $PROFILE_ENV
+run_command sls deploy --region $REGION --stage $STAGE
 cd ../..
-
 
 
 if [ "${STAGE}" = "prod" ]; then
