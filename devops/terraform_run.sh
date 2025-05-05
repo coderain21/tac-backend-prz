592ae53e2194861bbe1eed4ed12905ec4009ff05
Namratha Shettigar <namratha.shettigar@7edge.com>
2025-05-05 16:06:46 +0530

    INDY-304 #time 15m ci: configuring the indyaution main us domain

--- devops/terraform_run.sh
+++ devops/terraform_run.sh
@@ -30,7 +30,11 @@ echo "Enabling AWS_SDK_LOAD_CONFIG..."
 export AWS_SDK_LOAD_CONFIG=1 
 
 # # Configure AWS CLI profiles
+aws configure set region "eu-west-2" --profile "$$PROFILE_MAIN"
 run_command aws configure set credential_process "$(pwd)/aws_signing_helper credential-process --certificate $CERT_PATH --private-key $KEY_PATH --trust-anchor-arn $TRUST_ANCHOR_ARN_MAIN --profile-arn $PROFILE_ARN_MAIN --role-arn $ROLE_ARN_MAIN" --profile $PROFILE_MAIN
+aws configure set region "us-east-1" --profile "$PROFILE_MAIN-us"
+run_command aws configure set credential_process "$(pwd)/aws_signing_helper credential-process --certificate $CERT_PATH --private-key $KEY_PATH --trust-anchor-arn $TRUST_ANCHOR_ARN_MAIN_US --profile-arn $PROFILE_ARN_MAIN_US --role-arn $ROLE_ARN_MAIN_US" --profile "$PROFILE_MAIN-us"
+
 
 # aws configure set profile.$PROFILE_ENV.aws_access_key_id $AWS_ACCESS_KEY_ID
 # aws configure set profile.$PROFILE_ENV.aws_secret_access_key $AWS_SECRET_ACCESS_KEY
