6661b7b63739765dc5ba20ba181efbb363669dd3
Namratha Shettigar <namratha.shettigar@7edge.com>
2025-05-14 15:45:13 +0530

    INDY-339 #time 1h ci: running all dast in pipeline

--- dast.py
+++ dast.py
@@ -1,3 +1,179 @@
+# import pathlib
+# import git
+# import subprocess
+# import asyncio
+# import os
+# import boto3
+# from botocore.exceptions import ClientError
+# import requests
+# import json
+# import datetime
+# import jwt
+# from boto3.session import Session
+
+
+# # Determine the repository path using pathlib
+# repo_path = git.Repo('.', search_parent_directories=True).working_tree_dir
+# print(repo_path, 'repo path')
+
+# # Create a Git repository object
+# repo = git.Repo(repo_path)
+
+# # Get the last commit SHA
+# last_commit_sha = repo.head.commit.hexsha
+# print(last_commit_sha, 'last commit')
+
+# # Get the parent commit SHAs of the last commit
+# parent_commit_shas = [parent.hexsha for parent in repo.commit(last_commit_sha).parents]
+# print("Parent commit SHAs:", parent_commit_shas)
+
+
+# # Initialize the Cognito client
+# profile = os.environ['PROFILE_ENV']
+# session = Session(profile_name=profile)
+# client = session.client('cognito-idp', region_name='eu-west-2')
+
+# # Function to find Swagger files in the repository
+# def find_swagger_files(root_dir):
+#     swagger_files = []
+
+#     service_dir = os.path.join(root_dir, 'services')
+#     for dirpath, dirnames, filenames in os.walk(service_dir):
+#         for filename in filenames:
+#             if 'swagger.json' in filename:
+#                 path = os.path.join(dirpath, filename)
+#                 pats = path.split('/services/')
+#                 swagger_files.append('services/{0}'.format(pats[1]))
+
+#     return swagger_files
+
+# async def run_dast_for_swagger(url, api_token):
+#     command = f"docker run --user=root -v $(pwd):/zap/wrk/:rw -t -e ZAP_AUTH_HEADER_VALUE='Bearer {api_token}' softwaresecurityproject/zap-stable zap-api-scan.py -t \"{url}\" -f openapi -r test_results/report.html"
+#     try:
+#         process = await asyncio.create_subprocess_shell(
+#             command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+#         )
+#         stdout, stderr = await process.communicate()
+
+#         print(process.returncode, "process.returncode")
+#         print("Command output:")
+#         print(stdout.decode())
+#     except asyncio.CancelledError:
+#         process.terminate()
+#         raise
+#     return None
+
+
+# async def generate_cognito_token(user_type):
+#     try:
+        
+#         if user_type == 'USER':
+#             print("here")
+#             user_pool_id = os.environ['SELLER_COGNITO_USERPOOL_ID']
+#             client_id = os.environ['SELLER_COGNITO_CLIENT_ID']
+#             username = os.environ['API_USERNAME']
+#             password = os.environ['PASSWORD']
+#         elif user_type == 'BUYERS':
+#             user_pool_id = os.environ['BUYER_COGNITO_USERPOOL_ID']
+#             client_id = os.environ['BUYER_COGNITO_CLIENT_ID']
+#             username = os.environ['BUYER_API_USERNAME']
+#             password = os.environ['BUYER_PASSWORD']
+#         elif user_type == 'ADMIN':
+#             user_pool_id = os.environ['ADMIN_COGNITO_USERPOOL_ID']
+#             client_id = os.environ['ADMIN_COGNITO_CLIENT_ID']
+#             username = os.environ['ADMIN_USERNAME']
+#             password = os.environ['ADMIN_PASSWORD']
+#         else:
+#             print("Invalid user type specified.")
+#             return
+
+#         if user_pool_id is None or client_id is None or username is None or password is None:
+#             print("Required environment variables are not set.")
+#             return
+
+#         response = client.admin_initiate_auth(
+#             UserPoolId=user_pool_id,
+#             ClientId=client_id,
+#             AuthFlow='ADMIN_NO_SRP_AUTH',
+#             AuthParameters={
+#                 'USERNAME': username,
+#                 'PASSWORD': password
+#             }
+#         )
+
+#         if 'AuthenticationResult' in response:
+#             token = response['AuthenticationResult']['IdToken']
+            
+#         else:
+#             token = response['Session']
+
+#         os.environ['TOKEN'] = token
+#         return token
+#     except ClientError as e:
+#         print(e)
+
+#     return None
+
+# async def main():
+#     try:
+#         users_token = await generate_cognito_token('USER')
+#         buyers_token = await generate_cognito_token('BUYERS')
+#         admin_token = await generate_cognito_token('ADMIN')
+
+#         if users_token and buyers_token and admin_token:
+#             tokens = {
+#                 'admin-buyer-management': admin_token,
+#                 'users': users_token,
+#                 'buyers': users_token,
+#                 'address-management': buyers_token,
+#                 'access-logs': admin_token,
+#                 'admin-management': admin_token,
+#                 'buyer-wishlist': buyers_token,
+#                 'cart-management': buyers_token,
+#                 'lot-bid-history': users_token,
+#                 'newsletter': users_token,
+#                 'order-management': users_token,
+#                 'payments': buyers_token,
+#                 'paypal': users_token,
+#                 'quicksight-dashboards': users_token,
+#                 'seller-bidder-management': users_token,
+#                 'site-banner': admin_token,
+#                 'subdomain': users_token
+#                 # Add other service directories and their corresponding tokens here
+#             }
+
+#             # Collect the list of changed files using git show
+#             changed_files = []
+#             for parent_sha in parent_commit_shas:
+#                 try:
+#                     parent_changed_files_output = subprocess.check_output(['git', 'show', '--name-only', parent_sha], text=True)
+#                     parent_changed_files = parent_changed_files_output.strip().split('\n')
+#                     print(parent_changed_files_output,'parent changes files output')
+#                     changed_files.extend(parent_changed_files)
+#                 except subprocess.CalledProcessError as e:
+#                     print(f"Error when running 'git show' for {parent_sha}:", e)
+#             swagger_files = [file for file in changed_files if file.endswith('.json') and 'swagger' in file]
+#             print("Swagger files:")
+#             print(swagger_files)
+#             for service_dir in find_swagger_files(repo_path):
+#                 # Check if the Swagger file has changed in the latest commit
+#                 if any(service_dir in changed_file for changed_file in changed_files):
+#                     for service_directory, token in tokens.items():
+#                         if service_directory in service_dir:
+#                             await run_dast_for_swagger(service_dir, token)
+#                             break  # Break the loop after finding and using the correct token
+#         # else:
+#         #     print("Token generation failed")
+#     except Exception as e:
+#         print("Error in the main function:", str(e))
+
+# if __name__ == '__main__':
+#     loop = asyncio.get_event_loop()
+#     loop.run_until_complete(main())
+
+
+
+import argparse
 import pathlib
 import git
 import subprocess
@@ -5,12 +181,16 @@ import asyncio
 import os
 import boto3
 from botocore.exceptions import ClientError
-import requests
-import json
-import datetime
-import jwt
+from dotenv import load_dotenv
 from boto3.session import Session
 
+# Load environment variables
+load_dotenv()
+
+# Argument parser
+parser = argparse.ArgumentParser(description="Run DAST scan for Swagger APIs.")
+parser.add_argument('-s', '--service', type=str, help='Run scan for a specific service only (e.g. access-logs)')
+args = parser.parse_args()
 
 # Determine the repository path using pathlib
 repo_path = git.Repo('.', search_parent_directories=True).working_tree_dir
@@ -19,24 +199,20 @@ print(repo_path, 'repo path')
 # Create a Git repository object
 repo = git.Repo(repo_path)
 
-# Get the last commit SHA
+# Get the last commit SHA and its parent(s)
 last_commit_sha = repo.head.commit.hexsha
 print(last_commit_sha, 'last commit')
-
-# Get the parent commit SHAs of the last commit
 parent_commit_shas = [parent.hexsha for parent in repo.commit(last_commit_sha).parents]
 print("Parent commit SHAs:", parent_commit_shas)
 
-
-# Initialize the Cognito client
+# Initialize the Cognito client with AWS profile
 profile = os.environ['PROFILE_ENV']
 session = Session(profile_name=profile)
 client = session.client('cognito-idp', region_name='eu-west-2')
 
-# Function to find Swagger files in the repository
+# Find Swagger files
 def find_swagger_files(root_dir):
     swagger_files = []
-
     service_dir = os.path.join(root_dir, 'services')
     for dirpath, dirnames, filenames in os.walk(service_dir):
         for filename in filenames:
@@ -44,9 +220,9 @@ def find_swagger_files(root_dir):
                 path = os.path.join(dirpath, filename)
                 pats = path.split('/services/')
                 swagger_files.append('services/{0}'.format(pats[1]))
-
     return swagger_files
 
+# Run DAST scan
 async def run_dast_for_swagger(url, api_token):
     command = f"docker run --user=root -v $(pwd):/zap/wrk/:rw -t -e ZAP_AUTH_HEADER_VALUE='Bearer {api_token}' softwaresecurityproject/zap-stable zap-api-scan.py -t \"{url}\" -f openapi -r test_results/report.html"
     try:
@@ -54,21 +230,17 @@ async def run_dast_for_swagger(url, api_token):
             command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
         )
         stdout, stderr = await process.communicate()
-
         print(process.returncode, "process.returncode")
         print("Command output:")
         print(stdout.decode())
     except asyncio.CancelledError:
         process.terminate()
         raise
-    return None
 
-
+# Generate Cognito tokens
 async def generate_cognito_token(user_type):
     try:
-        
         if user_type == 'USER':
-            print("here")
             user_pool_id = os.environ['SELLER_COGNITO_USERPOOL_ID']
             client_id = os.environ['SELLER_COGNITO_CLIENT_ID']
             username = os.environ['API_USERNAME']
@@ -85,11 +257,7 @@ async def generate_cognito_token(user_type):
             password = os.environ['ADMIN_PASSWORD']
         else:
             print("Invalid user type specified.")
-            return
-
-        if user_pool_id is None or client_id is None or username is None or password is None:
-            print("Required environment variables are not set.")
-            return
+            return None
 
         response = client.admin_initiate_auth(
             UserPoolId=user_pool_id,
@@ -100,73 +268,83 @@ async def generate_cognito_token(user_type):
                 'PASSWORD': password
             }
         )
-
-        if 'AuthenticationResult' in response:
-            token = response['AuthenticationResult']['IdToken']
-            
-        else:
-            token = response['Session']
-
+        token = response.get('AuthenticationResult', {}).get('IdToken') or response.get('Session')
         os.environ['TOKEN'] = token
         return token
     except ClientError as e:
         print(e)
+        return None
 
-    return None
-
+# Main execution
 async def main():
     try:
         users_token = await generate_cognito_token('USER')
         buyers_token = await generate_cognito_token('BUYERS')
         admin_token = await generate_cognito_token('ADMIN')
 
-        if users_token and buyers_token and admin_token:
-            tokens = {
-                'admin-buyer-management': admin_token,
-                'users': users_token,
-                'buyers': users_token,
-                'address-management': buyers_token,
-                'access-logs': admin_token,
-                'admin-management': admin_token,
-                'buyer-wishlist': buyers_token,
-                'cart-management': buyers_token,
-                'lot-bid-history': users_token,
-                'newsletter': users_token,
-                'order-management': users_token,
-                'payments': buyers_token,
-                'paypal': users_token,
-                'quicksight-dashboards': users_token,
-                'seller-bidder-management': users_token,
-                'site-banner': admin_token,
-                'subdomain': users_token
-                # Add other service directories and their corresponding tokens here
-            }
+        if not all([users_token, buyers_token, admin_token]):
+            print("Token generation failed.")
+            return
 
-            # Collect the list of changed files using git show
-            changed_files = []
-            for parent_sha in parent_commit_shas:
-                try:
-                    parent_changed_files_output = subprocess.check_output(['git', 'show', '--name-only', parent_sha], text=True)
-                    parent_changed_files = parent_changed_files_output.strip().split('\n')
-                    print(parent_changed_files_output,'parent changes files output')
-                    changed_files.extend(parent_changed_files)
-                except subprocess.CalledProcessError as e:
-                    print(f"Error when running 'git show' for {parent_sha}:", e)
-            swagger_files = [file for file in changed_files if file.endswith('.json') and 'swagger' in file]
-            print("Swagger files:")
-            print(swagger_files)
-            for service_dir in find_swagger_files(repo_path):
-                # Check if the Swagger file has changed in the latest commit
-                if any(service_dir in changed_file for changed_file in changed_files):
-                    for service_directory, token in tokens.items():
-                        if service_directory in service_dir:
-                            await run_dast_for_swagger(service_dir, token)
-                            break  # Break the loop after finding and using the correct token
-        # else:
-        #     print("Token generation failed")
+        tokens = {
+            'admin-buyer-management': admin_token,
+            'users': users_token,
+            'buyers': users_token,
+            'auctions': users_token,
+            'address-management': buyers_token,
+            'access-logs': admin_token,
+            'admin-management': admin_token,
+            'buyer-wishlist': buyers_token,
+            'cart-management': buyers_token,
+            'lot-bid-history': users_token,
+            'newsletter': users_token,
+            'order-management': users_token,
+            'payments': buyers_token,
+            'paypal': users_token,
+            'quicksight-dashboards': users_token,
+            'seller-bidder-management': users_token,
+            'site-banner': admin_token,
+            'subdomain': users_token,
+            'bids': users_token
+        }
+
+        # Print git changed files (for debugging/audit)
+        for parent_sha in parent_commit_shas:
+            try:
+                parent_changed_files_output = subprocess.check_output(
+                    ['git', 'show', '--name-only', parent_sha], text=True)
+                print(parent_changed_files_output, 'parent changes files output')
+            except subprocess.CalledProcessError as e:
+                print(f"Error running git show for {parent_sha}:", e)
+
+        # Find Swagger files
+        swagger_files = find_swagger_files(repo_path)
+
+        # If specific service is provided, run only that
+        if args.service:
+            matched = False
+            for swagger_path in swagger_files:
+                if args.service in swagger_path:
+                    matched = True
+                    token = tokens.get(args.service)
+                    if token:
+                        await run_dast_for_swagger(swagger_path, token)
+                    else:
+                        print(f"No token found for service '{args.service}'")
+                    break
+            if not matched:
+                print(f"No Swagger file found for service '{args.service}'")
+        else:
+            # Run for all services
+            for swagger_path in swagger_files:
+                for service_directory, token in tokens.items():
+                    if service_directory in swagger_path:
+                        await run_dast_for_swagger(swagger_path, token)
+                        break
+
     except Exception as e:
         print("Error in the main function:", str(e))
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
+
