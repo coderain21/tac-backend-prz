b282a0d9b7c3afab71182d7fdb81ee4822cfe2b3
william.pham88 <william.pham88@outlook.com>
2025-05-27 01:44:32 -0400

    [INDY-343] Implement delete bid functionality with API endpoint and related logic

--- services/lot-bid-history/utilities/helper.js
+++ services/lot-bid-history/utilities/helper.js
@@ -24,3 +24,34 @@ module.exports.getLowestBidder = async (lotId, Bid) => {
         return error
     }
 }
+
+module.exports.getNewTopBidderAfterDeletion = async (lotId, deletedBidderId, Bid) => {
+    try {
+        const query = {
+            lot_id: lotId,
+            buyer_id: { $ne: deletedBidderId }
+        }
+
+        const options = {
+            sort: {
+                bid_amount: -1, // Sorting in descending order by bid_amount (highest first)
+                updated_at: 1,
+            },
+            select: {
+                bid_amount: 1,
+                name: 1,
+                _id: 1,
+                updated_at: 1,
+                buyer_id: 1,
+                paddle_number: 1,
+                max_bid: 1,
+                // Add other fields you want to include in the result
+            },
+        }
+        // Get the highest bid that is not from the deleted bidder
+        const newTopBid = await Bid.findOne(query, null, options).lean()
+        return newTopBid
+    } catch (error) {
+        return error
+    }
+}
