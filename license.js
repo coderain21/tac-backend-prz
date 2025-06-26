f9f7c16ae62e9d4ba05ed972f811187920e3ab32
Namratha Shettigar <namratha.shettigar@7edge.com>
2025-06-26 18:35:21 +0530

    INDY-405 #time 5m ci: added licenses.json in gitignore

--- license.js
+++ license.js
@@ -6,7 +6,7 @@ const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 // Extract the license and description information
 const packageInfo = {};
 for (const dependency in packageJson.dependencies) {
-    const depPackageJson = JSON.parse(fs.readFileSync(`node_modules/${dependency}/package.json`, 'utf8'));
+    const depPackageJson = JSON.parse(fs.readFileSync(`devops/dependency/node/package.json`, 'utf8'));
     packageInfo[`${dependency}@${packageJson.dependencies[dependency]}`] = {
         license: depPackageJson.license,
         description: depPackageJson.description
