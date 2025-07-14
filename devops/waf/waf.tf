7f64467bf29a1a8fdd13d7dda656c59c9a8623f6
Namratha Shettigar <namratha.shettigar@7edge.com>
2025-07-14 14:36:50 +0530

    INDY-475 #time 5m ci:  removed bott control rule to fix integration testing

--- devops/waf/waf.tf
+++ devops/waf/waf.tf
@@ -217,24 +217,24 @@ resource "aws_wafv2_web_acl" "secure_api_web_acl" {
   #   }
   # }
 
-  rule {
-    name     = "AWSManagedRulesBotControlRuleSet"
-    priority = 3
-    override_action {
-      none {}
-    }
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "BotControl"
-      sampled_requests_enabled   = true
-    }
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesBotControlRuleSet"
-        vendor_name = "AWS"
-      }
-    }
-  }
+  # rule {
+  #   name     = "AWSManagedRulesBotControlRuleSet"
+  #   priority = 3
+  #   override_action {
+  #     none {}
+  #   }
+  #   visibility_config {
+  #     cloudwatch_metrics_enabled = true
+  #     metric_name                = "BotControl"
+  #     sampled_requests_enabled   = true
+  #   }
+  #   statement {
+  #     managed_rule_group_statement {
+  #       name        = "AWSManagedRulesBotControlRuleSet"
+  #       vendor_name = "AWS"
+  #     }
+  #   }
+  # }
 
   rule {
     name     = "AWSManagedRulesSQLiRuleSet"
