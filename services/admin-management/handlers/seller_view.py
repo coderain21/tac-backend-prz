f6c04a814d8c15818184c15c65e10342e82d1a45
Logesh Thulasiraman <logesh@Logeshs-MacBook-Pro.local>
2025-08-10 19:54:46 +0530

    add checkout_enabled toggle

--- services/admin-management/handlers/seller_view.py
+++ services/admin-management/handlers/seller_view.py
@@ -94,7 +94,8 @@ def seller_view(event, context):
             "country": 1,
             "country_code":1,
             "about":1,
-            "send_automated_auction_complete_email": 1
+            "send_automated_auction_complete_email": 1,
+            "checkout_enabled": 1
         }
         seller_details = seller_collection.find_one({"_id": seller_id})#, projections)
         if seller_details is None:
