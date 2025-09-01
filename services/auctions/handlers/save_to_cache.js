0325aaadc0c753ecbc26d9a33519cb4b7b2ec852
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2025-09-01 20:14:16 +0530

    INDY-650 #time 1h 45m hotfix: fixed the issue related to starting price not saving with the latest updated price removed the description while updating the lot when published status

--- services/auctions/handlers/save_to_cache.js
+++ services/auctions/handlers/save_to_cache.js
@@ -20,22 +20,27 @@ let connection
  *
  * @param {string} lot_id - The ID of the lot to retrieve.
  * @param {object} client - The Redis client for database interaction.
- *  @param {object} lot information - to save the lot to redis cache.
+ * @param {object} lot information - to save the lot to redis cache.
  * @returns {object} true
  */
 module.exports.handler = async (event, context, callback) => {
     try {
-        if (connection === null || !connection.readyState) {
-            connection = await mongodbHelper.connect()
-        }
+        connection = await mongodbHelper.connect()
+        console.log('event', event)
+        console.log('connection', connection)
+
         const data = typeof event === 'string' ? JSON.parse(event) : event
         const client = await redisHelper.createRedisClient()
         const redisKey = `lot:${data._id}`
         let endDateISO
-        const redisPayload = JSON.stringify(data)
+
         const lotDetails = await mongodbHelper.view(Lot, {
             _id: new ObjectId(data._id),
         })
+        console.log('lotDetails', lotDetails)
+
+        const redisPayload = { ...data }
+
         if (data && data.lot_end_time) {
             endDateISO = new Date(data.lot_end_time).toISOString()
             data.extended = false
@@ -45,15 +50,24 @@ module.exports.handler = async (event, context, callback) => {
             endDateISO = new Date(data.end_date).toISOString()
             data.extended = true
             data.lot_extended = true
-            redisPayload.starting_price = lotDetails.starting_price
-            await client.hset('lot', redisKey, redisPayload)
+
+            // ✅ overwrite values from DB
+            redisPayload.starting_price = lotDetails[0]?.starting_price || redisPayload.starting_price
+            redisPayload.images = lotDetails[0]?.images || redisPayload.images
+
+            console.log('redisPayload', redisPayload)
+
+            // ✅ save to Redis
+            await client.hset('lot', redisKey, JSON.stringify(redisPayload))
         }
+
         data.end_date = endDateISO
 
         // Calculate delay_seconds = (lot_number - 1) * 10
         const lot_number = data.lot_number || 1 // Default to 1 if lot_number is missing
         const delay_seconds = (lot_number - 1) * 10
         data.delay_seconds = delay_seconds
+
         return { ...data }
     } catch (e) {
         return e
