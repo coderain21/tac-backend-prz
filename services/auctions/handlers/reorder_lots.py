3e3b3c17a97326f8c8c3c524a724ee57754b8845
william.pham88 <william.pham88@outlook.com>
2025-09-23 00:52:23 +0300

    Updated reoder_lots function to accpet new structure of reordering lots

--- services/auctions/handlers/reorder_lots.py
+++ services/auctions/handlers/reorder_lots.py
@@ -1,4 +1,4 @@
-"""This module is used to reorder lots"""
+"""This module is used to bulk reorder lots"""
 import json
 import os
 from pymongo import MongoClient
@@ -11,18 +11,16 @@ headers = {
     'Access-Control-Allow-Methods': '*'
 }
 
-
-def reorder_lots(event):
+def bulk_reorder_lots(event):
     """
-    The `reorder_lots` accepts auction id and the lot number that has to be re-ordered along with the new position.
+    The `bulk_reorder_lots` accepts auction id and an array of lot reorder instructions.
+    This is more efficient than individual reorders and ensures data consistency.
 
-    :param auction_id: The `auction_id` for which the lots has to be reordered.
-    :param reorder_lot_number: The `reorder_lot_number` parameter denotes the lot that has to be re-ordered.
-    :param reorder_position: The position to which the lot has to be moved too.
+    :param auction_id: The auction_id for which the lots have to be reordered.
+    :param lot_orders: Array of {lot_id, old_lot_number, new_lot_number} objects
 
     :return: a JSON response with the following properties:
-    - "statusCode": The HTTP status code of the response (204 for success, 403 for access denied, 500
-    for error)
+    - "statusCode": The HTTP status code of the response (200 for success, 403 for access denied, 500 for error)
     """
     try:
         try:
@@ -35,104 +33,129 @@ def reorder_lots(event):
                 "body": json.dumps({"message": "You do not have access to perform this API action"})
             }
 
-        #read input body
+        # Read input body
         request_body = json.loads(event['body'])
         auction_id = request_body.get('auction_id')
-        reorder_lot_number = request_body.get('reorder_lot_number')
-        reorder_position = request_body.get('reorder_position')
+        lot_orders = request_body.get('lot_orders', [])
 
-        #initialize mongo db
+        # Initialize mongo db
         client = MongoClient(
-                      os.environ['MONGO_CLIENT'],
-                      maxIdleTimeMS=60000  # Set maxIdleTimeMS to 60 seconds (60000 milliseconds)
-                        )
+            os.environ['MONGO_CLIENT'],
+            maxIdleTimeMS=60000  # Set maxIdleTimeMS to 60 seconds (60000 milliseconds)
+        )
         db = client[os.environ['DATABASE']]
         lot_collection = db[os.environ["LOT_COLLECTION_NAME"]]
 
-        if not auction_id or not reorder_lot_number or not reorder_position or not seller_email:
-            return (400, {"message": "auction_id reorder_lot_number and reorder_position are required for reordering a lot."})
-        lot_to_be_reordered = list(lot_collection.find(
-            {"seller_email": seller_email, "auction_id": auction_id, "lot_number": int(reorder_lot_number)}))
+        if not auction_id or not lot_orders or not seller_email:
+            return {
+                "statusCode": 400,
+                "headers": headers,
+                "body": json.dumps({"message": "auction_id and lot_orders are required for reordering lots."})
+            }
 
-        if len(lot_to_be_reordered) != 1:
-            return (400, {"message": "Lot number is not valid to re-order."})
+        # Validate that all lots belong to the seller and auction
+        lot_ids = [order['lot_id'] for order in lot_orders]
+        existing_lots = list(lot_collection.find({
+            "seller_email": seller_email,
+            "auction_id": auction_id,
+            "_id": {"$in": lot_ids}
+        }))
 
-        lot_collection.delete_one({"seller_email": seller_email, "auction_id": auction_id, "lot_number": int(reorder_lot_number)})
+        if len(existing_lots) != len(lot_orders):
+            return {
+                "statusCode": 400,
+                "headers": headers,
+                "body": json.dumps({"message": "Some lots are not valid for reordering."})
+            }
 
-        if reorder_lot_number > reorder_position:
-            #move lot upwards
-            lots_to_update = lot_collection.find({"seller_email": seller_email, "auction_id": auction_id, "lot_number": {"$gte": int(reorder_position)}})
+        # Check for auction status (should be in draft)
+        auction_collection = db[os.environ.get("AUCTION_COLLECTION_NAME", "auctions")]
+        auction = auction_collection.find_one({
+            "auction_id": auction_id,
+            "seller_email": seller_email
+        })
 
-            lots_updated = []
-            for lot in lots_to_update:
-                lot_collection.delete_one(
-                     {"auction_id": lot['auction_id'], "seller_email": lot["seller_email"], "lot_number":lot["lot_number"]})
-                lot['lot_number'] = lot['lot_number']+1
-                del lot["_id"]
-                lots_updated.append(lot)
-                lot_collection.insert_one(lot)
+        if not auction or auction.get('status') != 'Draft':
+            return {
+                "statusCode": 400,
+                "headers": headers,
+                "body": json.dumps({"message": "Auction must be in draft state to reorder lots."})
+            }
 
-            lot_to_be_reordered[0]["lot_number"] = int(reorder_position)
-            del lot_to_be_reordered[0]['_id']
-            lot_collection.insert_one(lot_to_be_reordered[0])
+        # Start bulk update operation
+        bulk_operations = []
+        
+        for order in lot_orders:
+            lot_id = order['lot_id']
+            new_lot_number = order['new_lot_number']
+            
+            # Update the lot with new lot_number
+            bulk_operations.append({
+                "updateOne": {
+                    "filter": {
+                        "_id": lot_id,
+                        "seller_email": seller_email,
+                        "auction_id": auction_id
+                    },
+                    "update": {
+                        "$set": {"lot_number": int(new_lot_number)}
+                    }
+                }
+            })
 
-        else:
-            #lot moving down
-            lots_to_update = lot_collection.find({"seller_email": seller_email, "auction_id": auction_id, "lot_number": {"$gte": int(reorder_lot_number)}})
+        # Execute all updates in a single bulk operation for consistency
+        if bulk_operations:
+            result = lot_collection.bulk_write(bulk_operations)
+            print(f"Updated {result.modified_count} lots")
 
-            lots_updated = []
-            for lot in lots_to_update:
-                lot_collection.delete_one(
-                     {"auction_id": lot['auction_id'], "seller_email": lot["seller_email"], "lot_number":lot["lot_number"]})
-                lot['lot_number'] = lot['lot_number']-1
-                del lot["_id"]
-                lots_updated.append(lot)
-                lot_collection.insert_one(lot)
-
-            lot_to_be_reordered[0]["lot_number"] = int(reorder_position)
-            del lot_to_be_reordered[0]['_id']
-            lot_collection.insert_one(lot_to_be_reordered[0])
         client.close()
-        return (204, {}) #update successful
-    except Exception as err:
+        
         return {
+            "statusCode": 200,
             "headers": headers,
+            "body": json.dumps({
+                "message": "Lots successfully reordered",
+                "updated_count": len(bulk_operations)
+            })
+        }
+
+    except Exception as err:
+        print(f"Error in bulk_reorder_lots: {str(err)}")
+        return {
             "statusCode": 500,
-            "body": json.dumps({"message": "There was an error "})
+            "headers": headers,
+            "body": json.dumps({"message": "There was an error reordering the lots"})
         }
 
 def lambda_handler(event, context):
     """
-    The lambda_handler function is a Python function that handles incoming JSON requests,
-    updates a lot based on the request body, and returns a response with the appropriate status
-    code and response body.
-
-    :param event:
-    The `event` parameter is a dictionary that contains information about the event that
-    triggered the Lambda function. It typically includes details such as the HTTP request headers,
-    request body, and other metadata
-    :param context: T
-    he `context` parameter in the `lambda_handler` function is an object that provides
-    information about the runtime environment of the Lambda function.
-    It includes properties such as the
-    AWS request ID, function name, function version, and more. The `context` object is automatically
-    passed to the Lambda function by the
-    :return: The lambda_handler function is returning a dictionary with three keys: "statusCode",
-    "headers", and "body". The value of "statusCode" is the status code of the response,
-    the value of "headers" is a dictionary of headers for the response,
-    and the value of "body" is a JSON string representing the response body.
+    The lambda_handler function handles bulk lot reordering requests.
     """
     try:
-        # Parse the incoming JSON request
-        status_code, response_body = reorder_lots(event)
-        return {
-            "statusCode": status_code,
-            "headers": headers,
-            "body": json.dumps(response_body)
-        }
+        # Check HTTP method
+        if event.get('httpMethod') == 'POST':
+            status_code, response_body = bulk_reorder_lots(event)
+            
+            # Handle tuple return from bulk_reorder_lots
+            if isinstance(status_code, tuple):
+                actual_status, actual_body = status_code
+                return {
+                    "statusCode": actual_status,
+                    "headers": headers,
+                    "body": json.dumps(actual_body)
+                }
+            else:
+                # Handle dict return (new format)
+                return response_body
+        else:
+            return {
+                "statusCode": 405,
+                "headers": headers,
+                "body": json.dumps({"message": "Method not allowed"})
+            }
+            
     except Exception as e:
-        print(e)
-        print(e.with_traceback)
+        print(f"Lambda handler error: {str(e)}")
         return {
             "statusCode": 500,
             "headers": headers,
