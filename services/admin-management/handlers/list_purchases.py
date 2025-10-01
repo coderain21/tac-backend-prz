d9328a518c7a63775cf4b88e19e5177085303ac6
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2025-10-01 12:47:14 +0530

    INDY-635 #time 5m bugfix: fixed the export issue

--- services/admin-management/handlers/list_purchases.py
+++ services/admin-management/handlers/list_purchases.py
@@ -216,14 +216,14 @@ def export_as_csv(sales):
                 date = datetime.fromtimestamp(sale['created_at'])
                 # Format the date as a string with only the date
                 formatted_date = date.strftime('%Y-%m-%d')
-                shipping_address = sale['shipping_address']
+                shipping_address = sale.get('shipping_address')
                 full_name = f"{shipping_address['first_name']} {shipping_address['last_name']}"
-                modified_sales["ORDER ID"] = sale["order_number"]
+                modified_sales["ORDER ID"] = sale.get("order_number")
                 modified_sales["Customer Name"] = full_name
-                modified_sales["Auction Name"] = sale['auction_title']
+                modified_sales["Auction Name"] = sale.get('auction_title')
                 modified_sales["Order Date"] = formatted_date
-                modified_sales["Payment Status"] = sale["payment_status"]
-                modified_sales["Payment Type"]= sale["payment"]
+                modified_sales["Payment Status"] = sale.get("payment_status", "")
+                modified_sales["Payment Type"]= sale.get("payment", "")
                 writer.writerow(modified_sales)
         s3_client = boto3.client("s3", region_name='eu-west-2')
         s3_client.upload_file(csv_file, s3_bucket, s3_key)
