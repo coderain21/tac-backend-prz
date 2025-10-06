ddef57a1d7bf1b22d49ba07ff0790eda9b9d9118
Logesh Thulasiraman <logesh@Logeshs-MacBook-Pro.local>
2025-10-06 21:17:56 +0530

    fix the csv export to include max bid column

--- services/admin-buyer-management/handlers/bid_info.js
+++ services/admin-buyer-management/handlers/bid_info.js
@@ -116,12 +116,13 @@ async function exportAsCsv(bidders) {
             const currencySymbol = currencySymbolMapping[bidder.currency]
             // Append currency symbol to the bid amount
             const amountWithSymbol = `${currencySymbol}${bidder.bid_amount}`
+            const maxBidWithSymbol = `${currencySymbol}${bidder.max_bid}`
 
             records.push({
                 'Paddle Number': bidder.paddle_number || '',
                 'Bidder Name': bidder.name, // `${bidder.first_name || ''} ${bidder.last_name || ''}`.trim(),
                 Amount: amountWithSymbol,
-                'Max Bid': bidder.max_bid,
+                'Max Bid': maxBidWithSymbol,
                 'Bid Date': formattedDate,
             })
         }
