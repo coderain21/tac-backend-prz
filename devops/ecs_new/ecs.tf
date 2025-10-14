e6928a6c8e6597fcd02e73a018a69d9f697c00e2
Namratha Shettigar <namratha.shettigr@7edge.com>
2025-10-14 18:14:16 +0530

    INDY-619 #time 1h 30m ci: creating autoscaling based on request, testing bidding and extension

--- devops/ecs_new/ecs.tf
+++ devops/ecs_new/ecs.tf
@@ -204,11 +204,11 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_full_access_task_role" {
 }
 
 # Attach Autoscaling policy
-resource "aws_iam_role_policy_attachment" "ECS_autoscaling_policy" {
-  role     = "${aws_iam_role.ecs_task_role.name}"
-  policy_arn = "arn:aws:iam::149706502537:policy/ECS_autoscaling_policy"
-  provider = aws.deployment-eu
-}
+# resource "aws_iam_role_policy_attachment" "ECS_autoscaling_policy" {
+#   role     = "${aws_iam_role.ecs_task_role.name}"
+#   policy_arn = "arn:aws:iam::149706502537:policy/ECS_autoscaling_policy"
+#   provider = aws.deployment-eu
+# }
 
 # Attach CloudWatch Logs full access policy
 resource "aws_iam_role_policy_attachment" "cloudwatch_logs_full_access_task_execution_role" {
@@ -566,6 +566,24 @@ resource "aws_appautoscaling_policy" "memory" {
     }
 
     target_value = data.aws_ssm_parameter.ecs_memory.value
+  }
+  provider = aws.deployment-eu
+}
+resource "aws_appautoscaling_policy" "request_count" {
+  name = "request-count"
+  policy_type = "TargetTrackingScaling"
+  resource_id = aws_appautoscaling_target.target.resource_id
+  scalable_dimension = aws_appautoscaling_target.target.scalable_dimension
+  service_namespace = aws_appautoscaling_target.target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ALBRequestCountPerTarget"
+      resource_label = "${aws_lb.new-load-balancer.arn_suffix}/${aws_lb_target_group.new_target_group.arn_suffix}"
+    }
+    target_value = 1000
+    scale_out_cooldown = 300
+    scale_in_cooldown = 300
   }
   provider = aws.deployment-eu
 }
