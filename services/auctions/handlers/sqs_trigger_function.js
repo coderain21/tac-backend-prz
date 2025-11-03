f6df8feb63ed3023a1b814ac7e1bb69528242318
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2025-11-03 19:30:03 +0530

    INDY-748 #time 4h hotfix: created a new function for getting the bids form the db tested in dev still fix is required for the email

--- services/auctions/handlers/sqs_trigger_function.js
+++ services/auctions/handlers/sqs_trigger_function.js
@@ -21,6 +21,7 @@ const Auction = require('../entities/Auction')
 const mongodbHelper = require('../lib/mongodb_helper')
 const redisHelper = require('../lib/redis_helper')
 const BidInformation = require('../entities/BidInformation')
+const UniqueBid = require('../entities/Bid')
 const Users = require('../entities/Users')
 const Buyers = require('../entities/Buyers')
 const SubDomain = require('../entities/SubDomain')
@@ -266,25 +267,33 @@ module.exports.sqsTriggerFunction = async (event) => {
                     for (let i = 0; i < getLotInfo.length; i++) {
                         singleLot.push(JSON.parse(getLotInfo[i]))
                     }
+
                     // Add the CDN link to the image URL
                     const featuredImage = lot.images.find((image) => image.featured)
-                    lot.lot_image = `${process.env.CDN_LINK}${featuredImage ? featuredImage.url : lot.images[0].url}`
+
+                    // CREATE A DEEP COPY OF THE LOT FOR THIS BIDDER
+                    const lotForThisBidder = JSON.parse(JSON.stringify(lot))
+                    lotForThisBidder.lot_image = `${process.env.CDN_LINK}${featuredImage ? featuredImage.url : lot.images[0].url}`
 
                     // Add the formatted bid amount to the lot
                     if (lot.winning_user === user.buyer_id) {
                         event.lot_number = lot.lot_number
                         event.email_address = user.email_address
-                        const getAmount = await mongodbHelper.getBidAmount(event, BidInformation)
-                        lot.bid_amount = formatCurrency(getAmount.bid_amount, auctionData.currency)
-                        winningLot.push(lot)
+                        event.lot_id = lot._id
+                        event.buyer_id = user.buyer_id
+                        const getAmount = await mongodbHelper.getBid(event, UniqueBid)
+                        lotForThisBidder.bid_amount = formatCurrency(getAmount.bid_amount, auctionData.currency)
+                        winningLot.push(lotForThisBidder)
                     } else {
                         event.lot_number = lot.lot_number
                         event.email_address = user.email_address
-                        const getAmount = await mongodbHelper.getBidAmount(event, BidInformation)
+                        event.lot_id = lot._id
+                        event.buyer_id = user.buyer_id
+                        const getAmount = await mongodbHelper.getBid(event, UniqueBid)
                         if (getAmount !== null) {
                             console.log('not null')
-                            lot.bid_amount = formatCurrency(getAmount.bid_amount, auctionData.currency)
-                            notWinning.push(lot)
+                            lotForThisBidder.bid_amount = formatCurrency(getAmount.bid_amount, auctionData.currency)
+                            notWinning.push(lotForThisBidder)
                         }
                     }
                 }
@@ -319,12 +328,12 @@ module.exports.sqsTriggerFunction = async (event) => {
 
                         if (buyerInformation.length > 0) {
                             const template_data = {
-                                winning_lot: [...winningLot].sort((a, b) => a.lot_number - b.lot_number),
+                                winning_lot: winningLot.sort((a, b) => a.lot_number - b.lot_number),
                                 winning_lot_count: winningLot.length,
                                 buyer: buyerInformation[0].first_name === '' ? 'Customer' : `${buyerInformation[0].first_name} ${buyerInformation[0].last_name}`,
                                 title: auctionData.title,
                                 logo_url: auctionData.logo_image === '' ? `${process.env.S3_BUCKET_URL}Logo.png` : `${process.env.S3_BUCKET_URL}${auctionData.logo_image}`,
-                                not_winning_lot: [...notWinning].sort((a, b) => a.lot_number - b.lot_number),
+                                not_winning_lot: notWinning.sort((a, b) => a.lot_number - b.lot_number),
                                 not_winning_lot_count: notWinning.length,
                                 seller_name: sellerInformation[0].first_name === '' ? 'User' : `${sellerInformation[0].first_name} ${sellerInformation[0].last_name}`,
                                 seller_email: auctionData.seller_email,
@@ -367,7 +376,7 @@ module.exports.sqsTriggerFunction = async (event) => {
                         console.log('Final Auction Image:', auctionImage)
 
                         console.log(`Creating order for ${user.email_address} with amount: ${orderAmount}`)
-                        
+
                         const orderData = {
                             order_number: orderNumber,
                             seller_email: auctionData.seller_email,
@@ -425,12 +434,12 @@ module.exports.sqsTriggerFunction = async (event) => {
                         // Create the email data
                         if (buyerInformation.length > 0) {
                             const template_data = {
-                                winning_lot: [...winningLot].sort((a, b) => a.lot_number - b.lot_number),
+                                winning_lot: winningLot.sort((a, b) => a.lot_number - b.lot_number),
                                 winning_lot_count: winningLot.length,
                                 buyer: buyerInformation[0].first_name === '' ? 'Customer' : `${buyerInformation[0].first_name} ${buyerInformation[0].last_name}`,
                                 title: auctionData.title,
                                 logo_url: auctionData.logo_image === '' ? `${process.env.S3_BUCKET_URL}Logo.png` : `${process.env.S3_BUCKET_URL}${auctionData.logo_image}`,
-                                not_winning_lot: [...notWinning].sort((a, b) => a.lot_number - b.lot_number),
+                                not_winning_lot: notWinning.sort((a, b) => a.lot_number - b.lot_number),
                                 not_winning_lot_count: notWinning.length,
                                 seller_name: sellerInformation[0].first_name === '' ? 'User' : `${sellerInformation[0].first_name} ${sellerInformation[0].last_name}`,
                                 seller_email: auctionData.seller_email,
@@ -460,7 +469,7 @@ module.exports.sqsTriggerFunction = async (event) => {
                             buyer: buyerInformation[0].first_name === '' ? 'Customer' : `${buyerInformation[0].first_name} ${buyerInformation[0].last_name}`,
                             title: auctionData.title,
                             logo_url: auctionData.logo_image === '' ? `${process.env.S3_BUCKET_URL}Logo.png` : `${process.env.S3_BUCKET_URL}${auctionData.logo_image}`,
-                            not_winning_lot: [...notWinning].sort((a, b) => a.lot_number - b.lot_number),
+                            not_winning_lot: notWinning.sort((a, b) => a.lot_number - b.lot_number),
                             not_winning_lot_count: notWinning.length,
                             seller_name: sellerInformation[0].first_name === '' ? 'User' : `${sellerInformation[0].first_name} ${sellerInformation[0].last_name}`,
                             seller_email: auctionData.seller_email,
