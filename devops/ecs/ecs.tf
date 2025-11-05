dc0212e1f2e97fc077b07b7af17f84aab57071eb
Namratha Shettigar <namratha.shettigr@7edge.com>
2025-11-05 09:59:08 +0530

    INDY-419 #time 1h ci: updating the thershold of cpu and memory scaling

--- devops/ecs/ecs.tf
+++ devops/ecs/ecs.tf
@@ -628,6 +628,45 @@ resource "aws_appautoscaling_policy" "ecs_cpu_scaling" {
   provider = aws.deployment-eu
 }
 
+resource "aws_appautoscaling_policy" "ecs_memory_target_scaling" {
+  name               = "ecs-${local.is_bidding ? "bidding-" : (local.is_dev_preprod ? "new-" : "")}memory-scaling-${var.STAGE}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.target.resource_id
+  scalable_dimension = aws_appautoscaling_target.target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value       = 50               # keep average memory usage ~65%
+    scale_in_cooldown  = 60                # wait 1 min before scaling in
+    scale_out_cooldown = 30                # scale out quickly when needed
+  }
+
+  provider = aws.deployment-eu
+}
+resource "aws_appautoscaling_policy" "ecs_cpu_target_scaling" {
+  name               = "ecs-${local.is_bidding ? "bidding-" : (local.is_dev_preprod ? "new-" : "")}memory-scaling-${var.STAGE}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.target.resource_id
+  scalable_dimension = aws_appautoscaling_target.target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value       = 50               # keep average memory usage ~65%
+    scale_in_cooldown  = 60                # wait 1 min before scaling in
+    scale_out_cooldown = 30                # scale out quickly when needed
+  }
+
+  provider = aws.deployment-eu
+}
+
 resource "aws_appautoscaling_policy" "ecs_memory_scaling" {
   name               = "ecs-${local.is_bidding ? "bidding-" : (local.is_dev_preprod ? "new-" : "")}memory-scaling-${var.STAGE}"
   policy_type        = "StepScaling"
