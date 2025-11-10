7a7434cce336d2c22529594062a25c755cb536d9
Ibrahim Khaleel <ibrahim.khaleel@7edge.com>
2025-11-10 11:11:39 +0530

    INDY-817 #time 30m feature: successfully sending notification when an auction ends tested in dev

--- services/auctions/handlers/sqs_trigger_function.js
+++ services/auctions/handlers/sqs_trigger_function.js
@@ -522,7 +522,7 @@ module.exports.sqsTriggerFunction = async (event) => {
         // Send notification
         if (process.env.SOCKET_URL) {
             try {
-                const payload = { auction_id: event.auction_id, seller_email: event.seller_email }
+                const payload = { auction_id: event._id, seller_email: event.seller_email }
                 const response = await axios({
                     method: 'POST',
                     url: `${process.env.SOCKET_URL}/completed`,
@@ -533,8 +533,13 @@ module.exports.sqsTriggerFunction = async (event) => {
                     },
                     data: payload,
                 })
-                console.log(`Updated and notified for lot ${lot_id}`)
-                return { success: true, lot_id, response: response.status }
+                console.log(`Updated and notified for lot ${event.seller_email} and auction ${event.auction_id}`)
+                return {
+                    success: true,
+                    auction_id: event.auction_id,
+                    seller_email: event.seller_email,
+                    response: response.status,
+                }
             } catch (err) {
                 console.log('err', err)
                 client.disconnect()
